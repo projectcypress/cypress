c84812fbb1a2f2dc6dd3cd5be96e96c29b9f1a62
Michael Nosal <mnosal@mitre.org>
2011-05-31 13:45:27 -0400

    remove some trailing/leading spaces

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,8 @@
 class Result
+
   include Mongoid::Document
+
   store_in :patient_cache
   embeds_one :value, class_name: "ResultValue", inverse_of: :result_value
-  
+
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,9 +1,12 @@
 class Vendor
+
   include Mongoid::Document
+
   embeds_many :tests, class_name: "Run", inverse_of: :vendor
-  
+
   field :name, type: String
   field :poc, type: String
   field :tel, type: String
   field :proctor, type: String
+
 end
