077ac01282848f5656852cfdae23bcb4a22218d3
Marc Hadley <mhadley@mitre.org>
2011-06-02 11:58:54 -0400

    Break out measure patient table into a separate view that is injected using AJAX - allows dynamic update of measure view during initial vendor setup

--- public/javascripts/cypress.js
+++ public/javascripts/cypress.js
@@ -8,7 +8,7 @@
     }, 3000);
   }
   
-  $.cypress.updateResultRow = function(result) {
+  $.cypress.updateResults = function(result) {
     if (result.numerator=="?")
       return true;
     var resultRow = $("#"+result.measure_id);
@@ -24,17 +24,36 @@
       // data can be a single result row as returned by the measures controller
       // or a structure containing an array of results as returned by the vendor controller
       if (data.results) {
+        // Vendor view page
         for (i=0;i<data.results.length;i++) {
-          pollAgain |= $.cypress.updateResultRow(data.results[i]);
+          pollAgain |= $.cypress.updateResults(data.results[i]);
         }
       } else {
-        pollAgain |= $.cypress.updateResultRow(data);
+        // Measure view page
+        pollAgain |= $.cypress.updateResults(data);
+        if (!pollAgain) {
+          // calculation is complete so show patient list table
+          $.cypress.updatePatientTable(url+"/patients");
+        }
       }
       if (pollAgain) {
         // true if any result is not yet available
         $.cypress.addPoll(url);
       }
     });
+  }
+  
+  $.cypress.updatePatientTable = function(url) {
+    $.ajax({ url: url,
+             type: "GET",
+             dataType: 'html',
+             success: function(res){
+               $('#vendor_patients').html(res);
+             },
+             error: function(xhr, err) {
+               alert("Patient table update failed");
+             }
+           });
   }
   
 })( jQuery );
