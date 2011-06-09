2e799ea572c7eda2fc1a1ae5ab3c17326a3d11c3
Marc Hadley <mhadley@mitre.org>
2011-06-09 14:13:06 -0400

    Add dynamic styling for reported results elements to measure view

--- app/helpers/vendors_helper.rb
+++ app/helpers/vendors_helper.rb
@@ -1,11 +1,2 @@
 module VendorsHelper
-  def result_class(expected, reported, component)
-    if (expected[component].class==String || reported[component].class==String)
-      'na'
-    elsif (expected[component]!=reported[component])
-      'fail'
-    else
-      ''
-    end
-  end
 end
