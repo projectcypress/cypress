0a0dcce2e8c42293b99fbf37164f4439bec16a74
Andy Gregorowicz <andy@mitre.org>
2011-07-14 14:16:22 -0400

    Adding encounters to the C32 generation

--- test/functional/patients_controller_test.rb
+++ test/functional/patients_controller_test.rb
@@ -22,4 +22,16 @@ class PatientsControllerTest < ActionController::TestCase
     assert_equal 'F', parsed['gender']
     assert_equal 345426614, parsed['birthdate']
   end
+  
+  test "encounters" do
+    sign_in :user, User.first(:conditions => {:email => 'bobby@tables.org'})
+    get(:show, {:id => '4dcbecdb431a5f5878000004', :format => 'c32'})
+    assert_response :success
+    doc = Nokogiri::XML(response.body)
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    patient = @pi.create_c32_hash(doc)
+    encounter = patient[:encounters][0]
+    assert_equal 1267322332, encounter.time
+    assert_equal({"CPT" => ["99201"]}, encounter.codes)
+  end
 end
