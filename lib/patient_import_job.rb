16ce33f2756e44c55e95eb660d909e9f34f27b8f
Andy Gregorowicz <andy@mitre.org>
2012-01-04 15:46:39 -0500

    Adding the denormalized data calculation to the PatientImportJob

--- lib/patient_import_job.rb
+++ lib/patient_import_job.rb
@@ -2,14 +2,13 @@ module Cypress
   class PatientImportJob < Resque::JobWithStatus
     
     # TODO: Need to add in the test id
-    # TODO: Need to run through the property matching/denormalization logic
     def perform
       Zip::ZipFile.foreach(options['zip_file_location']) do |zip_entry|
         if zip_entry.name.include?('.xml') && ! zip_entry.name.include?('__MACOSX')
           doc = Nokogiri::XML(zip_entry.get_input_stream)
           doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
           patient = HealthDataStandards::Import::C32::PatientImporter.instance.parse_c32(doc)
-          patient.save!
+          QME::Importer::MeasurePropertiesGenerator.instance.generate_properties!(patient)
         end
       end
     end
