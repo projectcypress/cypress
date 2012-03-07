b0151fd605de89b15496159b81fe0e1605e673af
Adam Goldstein <agoldstein@mitre.org>
2012-03-07 11:18:31 -0500

    Adding a few changes to make the demo server update smoother.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ Cypress::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Fallback to the asset pipeline if we've forgotten to precompile
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
