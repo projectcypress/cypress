8ebecc3505c93c672f350f6b242a5441c2a4f3ce
John Mazella <jmazella>
2012-03-30 05:30:10 -0400

    Fixing/improving population clone job test

--- test/unit/lib/population_clone_job_test.rb
+++ test/unit/lib/population_clone_job_test.rb
@@ -4,20 +4,26 @@ require 'fileutils'
 class PopulationCloneJobTest < ActiveSupport::TestCase
 
   setup do 
+  	collection_fixtures('patient_populations','_id')
   	collection_fixtures('records','_id','test_id')
   end
   	
   def test_perform
     assert Record.count == 2
 
-    pcj1 = Cypress::PopulationCloneJob.new(UUID.generate,'subset_id' => 'all','test_id' => '4f5a606b1d41c851eb000484')
+    pcj1 = Cypress::PopulationCloneJob.new(UUID.generate,'subset_id' => 'all','test_id' => '4f5a606b1d41c851eb000483')
     pcj1.perform
     assert Record.count == 4
-    assert Record.where(:test_id => '4f5a606b1d41c851eb000484').count == 2
+    assert Record.where(:test_id => '4f5a606b1d41c851eb000483').count == 2
 
-    pcj2 = Cypress::PopulationCloneJob.new(UUID.generate,'patient_ids' => ['19','20'],'test_id' => '4f5a606b1d41c851eb000485')
+    pcj2 = Cypress::PopulationCloneJob.new(UUID.generate,'subset_id' => 'test','test_id' => '4f5a606b1d41c851eb000484')
     pcj2.perform
     assert Record.count == 6
+    assert Record.where(:test_id => '4f5a606b1d41c851eb000484').count == 2
+
+    pcj3 = Cypress::PopulationCloneJob.new(UUID.generate,'patient_ids' => ['19','20'],'test_id' => '4f5a606b1d41c851eb000485')
+    pcj3.perform
+    assert Record.count == 8
 	assert Record.where(:test_id => '4f5a606b1d41c851eb000485').count == 2
   end
 
