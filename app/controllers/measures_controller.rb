0b1f80b2781fb863a70531c16d460765d18fc5c1
Adam Goldstein <agoldstein@mitre.org>
2012-04-02 18:27:15 -0400

    Refactored minimal set matrix.

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -78,29 +78,40 @@ class MeasuresController < ApplicationController
     overflow_ids = minimal_set[:overflow]
     
     # Query to find the actual Records for our minmal set and overflow
-    @patient_list = Record.where( { _id: { "$in" => minimal_ids } } ).only(:_id, :first, :last, :birthdate, :gender, :patient_id).order_by([["_id", :desc]])
-    @overflow = Record.where({ _id: { "$in" => overflow_ids } }).only(:_id, :first, :last, :birthdate, :gender, :patient_id)
-    all_patients = @patient_list | @overflow
+    @patient_list = Record.where( { _id: { "$in" => minimal_ids } } ).only(:_id, :first, :last, :birthdate, :gender, :patient_id).order_by([["_id", :desc]]).to_a
+    @overflow = Record.where({ _id: { "$in" => overflow_ids } }).only(:_id, :first, :last, :birthdate, :gender, :patient_id).to_a
     
     # Get the results that are relevant to the measures and patients the user asked for
-    results = Result.where({'value.measure_id' => { "$in" => measure_ids}, 'value.patient_id' => { "$in" => minimal_ids } })
+    results = Result.where({'value.measure_id' => { "$in" => measure_ids}, 'value.patient_id' => { "$in" => minimal_ids | overflow_ids } })
     
     # Use the relevant results to build @coverage of each measure
     @coverage = {}
-    buckets = ['numerator', 'denominator', 'antinumerator', 'exclusion']
+    buckets = ["numerator", "denominator", "antinumerator", "exclusions"]
     results.each do |result|
-      # Increment each bucket for the measure if this result belongs
+      # Skip results that don't fall into any of the buckets
+      next if !result.value['numerator'] && !result.value['denominator'] && !result.value['antinumerator'] && !result.value['exclusions']
+      
+      # Identify the measure to which this result is referring
       measure = "#{result.value.measure_id}#{result.value.sub_id}".to_s
-      @coverage[measure] ||= {}
-      buckets.each do |bucket|
-        @coverage[measure][bucket] ||= 0
-        @coverage[measure][bucket] += 1 if result.value[bucket]
+      
+      # Add this measure to the patients for easy lookup in both directions (i.e. patients <-> measures)
+      patient_index = @patient_list.index{|patient| patient.id == result.value.patient_id}
+      if patient_index
+        patient = @patient_list[patient_index]
+      else
+        patient_index = @overflow.index{|patient| patient.id == result.value.patient_id}
+        patient = @overflow[patient_index]
       end
-      # Find and add this results' patient to the list if we haven't done so already. We use just a condensed version of the patient
-      index = all_patients.index{|patient| patient.id == result.value.patient_id}
-      patient = all_patients[index]
-      @coverage[measure]['patients'] ||= []
-      @coverage[measure]['patients'] << patient if !@coverage[measure]['patients'].include?(patient)
+      patient['measures'] ||= []
+      patient['measures'] << measure
+      
+      # Add the patient along with their placement in buckets to this measure
+      @coverage[measure] ||= {}
+      @coverage[measure][patient._id] = {}
+      buckets.each do |bucket|
+        bucket_value = result.value[bucket] ? 1 : 0
+        @coverage[measure][patient._id][bucket] = bucket_value
+      end
     end
     
     respond_to do |format|
