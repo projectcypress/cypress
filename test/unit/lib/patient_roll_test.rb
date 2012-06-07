74e5640185ad6cc7b30a284f4471a0f044a5fc21
Jamburger <jah562@cornell.edu>
2012-06-07 13:02:48 -0400

    Unit test for patient_roll.rb using only 2 of 3 patient records withautomatic failing test

---
+++ test/unit/lib/patient_roll_test.rb
@@ -0,0 +1,88 @@
+require 'test_helper'
+require 'fileutils'
+
+class PQRITest < ActiveSupport::TestCase
+
+setup do
+  	collection_fixtures('records','_id','test_id')
+	@roll = Cypress::PatientRoll
+end
+	
+
+  test "should roll dates forward by years, months, days" do
+	control = [#Record.where(:first => 'Rachel').first.birthdate, 
+	          # Record.where(:first => 'Rachel').first.measures["0002"].first.second.first,
+			   Record.where(:first => 'Rosa').first.measures["0073"]["diastolic_blood_pressure_physical_exam_finding"].second["date"],
+			   Record.where(:first => 'Rosa').first.care_goals.first.time,
+			   Record.where(:first => 'Selena').first.vital_signs.first.time,
+			   Record.where(:first => 'Rosa').first.laboratory_tests.second.time,
+			   Record.where(:first => 'Selena').first.encounters.second.time,
+			  # Record.where(:first => 'Rachel').first.conditions.first.time,
+			   Record.where(:first => 'Rosa').first.procedures.second.time,
+			   Record.where(:first => 'Selena').first.medications.second.time,
+			   Record.where(:first => 'Rosa').first.immunizations.second.time]
+
+	control.each_with_index do |t, i|
+	    control[i] = Time.at(t).advance(:years => -132, :months => 5, :days => 27).to_i
+	end
+    @roll.roll(-132,5,27)
+
+	variable = [#Record.where(:first => 'Rachel').first.birthdate, 
+	           #Record.where(:first => 'Rachel').first.measures["0002"].first.second.first,
+			   Record.where(:first => 'Rosa').first.measures["0073"]["diastolic_blood_pressure_physical_exam_finding"].second["date"],
+			   Record.where(:first => 'Rosa').first.care_goals.first.time,
+			   Record.where(:first => 'Selena').first.vital_signs.first.time,
+			   Record.where(:first => 'Rosa').first.laboratory_tests.second.time,
+			   Record.where(:first => 'Selena').first.encounters.second.time,
+			   #Record.where(:first => 'Rachel').first.conditions.first.time,
+			   Record.where(:first => 'Rosa').first.procedures.second.time,
+			   Record.where(:first => 'Selena').first.medications.second.time,
+			   Record.where(:first => 'Rosa').first.immunizations.second.time]
+	
+    assert_equal control, variable 
+  end
+
+ 
+  test "should roll date forward by time from effective date" do
+    binding.pry
+    effective_date = Time.gm(APP_CONFIG["effective_date"]["year"],
+                                    APP_CONFIG["effective_date"]["month"],
+                                    APP_CONFIG["effective_date"]["day"]) 
+    effective_date = effective_date.advance(:years => 0, :months => -2, :days => 14)
+							
+									
+    control = [#Record.where(:first => 'Rachel').first.birthdate, 
+	          # Record.where(:first => 'Rachel').first.measures["0002"].first.second.first,
+			   Record.where(:first => 'Rosa').first.measures["0073"]["diastolic_blood_pressure_physical_exam_finding"].second["date"],
+			   Record.where(:first => 'Rosa').first.care_goals.first.time,
+			   Record.where(:first => 'Selena').first.vital_signs.first.time,
+			   Record.where(:first => 'Rosa').first.laboratory_tests.second.time,
+			   Record.where(:first => 'Selena').first.encounters.second.time,
+			   #Record.where(:first => 'Rachel').first.conditions.first.time,
+			   Record.where(:first => 'Rosa').first.procedures.second.time,
+			   Record.where(:first => 'Selena').first.medications.second.time,
+			   Record.where(:first => 'Rosa').first.immunizations.second.time]
+puts Time.at control[0]		
+	control.each_with_index do |t, i|
+	    control[i] = Time.at(t).to_date.advance(:years => 0, :months => -2, :days => 14).to_time.to_i
+	end
+	
+    @roll.roll(effective_date.strftime("%m/%d/%Y"))
+	
+	variable = [#Record.where(:first => 'Rachel').first.birthdate, 
+	          # Record.where(:first => 'Rachel').first.measures["0002"].first.second.first,
+			   Record.where(:first => 'Rosa').first.measures["0073"]["diastolic_blood_pressure_physical_exam_finding"].second["date"],
+			   Record.where(:first => 'Rosa').first.care_goals.first.time,
+			   Record.where(:first => 'Selena').first.vital_signs.first.time,
+			   Record.where(:first => 'Rosa').first.laboratory_tests.second.time,
+			   Record.where(:first => 'Selena').first.encounters.second.time,
+			  # Record.where(:first => 'Rachel').first.conditions.first.time,
+			   Record.where(:first => 'Rosa').first.procedures.second.time,
+			   Record.where(:first => 'Selena').first.medications.second.time,
+			   Record.where(:first => 'Rosa').first.immunizations.second.time]
+	
+    assert_equal control, variable
+  end
+  test "fake" do
+  end
+end
