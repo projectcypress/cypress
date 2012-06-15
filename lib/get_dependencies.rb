99059400bbf03ab49ba27215d273c827c4b0f5ac
John Mazella <jmazella>
2012-06-15 13:35:57 -0400

    Measures version stored in test execution

--- lib/get_dependencies.rb
+++ lib/get_dependencies.rb
@@ -1,11 +1,12 @@
 module Cypress
   class GetDependencies
 
-    def self.get_dependencies()
+    def self.get_dependencies(bundle_id)
       deps = Hash.new
       env = Bundler::environment
-      
-      deps["Measures"] = APP_CONFIG["measures_version"]
+
+      bundle = Mongoid.database['bundles'].find({'_id' => bundle_id}).first
+      deps["Measures"] = 'v' + bundle['version']
       deps["Health-Data-Standards"] = 'v' + env.specs.to_hash["health-data-standards"].first.version.to_s
       deps["Quality-Measure-Engine"]= 'v' + env.specs.to_hash["quality-measure-engine"].first.version.to_s
 
@@ -15,8 +16,6 @@ module Cypress
     def self.version()
       return APP_CONFIG["version"]
     end
-
-
     
   end
 end
