e4665ce885072de4db486d12a5c15ecffaa6ae96
John Mazella <jmazella>
2012-07-11 11:17:04 -0400

    Updating measure bundle.zip for measure 0105

--- test/functional/export_and_reimport_test.rb
+++ test/functional/export_and_reimport_test.rb
@@ -6,7 +6,7 @@ class ExportAndReimportTest < ActionController::TestCase
   
   setup do
     puts "wiping db"
-    wipe_db_and_load_patients()
+    @patient_count = wipe_db_and_load_patients()
     puts "loading measures"
     load_measures()
     collection_fixtures('vendors', '_id',"user_ids")
@@ -17,52 +17,52 @@ class ExportAndReimportTest < ActionController::TestCase
   
   test "Export records, reimport them, then compare results" do
     if @patient_count == 225
-                          assert Record.count  == 225 , "Wrong number of records in DB before export"
-                          assert Measure.count == 78  , "Wrong number of measures in DB"
-                          ptest = ProductTest.find('4f58f8de1d41c851eb000478')
-                          ptest.effective_date =  Time.gm(APP_CONFIG["effective_date"]["year"],
-                                                          APP_CONFIG["effective_date"]["month"],
-                                                          APP_CONFIG["effective_date"]["day"]).to_i
-                          puts "creating zip"                                
-                          zip = Cypress::CreateDownloadZip.create_zip(Record.where("test_id" => nil),'c32')
+      assert Record.count  == 225 , "Wrong number of records in DB before export"
+      assert Measure.count == 78  , "Wrong number of measures in DB"
+      ptest = ProductTest.find('4f58f8de1d41c851eb000478')
+      ptest.effective_date =  Time.gm(APP_CONFIG["effective_date"]["year"],
+                                      APP_CONFIG["effective_date"]["month"],
+                                      APP_CONFIG["effective_date"]["day"]).to_i
+      puts "creating zip"                                
+      zip = Cypress::CreateDownloadZip.create_zip(Record.where("test_id" => nil),'c32')
 
-                          puts "importing records"
-                          pij = Cypress::PatientImportJob.new(UUID.generate,
-                            'zip_file_location' => zip.path,
-                            'test_id' => ptest.id,
-                            'format' => 'c32')
-                          pij.perform
-                         
+      puts "importing records"
+      pij = Cypress::PatientImportJob.new(UUID.generate,
+        'zip_file_location' => zip.path,
+        'test_id' => ptest.id,
+        'format' => 'c32')
+      pij.perform
+     
 
-                          assert Record.count  == 450 , "Wrong number of records in DB after reimport"
+      assert Record.count  == 450 , "Wrong number of records in DB after reimport"
 
-                         puts "Evaling measures"
-                          Measure.installed.each do |measure|
-                            Cypress::MeasureEvaluator.eval_for_static_records(measure,false)
-                            Cypress::MeasureEvaluator.eval(ptest,measure,false)
-                          end
-                          puts "querying records"
-                          # there will be only 2 sets of values inthe query cache, the initail patients and those of the test so we can 
-                          # group the results by measure_id and sub_id then check that all items in the group match
-                          results =  Mongoid.master["query_cache"].group({key:[:measure_id, :sub_id],initial:{values:[]},reduce:"function(doc,out){out.values[out.values.length] = doc;}"})
-                          assert_equal results.length, 78
-                          correct = true
-                          results.each do |res|
-                            if res.values.length != 2
-                              correct == false
-                              next
-                            end
-                            r1 = values[0]
-                            r2 = values[1]
-                            
-                            if r1["population"]  !=  r2["population"]  || r1["denominator"] != 	r2["denominator"] ||
-                          			r1["numerator"]   != 	r2["numerator"]   || r1["exclusions"]  != 	r2["exclusions"]  then
-                      		     puts "#{r1.inspect }     #{r2.inspect}"
-                      		     correct = false
-                            end                   
-                          end
-                          
-                          assert correct , "Inconsistent results found"
+     puts "Evaling measures"
+      Measure.installed.each do |measure|
+        Cypress::MeasureEvaluator.eval_for_static_records(measure,false)
+        Cypress::MeasureEvaluator.eval(ptest,measure,false)
+      end
+      puts "querying records"
+      # there will be only 2 sets of values inthe query cache, the initail patients and those of the test so we can 
+      # group the results by measure_id and sub_id then check that all items in the group match
+      results =  Mongoid.master["query_cache"].group({key:[:measure_id, :sub_id],initial:{values:[]},reduce:"function(doc,out){out.values[out.values.length] = doc;}"})
+      assert_equal results.length, 78
+      correct = true
+      results.each do |res|
+        if res.values.length != 2
+          correct == false
+          next
+        end
+        r1 = values[0]
+        r2 = values[1]
+        
+        if r1["population"]  !=  r2["population"]  || r1["denominator"] != 	r2["denominator"] ||
+      			r1["numerator"]   != 	r2["numerator"]   || r1["exclusions"]  != 	r2["exclusions"]  then
+  		     puts "#{r1.inspect }     #{r2.inspect}"
+  		     correct = false
+        end                   
+      end
+      
+      assert correct , "Inconsistent results found"
     else
       puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
       puts "MPL directory does not have all 225 records needed to run this test"
