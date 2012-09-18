b358dcb3cab817bd8785fc6aad0e92f5b27bcb7f
John Mazella <jmazella>
2012-09-18 15:45:50 -0400

    Updating measure definition viewer to use lastest pophealth code

--- app/assets/javascripts/cypress.js
+++ app/assets/javascripts/cypress.js
@@ -103,45 +103,111 @@
   }
 
     $.cypress.renderMeasureJSON = function(data) {
-      var measure = data;
-      var elemParent;
-      var addParamItems = function(obj,elemParent,container) {
-        var items = obj['and'] || obj['or'];
-        if ($.isArray(items)) {
-          var conjunction = obj['and'] ? 'and' : 'or';
-          // add the grouping container
-          if (!container) {elemParent = $("#ph_tmpl_paramGroup").tmpl({}).appendTo(elemParent).find(".paramItem:last");}
-            $.each(items,function(i,node) {
-              addParamItems(node,elemParent);
-              if (i < items.length-1) {
-                $(elemParent).append("<span class='"+conjunction+"'>"+conjunction+"</span>");
+        var measure = data;
+        var elemParent;
+        var addParamItems = function(obj,elemParent,container) {
+          var conjunction, obj, items, subset_operator, _i, _len, _ref;
+          items = obj["items"];
+          if ((obj.title != null)) {
+            if ((obj.subset_operators != null)) {
+              _ref = obj.subset_operators;
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                subset_operator = _ref[_i];
+                $(elemParent).append("<span class='" + subset_operator.type + "'>" +
+                (
+                  subset_operator.type + ' ' +
+                  (
+                    subset_operator.value ? popHealth.textForRange(subset_operator.value, false) : ''
+                  ) +
+                  ' of'
+                )
+                + "</span>"
+                );
+              }
+            }
+            if ((obj.children_criteria != null)) {
+              items = [];
+              if (obj.children_criteria.length > 0){
+                conjunction = obj.derivation_operator == 'XPRODUCT' ? 'and' : 'or';
+                items.push({'conjunction': conjunction, 'items': obj.children_criteria, 'negation':null})
+              }
+            } else {
+              if (obj.temporal_references) {
+                items = $.map(obj.temporal_references,
+                  function(temporal_reference){
+                    return {
+                      conjunction: temporal_reference.type,
+                      items: [temporal_reference.reference],
+                      temporal: true,
+                      title: (
+                        (
+                          temporal_reference.range ? popHealth.textForRange(temporal_reference.range,true) : ''
+                        ) +
+                        ({
+                          DURING:'During',SBS:'Starts Before Start of',SAS:'Starts After Start of',SBE:'Starts Before End of',
+                          SAE:'Starts After End of',EBS:'Ends Before Start of',EAS:'Ends After Start of',EBE:'Ends Before End of',
+                          EAE:'Ends After End of',SDU:'Starts During',EDU:'Ends During',ECW:'Ends Concurrent with',
+                          SCW:'Starts Concurrent with',CONCURRENT:'Concurrent with'
+                        })[temporal_reference.type] +
+                        (temporal_reference.reference == 'MeasurePeriod' ? ' Measure Period' : '')
+                      )
+                    }
+                  }
+                );
+              }
+              if (!obj.temporal){
+                elemParent = $("#ph_tmpl_paramGroup").tmpl().appendTo(elemParent).find(".paramItem:last");
+                $('#ph_tmpl_data_criteria_logic').tmpl(obj).appendTo(elemParent);
+              }
+            }
+          }
+          var neg = obj.negation || false;
+          if ($.isArray(items)) {
+            conjunction = obj['conjunction'];
+            if (items.length > 1 && !(container != null)) {
+              elemParent = $('#ph_tmpl_paramGroup').tmpl().appendTo(elemParent).find(".paramItem:last");
+            }
+            if (neg) {
+              $(elemParent).append("<span class='not'>not</span>");
+            }
+            return $.each(items, function(i, node) {
+              var next;
+              if (node.temporal) {
+                $(elemParent).append("<span class='" + node.conjunction + "'>" + node.title + "</span>");
+              }
+              addParamItems(node, elemParent);
+              if (i < items.length - 1 && !node.temporal) {
+                next = items[i + 1];
+                if (!conjunction) {
+                  conjunction = node.conjunction;
+                }
+                return $(elemParent).append("<span class='" + conjunction + "'>" + conjunction + "</span>");
               }
             });
-        } 
-        else {
-          // we dont have a nested measure clause, add the item to the bottom of the list
-          if (!elemParent.hasClass("paramItem")) {
-            elemParent = $("#ph_tmpl_paramGroup").tmpl({}).appendTo(elemParent).find(".paramItem:last");
-          }
-          $("#ph_tmpl_paramItem").tmpl(obj).appendTo(elemParent);
+          };
+        } // end addParamItems
+
+        if (data.population) {
+          elemParent = $("#ph_tmpl_paramGroup").tmpl({}).appendTo("#eligibilityMeasureItems").find(".paramItem:last");
+          addParamItems(data.population,elemParent,elemParent);
+          elemParent.parent().addClass("population");
         }
-      } // end addParamItems
 
-      if (data.population) {
-        elemParent = $("#ph_tmpl_paramGroup").tmpl({}).appendTo("#eligibilityMeasureItems").find(".paramItem:last");
-        addParamItems(data.population,elemParent,elemParent);
-        elemParent.parent().addClass("population");
-      }
+        if (!$.isEmptyObject(data.denominator)) {
+          $("#eligibilityMeasureItems").append("<span class='and'>and</span>");
+          addParamItems(data.denominator,$("#eligibilityMeasureItems"));
+        }
 
-      if (!$.isEmptyObject(data.denominator)) {
-        $("#eligibilityMeasureItems").append("<span class='and'>and</span>");
-        addParamItems(data.denominator,$("#eligibilityMeasureItems"));
-      }
+        if (data.numerator) {
+          addParamItems(data.numerator,$("#outcomeMeasureItems"));
+        }
 
-      if (data.numerator) {
-        addParamItems(data.numerator,$("#outcomeMeasureItems"));
-      }
-
-    }
+        if (!$.isEmptyObject(data.exclusions)) {
+          addParamItems(data.exclusions,$("#exclusionMeasureItems"));
+          $("#exclusionMeasureItems").hide();
+          $("#exclusionPanel").show();
+        }
+     }
+    
 })( jQuery );
 
