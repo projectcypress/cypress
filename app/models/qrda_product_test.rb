e17485be75ff0f2d5bbf937991ee497c8cf6fc42
Adam B. Goldstein <abgoldstein@gmail.com>
2012-10-09 14:39:29 -0400

    QRDA Product Tests now generate Cat 1 patients.

--- app/models/qrda_product_test.rb
+++ app/models/qrda_product_test.rb
@@ -1,21 +1,19 @@
 class QRDAProductTest < ProductTest
-  
-  
-  #after the test is created generate the population
   after_create :generate_population
   
   def generate_population
-    # To-DO change this over to sue test patient generator from bonnie
-    # give it the set of data criteria required for the selected measures and generate 
-    # a single patient record for the measures
+    measure_needs = {}
+    measure_value_sets = {}
     self.measures.each do |measure|
-      Record.where({test_id: nil, measure_id: measure.id, type: :qrda}).each do |rec|
-        cloned = rec.clone
-        cloned.test_id = self.id
-        cloned.save
-      end
+      measure_needs[measure.id] = measure.data_criteria.map{|dc| HQMF::DataCriteria.from_json(dc.keys.first, dc.values.first)}
+      measure_value_sets[measure.id] = measure.value_sets
     end
-    
+      
+    patients = HQMF::Generator.generate_qrda_patients(measure_needs, measure_value_sets)
+    patients.each do |measure, patient|
+      patient.test_id = self.id
+      patient.save
+    end
   end
   
   def execute(params)
@@ -27,12 +25,9 @@ class QRDAProductTest < ProductTest
     te.save
     (te.count_errors > 0) ? te.failed : te.pass
     te
-    
   end
   
   def self.product_type_measures
     Measure.top_level
   end
-
- 
 end
