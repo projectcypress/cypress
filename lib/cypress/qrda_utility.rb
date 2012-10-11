40ad29efcb2920a0dfec1db3e744da9db34ef269
Rob Dingwell <bobd@mitre.org>
2012-10-11 16:48:54 -0400

    removing dead code

--- lib/cypress/qrda_utility.rb
+++ lib/cypress/qrda_utility.rb
@@ -50,23 +50,6 @@ module Cypress
       end
     end
 
-
-    # Extract and return measure results from a QRDA CATIII document and add to the reported results
-    # for this test.
-    def self.extract_results(doc)
-      doc = (doc.kind_of? String )? Nokogiri::XML(doc) : doc
-      doc.root.add_namespace_definition("cda", "urn:hl7-org:v3")
-      #the nodes we want will have a child "templateId" with root = 2.16.840.1.113883.10.20.27.3.1
-      xpath_measures = '/cda:ClinicalDocument/cda:component/cda:structuredBody/cda:component/cda:section/cda:entry/cda:organizer/cda:templateId[@root = "2.16.840.1.113883.10.20.27.3.1"]/parent::*'
-      
-      results ||= {}
-      result_nodes = doc.xpath(xpath_measures)
-      result_nodes.each do |result_node|
-        results.merge!(extract_measure_results(result_node))
-      end
-      results
-    end
-
     #takes a document and a list of 1 or more id hashes, e.g.:
     #[{measure_id:"8a4d92b2-36af-5758-0136-ea8c43244986", set_id:"03876d69-085b-415c-ae9d-9924171040c2", ipp:"D77106C4-8ED0-4C5D-B29E-13DBF255B9FF", den:"8B0FA80F-8FFE-494C-958A-191C1BB36DBF", num:"9363135E-A816-451F-8022-96CDA7E540DD"}]
     #returns nil if nothing matching is found 
@@ -138,217 +121,11 @@ module Cypress
 
 
 
-
+    # Nothing to see here - Move along
     def self.validate_cat3(file)
 	    []
     end
 
-
-    
-
-    #checks if a hash of values has a value for every field in a key
-    def self.check_result(keys, values)
-      keys.each do |name, ref|
-        #skip special case of measure ids
-        next if name == :measure_id || name == :set_id
-        return false if values[name].nil?
-      end
-      true
-    end
-
-    #extract all the data from a measure node, keys is an optional list of
-    #key hashes. if not given, will create all possible key permutations based on the measure data
-    def self.extract_measure_results(measure_node, keys = nil)
-      xpath_measure_id = 'cda:reference/cda:externalDocument/cda:id'
-      xpath_set_id = 'cda:reference/cda:externalDocument/cda:setId'
-
-      measure_data = get_measure_data(measure_node)
-      measure_data[:measure_id] = measure_node.at_xpath(xpath_measure_id)['root']
-     # measure_data[:set_id] = measure_node.at_xpath(xpath_set_id)['root']
-      keys = generate_keys(measure_data) if keys.nil?
-      results = populate_results(keys, measure_data)
-
-      ## this is here to map this to what is really needed.  THe code below that handles this needs
-      # to be refactored and is just way more complicated than it needs to be.  This is a simple stop
-      # gap without needing to go in and figure out what that is doing.
-      res = {}
-      results.each_pair do |k,v|
-        code_mapping = {'NUMER' => :num, 'DENOM' => :den,'IPP' => :ipp, 'MSRPOPL' => :msr_popl , 
-                      'NUMEX' => :numex, 'DENEX' => :denex,'EXCEP' => :excep, "stratification" => :strata}
-        nkey = {}             
-        code_mapping.each_pair do |code,name|
-          k.delete(:set_id)
-          if k[name]
-            k[code] = k[name]
-            k.delete(name)
-          end
-        end
-
-        {den: :denominator, num: :numerator}.each_pair do |old,n|
-          if v[old]
-            v[n] = v[old]
-            v.delete(old)
-          end
-
-        end
-
-      end
-
-      results
-    end
-
-
-
-    #given a set of keys and the measure data, build a hash mapping the keys to the right data
-    def self.populate_results(keys, mdata)
-      results = {}
-
-      keys.each do |k|
-        result = {}
-        k.each do |field, reference|
-          load_value(mdata, k, field, result)
-        end
-        load_value(mdata, k, :excep, result)
-        load_value(mdata, k, :denex, result)
-        load_value(mdata, k, :numex, result)
-     
-        if result[:ipp]
-          strata_hash = mdata[:ipp][k[:ipp]][:strata]
-        elsif result[:msr_popl]
-          strata_hash = mdata[:msr_popl][k[:msr_popl]][:strata]
-          continuous_values = mdata[:msr_popl][k[:msr_popl]][:continuous_values]
-        end
-
-        if strata_hash
-          result[:strata] = strata_hash[k[:strata]]
-        end
-
-        #This code will alter the key to add all the continuous values
-        if continuous_values
-          continuous_values.each do |reference,values|
-            code = values['code'].to_sym
-            k[code] = reference
-            result[code] = values['value']
-          end
-        end
-
-        results[k] = result
-      end
-      results
-    end
-
-    def self.load_value(mdata, key, field, result)
-      exceptions = [:excep,:denex,:numex]
-      ignore = [:measure_id, :set_id, :strata]
-
-      if ignore.include?(field)
-        return
-      elsif exceptions.include?(field)
-        result[field] = mdata[field].values[0][:value] if mdata[field]
-      else
-        result[field] = mdata[field][key[field]][:value] if mdata[field] && mdata[field][key[field]]
-      end
-    end
-
-    #given the measure data pulled from the QRDA, return hash of arrays of reference ids for the fields
-    #assumes only measure populations and initial patient populations will have strata,
-    #and a measure will only have at most 1 measure population and 1 initial patient population
-    #TODO build this stuff when getting data from qrda?
-    def self.get_ids(measure_data)
-      #delete the measure_id and set_id because they dont follow the same patterns as the rest of the data
-      ids = {measure_id:[measure_data.delete(:measure_id)], set_id:[measure_data.delete(:set_id)]}
-      measure_data.each do |name, data|
-        if name == :ipp || name == :msr_popl
-          if data.nil?
-            entry = {id:[''], strata:['']}
-          else
-            data.each do |ref, d|
-              entry = {id: [ref], strata: ['']} 
-              if d[:strata]
-                entry[:strata] = []
-                d[:strata].each do |ref_s, data_s|
-                  entry[:strata] << ref_s
-                end
-              end
-            end
-          end
-        else
-          if data.nil?
-            entry = ['']
-          else
-            entry = []
-            data.each do |ref, d|
-                entry << ref
-            end
-          end
-        end
-        ids[name] = entry
-      end
-      ids
-    end
-
-    def self.generate_keys(mdata)
-      key_fields = [:measure_id, :set_id, :ipp, :msr_popl, :den,  :num, :denex, :numex, :excep,:strata]
-      keys = []
-       
-      ids = get_ids(mdata)
-      ipp_ids = [ids[:measure_id], ids[:set_id], ids[:ipp][:id], [''], ids[:den], ids[:num], ids[:denex], ids[:numex],  ids[:excep], ids[:ipp][:strata]]
-      msr_popl_ids = [ids[:measure_id], ids[:set_id], [''], ids[:msr_popl][:id], ids[:den], ids[:num],ids[:denex], ids[:numex],  ids[:excep], ids[:msr_popl][:strata]]
-
-      permutations = generate_permutations(ipp_ids) + generate_permutations(msr_popl_ids)
-      permutations.each do |p|
-        new_key = {}
-        key_fields.each do |field|
-          val = p.shift
-          new_key[field] = val if val!=""
-        end
-        keys << new_key
-      end
-      keys
-    end
-
-    def self.generate_permutations(ref_array)
-      if ref_array.size == 1
-        return ref_array[0]
-      end
-      top = ref_array.shift
-      return top.product(generate_permutations(ref_array)).map {|x| x.flatten}
-    end
-
-    def self.get_measure_data(measure_node)
-      code_mapping = {'NUMER' => :num, 'DENOM' => :den,'IPP' => :ipp, 'MSRPOPL' => :msr_popl , \
-                      'NUMEX' => :numex, 'DENEX' => :denex,'EXCEP' => :excep}
-
-      measure_data = {}
-      code_mapping.each do |code, name|
-        entry_list = {}
-        xpath_value    = 'cda:observation/cda:value'
-        xpath_expected = 'cda:observation/cda:referenceRange/cda:observationRange/cda:value'
-        xpath_observations = 'cda:component/cda:observation/cda:value[@code = "'+ code +'"]/parent::*'
-        xpath_entryRelationship = 'cda:value[@code = "'+ code +'"]/following-sibling::cda:entryRelationship'
-
-        observation_nodes = measure_node.xpath(xpath_observations)
-        observation_nodes.each do |n|
-          entry_node = n.at_xpath(xpath_entryRelationship)
-          value_node = entry_node.at_xpath(xpath_value)
-          expected_value_node = entry_node.at_xpath(xpath_expected)
-
-          entry = {}
-          reference = get_reference(n)
-          list_stratum = get_strata(n)
-          list_continuous_val = get_continuous_values(n)
-          entry[:value] = convert_value(value_node) if value_node
-          entry[:strata] = list_stratum if list_stratum
-          entry[:continuous_values] = list_continuous_val if list_continuous_val
-          
-          entry_list[reference] = entry
-        end
-        entry_list = nil if entry_list.empty?
-        measure_data[name] = entry_list
-      end
-      binding
-      measure_data
-    end
 
     #get all continuous values within a measure data (numerator, denominator, etc) node
     def self.get_continuous_values(node)
@@ -370,18 +147,6 @@ module Cypress
       list_values
     end
 
-    def self.get_strata(node)
-      xpath_observations = 'cda:entryRelationship[@typeCode="COMP"]/cda:observation/cda:templateId[@root = "2.16.840.1.113883.10.20.27.3.4"]/parent::*'
-
-      observation_nodes = node.xpath(xpath_observations)
-      list_stratum = {}
-      observation_nodes.each do |n|
-        list_stratum[get_reference(n)] = get_aggregate_count(n)
-      end
-
-      list_stratum = nil if list_stratum.empty?
-      list_stratum
-    end
 
     #given an observation node with an aggregate count node, return the reported and expected value within the count node
     def self.get_aggregate_count(node)
@@ -392,13 +157,6 @@ module Cypress
       value
     end
 
-    #given an observation node with a reference, gets the reference 
-    def self.get_reference(node)
-      xpath_reference = 'cda:reference/cda:externalObservation/cda:id'
-      reference_node  =  node.at_xpath(xpath_reference)
-      reference = reference_node['root'] if reference_node
-      reference ||= ''
-    end
 
     #convert numbers in value nodes to Int / Float as necessary TODO add more types other than 'REAL'
     def self.convert_value(value_node)
