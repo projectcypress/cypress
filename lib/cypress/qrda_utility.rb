a2245eb386c564e0e2d24a48468a6f2876dfc391
Michael Nosal <mnosal@mitre.org>
2012-11-06 15:01:44 -0500

    fix spelling error in comment

--- lib/cypress/qrda_utility.rb
+++ lib/cypress/qrda_utility.rb
@@ -43,7 +43,7 @@ module Cypress
           end
 
           # Look in the document to see if there is an entry stating that it is reporting on the given measure
-          # we will be a bit lieniant and look for both the version specific id and the non version specific ids
+          # we will be a bit lenient and look for both the version specific id and the non version specific ids
 
           if !doc.at_xpath("//cda:organizer[./templateId[@root='2.16.840.1.113883.10.20.24.3.98']]/cda:reference[@typeCode='REFR']/cda:externalDocument[@classCode='DOC']/cda:id[#{translate("@root")}='#{measure.hqmf_id.upcase}']")
             file_errors << ExecutionError.new(:location=>"/", :msg_type=>"error", :message=>"Document does not state it is reporting measure #{measure.hqmf_id}  - #{measure.name}")
