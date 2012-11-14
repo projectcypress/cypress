fb348e1dcfdd87ca5ace3db2304101d1d6d1de08
Rob Dingwell <bobd@mitre.org>
2012-11-14 16:12:31 -0500

    putting some code back in place that was removed but is actually used

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,5 +1,25 @@
 module PatientsHelper
   
+  
+  def race(patient)
+     if patient.race.kind_of? String
+       return patient.race
+     end
+     
+     r = Race.from_code(patient.race["code"]).first if patient.race
+     r = r || {}
+     r["name"] || ""
+  end
+  
+  def ethnicity(patient)
+    if patient.ethnicity.kind_of? String
+      return patient.ethnicity
+    end
+    e = Ethnicity.from_code(patient.ethnicity["code"]).first if patient.ethnicity
+    e = e || {}
+    e["name"] || ""
+  end
+  
   def result_to_markup(included)
     if included
       '<img src="/assets/pass.png"/>'
