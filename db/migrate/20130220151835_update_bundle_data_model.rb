00f2e1c10499cef863e185569bf362d024972139
Rob Dingwell <bobd@mitre.org>
2013-02-28 12:07:36 -0500

    removing indexing from app start- causes issues with migration taking far to long to complete

--- db/migrate/20130220151835_update_bundle_data_model.rb
+++ db/migrate/20130220151835_update_bundle_data_model.rb
@@ -17,6 +17,7 @@ class UpdateBundleDataModel < Mongoid::Migration
           m.save
         end
       end
+      puts "Updated Measures"
 
       Record.where({:test_id=>nil, :bundle_id=>nil}).each do |m|
         if m["bundle"]
@@ -26,26 +27,35 @@ class UpdateBundleDataModel < Mongoid::Migration
         end
       end
 
-      Result.where({:test_id=> nil, :bundle_id=>nil}).each do |m|
-          m["bundle_id"] = bundle_id
-          m.save
-      end
+      puts "Updated Records"
+      Result.where({:test_id=> nil, :bundle_id=>nil}).update_all({"bundle_id" => bundle_id})
+      # Result.where({:test_id=> nil, :bundle_id=>nil}).to_a.each do |m|
+      #     m["bundle_id"] = bundle_id
+      #     m.save
+      # end
 
-      ProductTest.each do |pt|
-        pt[:bundle_id] = bundle_id
-        pt.save
-      end
+      puts "Updated Patient cache"
+      ProductTest.where({}).update_all({"bundle_id" => bundle_id})
 
-      HealthDataStandards::SVS::ValueSet.where({:bundle_id => nil}).each do |vs|
-        vs[:bundle_id] = bundle_id
-        vs.save
-      end
+      puts "updated product tests"
+      HealthDataStandards::SVS::ValueSet.where({:bundle_id => nil}).update_all({"bundle_id" => bundle_id})
 
- 
+      puts "updated valuesets"
       start_date = 1.year.ago(Time.at(bundle.effective_date)).to_i 
       bundle[:measure_period_start] = start_date
       bundle.save
     end
+
+     puts "Indexing database"
+    ::Rails.application.eager_load!
+
+    Mongoid.models.each do |model|
+      next if model.index_options.empty?
+      unless model.embedded?
+        model.create_indexes
+      end
+    end
+    puts "Finished"
   end
   # there is no going back from this 
   def self.down
