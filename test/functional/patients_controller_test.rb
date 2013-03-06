9a03d9ee9084e9f5ea83e64bd87a71a2f01b4923
Rob Dingwell <bobd@mitre.org>
2013-03-06 17:22:20 -0500

    updating tests to remove fistures that were causing issues with testing. Updated .gitignore file to include bundles directory as that is where the new rake task saves bundles to and they should not be committed to the repo

--- test/functional/patients_controller_test.rb
+++ test/functional/patients_controller_test.rb
@@ -4,8 +4,7 @@ class PatientsControllerTest < ActionController::TestCase
 include Devise::TestHelpers
 
   setup do
-    collection_fixtures('users')
-    collection_fixtures('query_cache', 'test_id')
+     collection_fixtures('query_cache', 'test_id')
     collection_fixtures('bundles', '_id')
     collection_fixtures('measures',"_id",'bundle_id')
     collection_fixtures('products','_id','vendor_id')
