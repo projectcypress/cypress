239baa5c4ea72124c3d16d30f7831c2c6e98e592
Rob Dingwell <bobd@mitre.org>
2013-03-12 15:48:54 -0400

    adding initial ability to upload zip of qrda cat 1 docs

--- test/unit/tests/calculated_product_test_test.rb
+++ test/unit/tests/calculated_product_test_test.rb
@@ -3,7 +3,7 @@ class CalculatedProductTestTest < ActiveSupport::TestCase
   setup do
     
     collection_fixtures('test_executions', '_id', "product_test_id")
-    collection_fixtures('product_tests', '_id','bundle_id')
+    collection_fixtures('product_tests', '_id','bundle_id', 'product_id', 'user_id')
     collection_fixtures('bundles','_id')
     collection_fixtures('measures','_id','bundle_id')
   end
@@ -20,4 +20,15 @@ class CalculatedProductTestTest < ActiveSupport::TestCase
     
    end
   
+
+   test "Generate QRDA Test" do
+    pt1 = ProductTest.find("4f58f8de1d41c851eb000999")
+    qrda = pt1.generate_qrda_cat1_test
+    assert_equal QRDAProductTest , qrda.class 
+    assert_equal pt1.measure_ids , qrda.measure_ids
+    assert_equal pt1.effective_date ,qrda.effective_date
+    assert_equal pt1.bundle_id, qrda.bundle_id
+    assert_equal pt1.product_id ,qrda.product_id
+    assert_equal pt1.user_id,  qrda.user_id
+  end
 end
