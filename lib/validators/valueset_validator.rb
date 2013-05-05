aa1c05636b6c7e77b736fde412032926ad0278b4
Rob <bobd@MM197768-pc.local>
2013-05-05 16:28:34 -0400

    implementing and testing valueset validator

--- lib/validators/valueset_validator.rb
+++ lib/validators/valueset_validator.rb
@@ -1,42 +1,45 @@
 module Validators
 	class ValuesetValidator
+		 attr_accessor :bundle
 		 def initialize(bundle)
-
+		 	@bundle = bundle
 		 end
 
 			def validate(document,data={})
 				doc = (document.kind_of? Nokogiri::XML::Document)? document : Nokogiri::XML(document.to_s)
 				doc.root.add_namespace_definition("cda", "urn:hl7-org:v3")
-				doc.root.add_namespace_definition("stdc", "urn:hl7-org:stdc")
+				doc.root.add_namespace_definition("sdtc", "urn:hl7-org:sdtc")
 				errors = []
 				# get all of the valueset items for the document
-				sdtc_values = doc.xpath("//[count(@stdc:valueset) = 1]")
+				sdtc_values = doc.xpath("//*[@sdtc:valueSet]")
+
 				sdtc_values.each do |node|
-					oid = node.at_xpath("@stdc:valueset")
+					oid = node.at_xpath("@sdtc:valueSet")
 					code = node.at_xpath("@code")
 					code_system = node.at_xpath("@codeSystem")
-					vs = bundle.valuesets.where({"oid" => oid}).first
+					vs = bundle.value_sets.where({"oid" => oid}).first
 					if vs.nil?
 						errors << Cypress::ValidationError.new(
-                :message => "The valueset #{oid} declared in the document cannot be found",
-                :validator => "",
-                :validator_type => :xml_validation,
-                :msg_type=>(data[:msg_type] || :error),
-                :file_name => data[:file_name],
-                location: node.path
-              )
-					elsif vs.concepts.where({"code" => code, "codeSystem"=>code_system}).count() > 0
+			                :message => "The valueset #{oid} declared in the document cannot be found",
+			                :validator => "",
+			                :validator_type => :xml_validation,
+			                :msg_type=>(data[:msg_type] || :error),
+			                :file_name => data[:file_name],
+			                location: node.path
+			              )
+					elsif vs.concepts.where({"code" => code, "code_system"=>code_system}).count() == 0
 						errors <<  Cypress::ValidationError.new(
-                :message => "The code #{code} in codeSystem #{code_system} cannot be found in the declared valueset #{oid} ",
-                :validator => "",
-                :validator_type => :xml_validation,
-                :msg_type=>(data[:msg_type] || :error),
-                :file_name => data[:file_name],
-                location: node.path
-              )
+			                :message => "The code #{code} in codeSystem #{code_system} cannot be found in the declared valueset #{oid} ",
+			                :validator => "",
+			                :validator_type => :xml_validation,
+			                :msg_type=>(data[:msg_type] || :error),
+			                :file_name => data[:file_name],
+			                location: node.path
+			              )
 					end
 
 				end
+				errors
 		 end
 	end
 end
---
+++ test/unit/lib/validation/valueset_validator_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+class ValuesetValidatorTest < ActiveSupport::TestCase
+
+   setup do
+ 
+    collection_fixtures('bundles', '_id')
+    collection_fixtures('health_data_standards_svs_value_sets', '_id','bundle_id')
+    @bundle = Bundle.find("4fdb62e01d41c820f6000001")
+    @validator = Validators::ValuesetValidator.new(@bundle)
+   end
+
+   test "Should produce errors for unknown valuesets or values not found in vs" do
+      xml = File.open("./test/fixtures/validators/valuesets/bad.xml","r") do |f| f.read() end
+      errors = @validator.validate(xml)
+      assert_equal 2,  errors.length, "File should contain 2 errors"
+   end
+
+   test "Should not produce errors for files for all " do 
+      xml = File.open("./test/fixtures/validators/valuesets/good.xml","r") do |f| f.read() end
+      errors = @validator.validate(xml)
+      assert_equal  0,  errors.length, "File should not contain any errors"
+   end
+   
+end
