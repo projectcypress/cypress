528e58568542ebca1ee9ee4d6d5502a33dfc4baf
Rob Dingwell <bobd@mitre.org>
2013-08-26 12:25:39 -0400

    fixing deprecation warnings while running tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ ENV["RAILS_ENV"] = "test"
 require_relative "./simplecov"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'mocha'
+require 'mocha/setup'
 
 require 'active_support/testing/pending'
 
