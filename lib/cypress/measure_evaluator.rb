8d0ee80411386b802d782b9cd8de0ad6492bdbb9
Samuel Sayer <ssayer@mitre.org>
2013-11-25 15:57:56 -0500

    pass test bundle id to QME

--- lib/cypress/measure_evaluator.rb
+++ lib/cypress/measure_evaluator.rb
@@ -20,7 +20,7 @@ module Cypress
         dictionary = Cypress::MeasureEvaluator.generate_oid_dictionary(measure, t.bundle)
         qr = QME::QualityReport.new(measure["hqmf_id"], measure.sub_id, 'effective_date' => t.effective_date,
                                      'test_id' => t.id, 'filters' => options['filters'], "oid_dictionary"=>dictionary,
-                                     'enable_logging' => true , "enable_rationale" =>true)
+                                     'enable_logging' => true , "enable_rationale" =>true, 'bundle_id' => t.bundle.id)
         t.status_message = " Calculating measure #{index} of #{measure_count} - #{measure.display_name}"
         t.save
         qr.calculate(false) 
