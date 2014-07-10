248f501dfb3763f2fcbd705c64ed8d5b0552bbd2
Michael O'Keefe <mokeefe@mitre.org>
2014-07-10 14:09:31 -0400

    Created a test for the uploading of files with non-ascii characters, along with some necessary fixtures

--- test/functional/test_executions_controller_test.rb
+++ test/functional/test_executions_controller_test.rb
@@ -2,20 +2,21 @@ require 'test_helper'
 
 class TestExecutionsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
-  
+
   setup do
 
     collection_fixtures('test_executions', '_id', "product_test_id")
     collection_fixtures('measures','bundle_id')
     collection_fixtures('products','_id','vendor_id')
     collection_fixtures('product_tests','_id','product_id','bundle_id')
+    collection_fixtures('vendors', '_id')
     collection_fixtures('users',"_id", "vendor_ids")
     collection_fixtures('records', '_id','bundle_id')
-    
+
     @user = User.where({:first_name => 'bobby', :last_name => 'tables'}).first
     sign_in @user
   end
-  
+
 
 
   test "show" do
@@ -33,14 +34,14 @@ class TestExecutionsControllerTest < ActionController::TestCase
     assert_response 302
 
     assert_equal ex_count+1 , TestExecution.where(:product_test_id => pt1.id).count, "SHould increment the test executuon count"
-    
+
   end
 
   test "destroy" do
     count = TestExecution.count
     test_execution = TestExecution.first
-    post(:destroy, {:id => test_execution.id})  
-    
+    post(:destroy, {:id => test_execution.id})
+
     assert_response 302
 
     assert_equal count-1, TestExecution.count, "SHould decrament the test execution count"
@@ -48,5 +49,20 @@ class TestExecutionsControllerTest < ActionController::TestCase
 
   test "download" do
      get :download, {id: TestExecution.first}
+  end
+
+  test "create with zip with non-ascii chars" do
+    pt1 = ProductTest.find("51703a883054cf843900ffff")
+    ex_count = TestExecution.where(:product_test_id => pt1.id).count
+
+    qrda = Rack::Test::UploadedFile.new(File.join(Rails.root, 'test/fixtures/qrda/cat_1/qrda_non_ascii_filenames.zip'), "application/zip")
+    params = {product_test_id: pt1.id, test_execution: {results: qrda}}
+    post(:create, params)
+    assert_response 302
+
+    assert_equal ex_count+1 , TestExecution.where(:product_test_id => pt1.id).count, "Should increment the test execution count"
+
+    get :show, {id: TestExecution.find_by(:product_test_id => pt1.id)}
+    assert_response 200
   end
 end
