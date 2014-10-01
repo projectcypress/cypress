9ba13eb2949aedb904048cfaec3c7e1b2ec13957
Samuel Sayer <ssayer@mitre.org>
2014-10-01 19:03:39 -0400

    update fixtures to match qrda errata release, don't run warning phase of new schematrons

--- lib/cypress/qrda_utility.rb
+++ lib/cypress/qrda_utility.rb
@@ -44,13 +44,13 @@ module Cypress
 
         # Valdiate aginst the generic schematron rules
         file_errors.concat QRDA_CAT1_SCHEMATRON_ERROR_VALIDATOR.validate(doc, {phase: :errors, msg_type: :error, file_name: name})
-        file_errors.concat QRDA_CAT1_SCHEMATRON_WARNING_VALIDATOR.validate(doc, {phase: :errors, msg_type: :warning, file_name: name })
+        # file_errors.concat QRDA_CAT1_SCHEMATRON_WARNING_VALIDATOR.validate(doc, {phase: :warnings, msg_type: :warning, file_name: name })
 
         # validate the mesure specific rules
         measures.each do |measure|
           # Look in the document to see if there is an entry stating that it is reporting on the given measure
           # we will be a bit lieniant and look for both the version specific id and the non version specific ids
-          if !doc.at_xpath("//cda:organizer[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.98']]/cda:reference[@typeCode='REFR']/cda:externalDocument[@classCode='DOC']/cda:id[#{translate("@root")}='#{measure.hqmf_id.upcase}']")
+          if !doc.at_xpath("//cda:organizer[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.98']]/cda:reference[@typeCode='REFR']/cda:externalDocument[@classCode='DOC']/cda:id[#{translate("@extension")}='#{measure.hqmf_id.upcase}']")
             file_errors << ExecutionError.new(:location=>"/", :msg_type=>"error", :message=>"Document does not state it is reporting measure #{measure.hqmf_id}  - #{measure.name}", :validator=>"Measure Declaration Check")
 
           end
@@ -183,7 +183,7 @@ module Cypress
       file_errors.concat QRDA_SCHEMA_VALIDATOR.validate(doc, {msg_type: :error}) 
       # Valdiate aginst the generic schematron rules
       file_errors.concat QRDA_CAT3_SCHEMATRON_ERROR_VALIDATOR.validate(doc, {phase: :errors, msg_type: :error, file_name: name})
-      file_errors.concat QRDA_CAT3_SCHEMATRON_WARNING_VALIDATOR.validate(doc, {phase: :errors, msg_type: :warning, file_name: name })
+      # file_errors.concat QRDA_CAT3_SCHEMATRON_WARNING_VALIDATOR.validate(doc, {phase: :warnings, msg_type: :warning, file_name: name })
       file_errors
     end
 
