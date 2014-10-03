ead425013504ddea27fd10cd7188b729d16e45ca
Michael O'Keefe <mokeefe@mitre.org>
2014-10-03 10:21:20 -0400

    Refactored QRDA Cat1 and Cat3 files to validate either errors or warnings (errors by default)

--- lib/cypress/qrda_cat1_file.rb
+++ lib/cypress/qrda_cat1_file.rb
@@ -9,7 +9,7 @@ module Cypress
     # Validates a QRDA Cat I file.  This routine will validate the file against the CDA schema as well as the 
     # Generic QRDA Cat I schematron rules and the measure specific rules for each of the measures passed in.
     # THe result will be an Array of execution errors or an empty array if there were no errors.
-    def validate(measures=[], name="")
+    def validate(measures=[], name="", msg_type = :error)
 
       file_errors = []
        # validate that each file in the zip contains a valid QRDA Cat I document.
@@ -17,20 +17,25 @@ module Cypress
        # patient records to match agaist QRDA Cat I documents
        
        # First validate the schema correctness
-       file_errors.concat QRDA_SCHEMA_VALIDATOR.validate(@document, {msg_type: :error}) 
+       file_errors.concat QRDA_SCHEMA_VALIDATOR.validate(@document, {msg_type: msg_type}) 
+        
+        if (msg_type == :error)
+          # Valdiate aginst the generic schematron rules, for errors
+          file_errors.concat SCHEMATRON_ERROR_VALIDATOR.validate(@document, {phase: :errors, msg_type: :error, file_name: name})
 
-        # Valdiate aginst the generic schematron rules
-        file_errors.concat SCHEMATRON_ERROR_VALIDATOR.validate(@document, {phase: :errors, msg_type: :error, file_name: name})
-        # file_errors.concat QRDA_CAT1_SCHEMATRON_WARNING_VALIDATOR.validate(doc, {phase: :warnings, msg_type: :warning, file_name: name })
+          # validate the measure specific rules
+          measures.each do |measure|
+            # Look in the document to see if there is an entry stating that it is reporting on the given measure
+            # we will be a bit lenient and look for both the version specific id and the non version specific ids
+            if !@document.at_xpath("//cda:organizer[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.98']]/cda:reference[@typeCode='REFR']/cda:externalDocument[@classCode='DOC']/cda:id[#{translate("@extension")}='#{measure.hqmf_id.upcase}']")
+              file_errors << ExecutionError.new(:location=>"/", :msg_type=>"error", :message=>"Document does not state it is reporting measure #{measure.hqmf_id}  - #{measure.name}", :validator=>"Measure Declaration Check")
 
-        # validate the mesure specific rules
-        measures.each do |measure|
-          # Look in the document to see if there is an entry stating that it is reporting on the given measure
-          # we will be a bit lieniant and look for both the version specific id and the non version specific ids
-          if !@document.at_xpath("//cda:organizer[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.98']]/cda:reference[@typeCode='REFR']/cda:externalDocument[@classCode='DOC']/cda:id[#{translate("@extension")}='#{measure.hqmf_id.upcase}']")
-            file_errors << ExecutionError.new(:location=>"/", :msg_type=>"error", :message=>"Document does not state it is reporting measure #{measure.hqmf_id}  - #{measure.name}", :validator=>"Measure Declaration Check")
-
+            end
           end
+
+        else
+          # Valdiate aginst the generic schematron rules, for warnings
+          file_errors.concat QRDA_CAT1_SCHEMATRON_WARNING_VALIDATOR.validate(doc, {phase: :warnings, msg_type: :warning, file_name: name })
         end
 
         file_errors
