1b0d9438ac44ac6c5701e5f27e5a0c8b7067aa46
Andy Gregorowicz <andy@mitre.org>
2014-11-13 16:06:28 -0500

    Refactoring to break code into chunks and make things more readable

--- app/models/product_test.rb
+++ app/models/product_test.rb
@@ -148,72 +148,42 @@ class ProductTest
   end
 
 
-  def self.match_calculation_results(expected_result,reported_result)
-
+  def self.match_calculation_results(expected_result, reported_result)
     validation_errors = []
-    _ids = expected_result["population_ids"].dup
-    if reported_result.nil? || reported_result.keys.length <=1
-      message = "Could not find entry for measure #{expected_result["measure_id"]} with the following population ids "
-      message +=  _ids.inspect
-      validation_errors << ExecutionError.new(message: message, msg_type: :error, measure_id: expected_result["measure_id"] , stratification: _ids['stratification'], validator_type: :result_validation)
-      return validation_errors
-    end
+    measure_id = expected_result["measure_id"]
+    logger = -> (message, stratification) {
+      validation_errors << ExecutionError.new(message: message, msg_type: :error, measure_id: measure_id,
+                                              validator_type: :result_validation, stratification: stratification)
+    }
+
+    check_for_reported_results_population_ids(expected_result, reported_result, logger)
+    return validation_errors if validation_errors.present?
 
     _ids = expected_result["population_ids"].dup
     # remove the stratification entry if its there, not needed to test against values
     stratification = _ids.delete("stratification")
-
-
+    logger_with_stratification = -> (message) {logger.call(message, stratification)}
     _ids.keys.each do |pop_key|
+      if expected_result[pop_key].present?
+        check_population(expected_result, reported_result, pop_key, stratification, logger)
 
-
-      if !expected_result[pop_key].nil?
-
-        # only add the error that they dont match if there was an actual result
-        if !reported_result.empty? && !reported_result.has_key?(pop_key)
-          message = "Could not find value"
-          message += " for stratification #{stratification} " if stratification
-          message += " for Population #{pop_key}"
-          validation_errors << ExecutionError.new(message: message, msg_type: :error, measure_id: expected_result["measure_id"] , validator_type: :result_validation, stratification: stratification)
-        elsif (expected_result[pop_key] != reported_result[pop_key]) && !reported_result.empty?
-         err = "expected #{pop_key} #{_ids[pop_key]} value #{expected_result[pop_key]} does not match reported value #{reported_result[pop_key]}"
-         validation_errors << ExecutionError.new(message: err, msg_type: :error, measure_id: expected_result["measure_id"] , validator_type: :result_validation, stratification: stratification)
-        end
-           # Check supplemental data elements
+        # Check supplemental data elements
         ex_sup = (expected_result["supplemental_data"] || {})[pop_key]
         reported_sup  = (reported_result[:supplemental_data] || {})[pop_key]
         if stratification.nil? && ex_sup
 
-          sup_keys = ex_sup.keys.reject{|k| k == "" || k.nil?}
+          sup_keys = ex_sup.keys.reject(&:blank?)
           # check to see if we expect sup data and if they provide it a short circuit the rest of the testing
           # if they do not
           if sup_keys.length>0 && reported_sup.nil?
-              err = "supplemental data for #{pop_key} not found expected  #{ex_sup}"
-              validation_errors << ExecutionError.new(message: err, msg_type: :error, measure_id: expected_result["measure_id"] , validator_type: :result_validation, stratification: stratification)
+            err = "supplemental data for #{pop_key} not found expected  #{ex_sup}"
+            logger_with_stratification.call(err)
           else
             # for each supplemental data item (RACE, ETHNICITY,PAYER,SEX)
             sup_keys.each do |sup_key|
-
-
-              sup_value  = (ex_sup[sup_key] || {}).reject{|k,v| (k.nil? || k == "" || v.nil? || v=="" || v=="UNK")}
+              sup_value  = (ex_sup[sup_key] || {}).reject{|k,v| (k.blank? || v.blank? || v=="UNK")}
               reported_sup_value = reported_sup[sup_key]
-              if reported_sup_value.nil?
-                err = "supplemental data for #{pop_key} #{sup_key} #{sup_value} expected but was not found"
-               validation_errors << ExecutionError.new(message: err, msg_type: :error, measure_id: expected_result["measure_id"] , validator_type: :result_validation, stratification: stratification)
-              else
-                sup_value.each_pair do |code,value|
-                  if code != "UNK" && value != reported_sup_value[code]
-                   err = "expected supplemental data for #{pop_key} #{sup_key} #{code} value [#{value}] does not match reported supplemental data value [#{ reported_sup_value[code]}]"
-                   validation_errors << ExecutionError.new(message: err, msg_type: :error, measure_id: expected_result["measure_id"] , validator_type: :result_validation, stratification: stratification)
-                  end
-                end
-                reported_sup_value.each_pair do |code,value|
-                  if sup_value[code].nil?
-                   err = "unexpected supplemental data for #{pop_key} #{sup_key} #{code}"
-                   validation_errors << ExecutionError.new(message: err, msg_type: :error, measure_id: expected_result["measure_id"] , validator_type: :result_validation, stratification: stratification)
-                  end
-                end
-              end
+              check_supplemental_data(sup_value, reported_sup_value, pop_key, sup_key, logger_with_stratification)
             end
           end
         end
@@ -223,4 +193,49 @@ class ProductTest
     validation_errors
   end
 
+  private
+  def self.check_for_reported_results_population_ids(expected_result, reported_result, logger)
+    _ids = expected_result["population_ids"].dup
+    if reported_result.nil? || reported_result.keys.length <= 1
+      message = "Could not find entry for measure #{expected_result["measure_id"]} with the following population ids "
+      message +=  _ids.inspect
+      logger.call(message, _ids['stratification'])
+    end
+  end
+
+  private
+  def self.check_population(expected_result, reported_result, pop_key, stratification, logger)
+    # only add the error that they dont match if there was an actual result
+    if !reported_result.empty? && !reported_result.has_key?(pop_key)
+      message = "Could not find value"
+      message += " for stratification #{stratification} " if stratification
+      message += " for Population #{pop_key}"
+      logger.call(message, stratification)
+    elsif (expected_result[pop_key] != reported_result[pop_key]) && !reported_result.empty?
+     err = "expected #{pop_key} #{_ids[pop_key]} value #{expected_result[pop_key]} does not match reported value #{reported_result[pop_key]}"
+     logger.call(err, stratification)
+    end
+  end
+
+  private
+  def self.check_supplemental_data(expected_supplemental_value, reported_supplemantal_value,
+                                   population_key, supplemental_data_key, logger)
+    if reported_supplemantal_value.nil?
+      err = "supplemental data for #{population_key} #{supplemental_data_key} #{expected_supplemental_value} expected but was not found"
+      logger.call(err)
+    else
+      expected_supplemental_value.each_pair do |code,value|
+        if code != "UNK" && value != reported_supplemantal_value[code]
+         err = "expected supplemental data for #{population_key} #{supplemental_data_key} #{code} value [#{value}] does not match reported supplemental data value [#{ reported_supplemantal_value[code]}]"
+         logger.call(err)
+        end
+      end
+      reported_supplemantal_value.each_pair do |code,value|
+        if expected_supplemental_value[code].nil?
+         err = "unexpected supplemental data for #{population_key} #{supplemental_data_key} #{code}"
+         logger.call(err)
+        end
+      end
+    end
+  end
 end
