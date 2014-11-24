80b30813545dc6eae040634694db12ce0f39b265
Samuel Sayer <ssayer@mitre.org>
2014-11-24 16:32:24 -0500

    fix more whitespace

--- app/models/calculated_product_test.rb
+++ app/models/calculated_product_test.rb
@@ -142,30 +142,30 @@ class CalculatedProductTest < ProductTest
     logger_with_stratification = -> (message) {logger.call(message, stratification)}
     _ids.keys.each do |pop_key|
       if expected_result[pop_key].present?
-  check_population(expected_result, reported_result, pop_key, stratification, logger)
+        check_population(expected_result, reported_result, pop_key, stratification, logger)
 
-  # Check supplemental data elements
-  ex_sup = (expected_result["supplemental_data"] || {})[pop_key]
-  reported_sup  = (reported_result[:supplemental_data] || {})[pop_key]
-  if stratification.nil? && ex_sup
+        # Check supplemental data elements
+        ex_sup = (expected_result["supplemental_data"] || {})[pop_key]
+        reported_sup  = (reported_result[:supplemental_data] || {})[pop_key]
+        if stratification.nil? && ex_sup
 
-    sup_keys = ex_sup.keys.reject(&:blank?)
-    # check to see if we expect sup data and if they provide it a short circuit the rest of the testing
-    # if they do not
-    if sup_keys.length>0 && reported_sup.nil?
-      err = "supplemental data for #{pop_key} not found expected  #{ex_sup}"
-      logger_with_stratification.call(err)
-    else
-      # for each supplemental data item (RACE, ETHNICITY,PAYER,SEX)
-      sup_keys.each do |sup_key|
-        sup_value  = (ex_sup[sup_key] || {}).reject{|k,v| (k.blank? || v.blank? || v=="UNK")}
-        reported_sup_value = reported_sup[sup_key]
-        check_supplemental_data(sup_value, reported_sup_value, pop_key, sup_key, logger_with_stratification)
+          sup_keys = ex_sup.keys.reject(&:blank?)
+          # check to see if we expect sup data and if they provide it a short circuit the rest of the testing
+          # if they do not
+        if sup_keys.length>0 && reported_sup.nil?
+          err = "supplemental data for #{pop_key} not found expected  #{ex_sup}"
+          logger_with_stratification.call(err)
+        else
+          # for each supplemental data item (RACE, ETHNICITY,PAYER,SEX)
+          sup_keys.each do |sup_key|
+            sup_value  = (ex_sup[sup_key] || {}).reject{|k,v| (k.blank? || v.blank? || v=="UNK")}
+            reported_sup_value = reported_sup[sup_key]
+            check_supplemental_data(sup_value, reported_sup_value, pop_key, sup_key, logger_with_stratification)
+          end
+        end
       end
     end
   end
-      end
-    end
 
     validation_errors
   end
@@ -187,8 +187,8 @@ class CalculatedProductTest < ProductTest
       message += " for Population #{pop_key}"
       logger.call(message, stratification)
     elsif (expected_result[pop_key] != reported_result[pop_key]) && !reported_result.empty?
-     err = "expected #{pop_key} #{_ids[pop_key]} value #{expected_result[pop_key]} does not match reported value #{reported_result[pop_key]}"
-     logger.call(err, stratification)
+      err = "expected #{pop_key} #{_ids[pop_key]} value #{expected_result[pop_key]} does not match reported value #{reported_result[pop_key]}"
+      logger.call(err, stratification)
     end
   end
 
@@ -199,20 +199,18 @@ class CalculatedProductTest < ProductTest
       logger.call(err)
     else
       expected_supplemental_value.each_pair do |code,value|
-  if code != "UNK" && value != reported_supplemantal_value[code]
-   err = "expected supplemental data for #{population_key} #{supplemental_data_key} #{code} value [#{value}] does not match reported supplemental data value [#{ reported_supplemantal_value[code]}]"
-   logger.call(err)
-  end
+        if code != "UNK" && value != reported_supplemantal_value[code]
+          err = "expected supplemental data for #{population_key} #{supplemental_data_key} #{code} value [#{value}] does not match reported supplemental data value [#{ reported_supplemantal_value[code]}]"
+          logger.call(err)
+        end
       end
       reported_supplemantal_value.each_pair do |code,value|
-  if expected_supplemental_value[code].nil?
-   err = "unexpected supplemental data for #{population_key} #{supplemental_data_key} #{code}"
-   logger.call(err)
-  end
+        if expected_supplemental_value[code].nil?
+          err = "unexpected supplemental data for #{population_key} #{supplemental_data_key} #{code}"
+          logger.call(err)
+        end
       end
     end
   end
-
-
 
 end
