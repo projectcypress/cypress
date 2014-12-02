fc3ec54e21d244c66ab73c47603e540539ab4dba
Michael O'Keefe <mokeefe@mitre.org>
2014-12-02 11:22:18 -0500

    Added product and vendor collection_fixtures calls to patient_zipper_test

--- test/unit/lib/patient_zipper_test.rb
+++ test/unit/lib/patient_zipper_test.rb
@@ -7,6 +7,8 @@ class PatientZipperTest < ActiveSupport::TestCase
 
     collection_fixtures('records','_id','bundle_id')
     collection_fixtures('bundles', '_id')
+    collection_fixtures('vendors', '_id')
+    collection_fixtures('products', '_id')
     collection_fixtures('product_tests', '_id')
     collection_fixtures('test_executions', '_id')
     @patients = Record.where("gender" => "F")
