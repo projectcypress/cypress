5d07c4da91e1a83d8685291579b953dff301e828
Michael O'Keefe <mokeefe@mitre.org>
2015-02-01 11:55:17 -0500

    Removed app secret token from secret_token.rb (the Rails 3 and below way of doing it) and put it in secrets.yml instead.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cypress::Application.config.secret_token = 'b20ef35327795717ef9e5a105c885dd47fb2075dda5d7fed6fddbcbca338ec9edb1e2203f4d767dc7e6f8bf134d8d3235b388c07546f6495096206007f65c345'
