828ce344c31986fafa9a4dd6683042af9ee19cce
jawz101 <jordanwms@hotmail.com>
2015-02-18 17:58:38 -0600

    Update install_cypress.sh
    another fix

--- contrib/install_cypress.sh
+++ contrib/install_cypress.sh
@@ -565,7 +565,7 @@ apt-key list | grep -q "^pub[[:space:]]\+.*/${mongodb_key_id}"
 if [ $? -eq 0 ]; then
   success "already in keyring"
 else
-  apt-key adv --keyserver keyserver.ubuntu.com --recv "$mongodb_key_id" &> /dev/null
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv "$mongodb_key_id" &> /dev/null
   success_or_fail $? "added to keyring" "failed."
 fi
 # update package lists
