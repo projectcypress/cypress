47243f98d7408e4674ff180f9c257625348c6d5a
David Czulada <dczulada@gmail.com>
2015-03-17 16:18:51 -0400

    Update supplemental information test to allow for 0 values

--- app/models/calculated_product_test.rb
+++ app/models/calculated_product_test.rb
@@ -222,9 +222,11 @@ class CalculatedProductTest < ProductTest
         end
       end
       reported_supplemantal_value.each_pair do |code,value|
-        if expected_supplemental_value[code].nil?
-          err = "unexpected supplemental data for #{population_key} #{supplemental_data_key} #{code}"
-          logger.call(err)
+        if value > 0
+          if expected_supplemental_value[code].nil?
+            err = "unexpected supplemental data for #{population_key} #{supplemental_data_key} #{code}"
+            logger.call(err)
+          end
         end
       end
     end
