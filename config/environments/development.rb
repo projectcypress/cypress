3a6e62c11b76f1b29ade44142bd55a3d2f245dcc
Andrew Schreiber <schreiaj@gmail.com>
2015-10-13 13:06:38 -0400

    added devise and set up user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Config action mailer for devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
