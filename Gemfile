3a7e9af341d7717fb5b90e8b79f041412d0dcee8
thecristen <cristen@outlook.com>
2015-10-14 15:57:47 -0400

    use gems instead of importing things with bower

--- Gemfile
+++ Gemfile
@@ -14,21 +14,21 @@ gem 'quality-measure-engine', :git => "https://github.com/projectcypress/quality
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0.4'
-# Use Bootstrap
+# Dependencies for CMS Assets Framework
 gem 'bootstrap-sass', '~> 3.3.5'
+gem 'jquery-rails', '~> 4.0.4'
+gem 'jquery-ui-rails', '~> 5.0.5'
+gem 'modernizr-rails', '~> 2.7.1'
+gem 'font-awesome-rails'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use bower-rails to manage front-end assets
-gem 'bower-rails'
 # Bake the best breadcrumbs
 gem 'breadcrumbs_on_rails'
 
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
-# Use jquery as the JavaScript library
-gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,30 +1,10 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require turbolinks
-
-//// Run 'rake bower:install' to download dependencies from bower.json ////
-
-//// Core scripts needed for Assets Framework
-//= require jquery/dist/jquery.min
-//= require jquery-ui/ui/minified/jquery-ui.min
-//= require bootstrap-sass-official/assets/javascripts/bootstrap.min
-//= require modernizr/modernizr
-//= require assets.core
-//= require assets.responsive
-
-//// Needed for breadcrumbs
-//= require breadcrumb
-
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require assets_framework
 //= require_tree .
 
 $(function() {
