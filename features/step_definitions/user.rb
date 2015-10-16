1a4b05969b25dd8bc905a856c7d4bb6c15c1d3b0
Andrew Schreiber <aschreiber@VA00029-PC.local>
2015-10-16 11:11:02 -0400

    Step definitions for all tests, not all flash messages are displaying currently.

--- features/step_definitions/user.rb
+++ features/step_definitions/user.rb
@@ -3,8 +3,10 @@ Given(/^a user has an account$/) do
 end
 
 When(/^the user tries to log in with invalid information$/) do
-  incorrect_user = FactoryGirl.create(:user, email: "unauth@mitre.org")
-  login_as(incorrect_user, :scope => :user)
+  visit '/users/sign_in'
+  page.fill_in "Email", :with => "unauth@mitre.org"
+  page.fill_in "Password", :with => "incorrectPassword"
+  page.click_button "Log in"
 end
 
 Then(/^the user should see an log in error message$/) do
