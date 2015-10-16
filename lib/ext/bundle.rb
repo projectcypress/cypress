84876672d60716315721a1c7888047d988e17177
Rob Dingwell <bobd@mitre.org>
2015-10-16 11:29:46 -0400

    pulling over extensions to HDS models from master branch

---
+++ lib/ext/bundle.rb
@@ -0,0 +1,23 @@
+# yes this is a bit ugly as it is aliasing The bundle class but it
+# works for now until we can truley unify these items accross applications
+Bundle = HealthDataStandards::CQM::Bundle
+class Bundle
+  field :smoking_gun_capable, type: Boolean
+  has_many :product_tests
+  field :measures, type: Array
+
+  store_in collection: 'bundles'
+  def results
+    Result.where(bundle_id: self.id, "value.test_id" => nil).order_by(["value.last",:asc])
+  end
+
+  def delete
+    self.measures.destroy
+    self.records.destroy
+    self.value_sets.destroy
+    self.results.destroy
+    product_tests.destroy
+    super
+  end
+
+end
---
+++ lib/ext/valuesets.rb
@@ -0,0 +1,5 @@
+ValueSet = HealthDataStandards::SVS::ValueSet
+
+class ValueSet
+  index :bundle_id=> 1
+end
