9cb2e4a32fa7b49909397c40d3373600d80810d5
jaebird123 <jeaton@mitre.org>
2015-10-20 08:41:53 -0400

    removed certification test types from products

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,30 +10,16 @@ class Product
   field :name, type: String
   field :version, type: String
   field :description, type: String
+  field :ehr_type, type: String
+  # state should be "passing", "failing", etc.
+  field :state, type: String
 
   validates :name, presence: true, uniqueness: { scope: :vendor, message: "Product name was already taken. Please choose another." }
   validates :ehr_type, presence: true, inclusion: { in: %w(provider hospital) }
-  validate :at_least_one_test_type?
   validates :vendor, presence: true
-
-  field :ehr_type, type: String
-  field :c1_test, type: Boolean
-  field :c2_test, type: Boolean
-  field :c3_test, type: Boolean
-  field :c4_test, type: Boolean
-
-  # state should be "passing", "failing", etc.
-  field :state, type: String
 
   scope :passing, -> { where(state: "passing") }
   scope :failing, -> { where(state: "failing") }
   scope :incomplete, -> { where(state: "incomplete") }
 
-  def at_least_one_test_type?
-    errors.add(:tests, 'Product must include at least one certification test.') unless [ c1_test, c2_test, c3_test, c4_test ].any? { |is_true| is_true }
-  end
-
 end
-
-# validates :size, inclusion: { in: %w(small medium large),
-#     message: "%{value} is not a valid size" }
