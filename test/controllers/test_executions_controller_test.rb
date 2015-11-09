3b6c8440636efbe0faa2399b8012885f330489eb
Dylan Hall <dehall@mitre.org>
2015-11-09 08:20:33 -0500

    ability to post file to test execution controller

--- test/controllers/test_executions_controller_test.rb
+++ test/controllers/test_executions_controller_test.rb
@@ -9,6 +9,7 @@ class TestExecutionsControllerTest < ActionController::TestCase
   teardown do
     drop_database
   end
+
   test 'should get index' do
     get :index, task_id: Task.first.id
     assert_response :success
@@ -26,5 +27,21 @@ class TestExecutionsControllerTest < ActionController::TestCase
     delete :destroy, id: TestExecution.first.id
     assert_response 204
     assert_equal nil, TestExecution.where(_id: id).first, 'SHould have deleted test execution'
+  end
+
+  test 'should post create' do
+    C1Task.any_instance.stubs(:validators).returns([])
+    C1Task.any_instance.stubs(:records).returns([])
+
+    task = Task.first
+    orig_count = task.test_executions.count
+
+    zipfile = File.new(File.join(Rails.root, 'test/fixtures/product_tests/ep_qrda_test_good.zip'))
+    upload = Rack::Test::UploadedFile.new(zipfile, 'application/zip')
+
+    post :create, task_id: task.id, results: upload
+
+    assert_response 200
+    assert_equal task.test_executions.count, orig_count + 1, 'Should have added 1 new TestExecution'
   end
 end
