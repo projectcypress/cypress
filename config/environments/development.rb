bffccd0fc018dbb19efc6515c5ea1c2f4ae9db8f
Sam Sayer <samuel.sayer@gmail.com>
2015-11-17 14:03:57 -0500

    fix development logger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  config.mongoid.level = Logger::INFO
+  config.mongoid.logger.level = Logger::INFO
 
   # Raise an error on page load if there are pending migrations.
   # config.active_record.migration_error = :page_load
