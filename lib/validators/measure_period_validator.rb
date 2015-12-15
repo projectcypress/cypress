503a27ea9146da6b4a1849b19d174ee2a8cf4b35
Sam Sayer <samuel.sayer@gmail.com>
2015-12-14 19:54:34 -0500

    fix a few small bug for the demo

--- lib/validators/measure_period_validator.rb
+++ lib/validators/measure_period_validator.rb
@@ -20,7 +20,7 @@ module Validators
       measure_start = Settings.effective_date.year.to_s + '0101'
       unless @document.at_xpath("/cda:ClinicalDocument/cda:component/cda:structuredBody/cda:component/cda:section/
         cda:entry/cda:act[./cda:templateId[@root='2.16.840.1.113883.10.20.17.3.8']]/
-        cda:effectiveTime/cda:low/@value").value.to_s.include? measure_start
+  cda:effectiveTime/cda:low/@value").value.to_s.start_with? measure_start
         msg = "Reported Measurement Period should start on #{measure_start}"
         add_error(msg, :location => '/', :validator_type => :submission_validation, :file_name => @options[:file_name])
       end
@@ -30,7 +30,7 @@ module Validators
       measure_end = Settings.effective_date.year.to_s + '1231'
       unless @document.at_xpath("/cda:ClinicalDocument/cda:component/cda:structuredBody/cda:component/cda:section/
         cda:entry/cda:act[./cda:templateId[@root='2.16.840.1.113883.10.20.17.3.8']]/
-        cda:effectiveTime/cda:high/@value").value.to_s.include? measure_end
+  cda:effectiveTime/cda:high/@value").value.to_s.start_with? measure_end
         msg = "Reported Measurement Period should end on #{measure_end}"
         add_error(msg, :location => '/', :validator_type => :submission_validation, :file_name => @options[:file_name])
       end
