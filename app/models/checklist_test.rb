fa33a838a685e14b86185f566ced795e4abc5a8c
jaebird123 <jeaton@mitre.org>
2016-01-12 12:40:15 -0500

    changed product status display. now split into 'C1 Checklist', 'C1/C3 Measure Tests', 'C2/C3 Measure Tests', and 'C4 Filtering Tests'. each show the number of passing, failing, not started, and total tests

--- app/models/checklist_test.rb
+++ app/models/checklist_test.rb
@@ -1,4 +1,17 @@
 class ChecklistTest < ProductTest
   embeds_many :checked_criteria, class_name: 'ChecklistSourceDataCriteria'
   accepts_nested_attributes_for :checked_criteria, allow_destroy: true
+
+  def num_measures_complete
+    num_complete = 0
+    Measure.top_level.where(:hqmf_id.in => measure_ids).each do |measure|
+      criterias = checked_criteria.select { |criteria| criteria.measure_id == measure.id.to_s }
+      num_complete += 1 if criterias.count(&:completed) == criterias.count
+    end
+    num_complete
+  end
+
+  def num_measures
+    measure_ids.count
+  end
 end
