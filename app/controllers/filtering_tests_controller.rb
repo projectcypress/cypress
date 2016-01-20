261654c6c62ca512b4ed5570b8d325aa872416fb
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-01-20 09:38:44 -0500

    Remove references to C4Task subclasses.

--- app/controllers/filtering_tests_controller.rb
+++ app/controllers/filtering_tests_controller.rb
@@ -3,19 +3,11 @@ class FilteringTestsController < ProductTestsController
 
   def new
     @filtering_test = @product.product_tests.build({}, FilteringTest)
-    @filtering_test.tasks << DemographicsTask.new
-    @filtering_test.tasks << DemographicsTask.new
-    @filtering_test.tasks << LocationsTask.new
   end
 
   def create
     @filtering_test = @product.product_tests.build(params[:product_tests], FilteringTest)
-    tasks = [DemographicsTask, DemographicsTask, LocationsTask]
-    params[:product_test][:tasks_attributes].each do |_, v|
-      task_type = tasks.shift
-      @filtering_test.tasks << task_type.send('new', v)
-    end
-    tasks.each { |task| @filtering_test.tasks << task.send('new') }
+
     @filtering_test.name = "C4 #{@product.name}"
     measure = Measure.top_level.first
     # TODO: This shouldn't default to first measures, measures need to be stored on the product
