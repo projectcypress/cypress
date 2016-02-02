33e5bf5205c9e263288d742db758c7696a7cff48
Michael O'Keefe <mokeefe@mitre.org>
2016-02-02 09:28:25 -0500

    Fixed a bunch of Rubocop errors that were only flagged as warnings in Overcommit (?)

--- lib/cypress/population_clone_job.rb
+++ lib/cypress/population_clone_job.rb
@@ -34,14 +34,12 @@ module Cypress
     def find_patients_to_clone
       # Clone AMA records from Mongo
       @test = ProductTest.find(options['test_id'])
-      patients = []
       patients = if options['patient_ids']
                    # clone each of the patients identified in the :patient_ids parameter
                    @test.bundle.records.where(test_id: nil).in(medical_record_number: options['patient_ids']).to_a
                  else
                    @test.bundle.records.where(test_id: nil).to_a
-      end
-
+                 end
       patients
     end
 
@@ -120,7 +118,7 @@ module Cypress
                generate_provider
              else
                Provider.default_provider
-      end
+             end
       patient.provider_performances.build(provider: prov) if prov
     end
 
