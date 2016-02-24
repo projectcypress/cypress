cec298eb71dfac31f23dc727095cb84185a7dd6d
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-02-24 15:21:34 -0500

    Remove erroneous logging statement from asset pipeline debugging
    Change test to acknowledge that JS blocks the submission and a different error message is displayed.

--- vendor/assets/javascripts/parsley/parsley.js
+++ vendor/assets/javascripts/parsley/parsley.js
@@ -12,8 +12,6 @@
 //
 var _slice = Array.prototype.slice;
 
-console.log("PARSLEY LOADED");
-
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
 
 (function (global, factory) {
