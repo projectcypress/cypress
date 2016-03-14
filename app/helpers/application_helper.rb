21296e2a10299b85c3a0cfcd9623d43d3ce9dc36
thecristen <cristen@outlook.com>
2016-03-14 09:54:16 -0400

    use helper to ensure website link has correct formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def display_time(seconds_since_epoch)
     Time.at(seconds_since_epoch).utc.strftime('%m/%d/%Y') || '?'
   end
+
+  def website_link(url_string)
+    uri = URI.parse(url_string)
+    return url_string if uri.scheme
+    "http://#{url_string}"
+  end
 end
