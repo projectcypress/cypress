92a063f68d7f50e63c8405d5a05427379ab181a6
thecristen <cristen@outlook.com>
2016-03-25 10:01:32 -0400

    add two custom error pages

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,8 @@ module Cypress
     # prevent rails from wrapping inputs with errors in a div of class "field_with_errors"
     config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
     config.active_job.queue_adapter = :delayed_job
+
+    # This lets us define our own routes for error pages
+    config.exceptions_app = routes
   end
 end
