0924db9a68cf615b1527bc820b8e6ca1f1628f71
thecristen <cristen@outlook.com>
2016-03-31 09:42:10 -0400

    update Assets Framework to 3.4.1 and load only the files we need

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,8 @@
 //= require dataTables/jquery.dataTables
 //= require jquery-ui
 //= require bootstrap-sprockets
-//= require assets_framework
+//= require assets_framework/assets.core
+//= require assets_framework/breadcrumb
 //= require jquery_nested_form
 //= require jasny-bootstrap.min
 //= require_tree .
