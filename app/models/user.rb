aee4825f3151bbf1d5ea26360e50374e6b3dfa51
Michael O'Keefe <mokeefe@mitre.org>
2016-04-07 10:42:33 -0400

    Added a 'failed_attempts' field to User, so we can count failed attempts before lockout

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User
 
   ## Lockable
   field :locked_at, type: Time
+  field :failed_attempts, type: Integer, default: 0
 
   field :approved, type: Boolean, default: APP_CONFIG.auto_approve || false
 
@@ -75,15 +76,4 @@ class User
       end
     end
   end
-
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
 end
