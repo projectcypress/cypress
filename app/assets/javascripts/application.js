663686200ae2ee15cc0ae3b0a0baaf4888a1fab8
thecristen <cristen@outlook.com>
2016-04-15 16:32:03 -0400

    dont import ALL the jQuery UI and ALL the Bootstrap JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,11 @@
 //= require jquery_ujs
 //= require parsley/parsley
 //= require dataTables/jquery.dataTables
-//= require jquery-ui
-//= require bootstrap-sprockets
+//= require jquery-ui/autocomplete
+//= require jquery-ui/tabs
+//= require bootstrap/tab
+//= require bootstrap/tooltip
+//= require bootstrap/popover
 //= require assets_framework/assets.core
 //= require assets_framework/breadcrumb
 //= require jquery_nested_form
