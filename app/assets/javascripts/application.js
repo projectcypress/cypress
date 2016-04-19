98e792550251615b8ff5af9cab57d4ca909142b4
thecristen <cristen@outlook.com>
2016-04-19 11:07:37 -0400

    these are only here because Assets core library is terrible

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,15 @@
 //= require bootstrap/tab
 //= require bootstrap/tooltip
 //= require bootstrap/popover
+//= require jquery-ui/accordion
+//= require jquery-ui/button
+//= require jquery-ui/dialog
+//= require jquery-ui/menu
+//= require jquery-ui/progressbar
+//= require jquery-ui/slider
+//= require jquery-ui/spinner
+//= require jquery-ui/tooltip
+//= require jquery-ui/datepicker
 //= require assets_framework/assets.core
 //= require assets_framework/breadcrumb
 //= require jquery_nested_form
