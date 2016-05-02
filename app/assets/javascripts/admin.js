47ea57bd5bdd9771c7970691a885667c87c5fade
Rob Dingwell <bobd@mitre.org>
2016-05-02 13:23:10 -0400

    updating ui for user managment

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -9,15 +9,15 @@ var assignmentsReady = function(){
     //does the assignment already exist"
     //should this be done for a user that has a role other then user (atl,admin this means nothing as they alreay have those permissions)
     if( $("input[value='"+vendor.value+"'][name*='[vendor_id]']").length  == 0){
-      var div = $("<div>")
-      div.append("Role: "+role.text + " Vendor: "+vendor.text);
-      var roleIn = $("<input type='hidden' name='assignments["+assignmentIndex+"][vendor_id]' value='"+vendor.value+"'/>")
-      var vendorIn = $("<input type='hidden' name='assignments["+assignmentIndex+"][role]' value='"+role.value+"'/>")
-      var button = $("<button onclick='$(this).parent().remove()' > Remove </button>")
-      div.append(roleIn)
-      div.append(vendorIn)
-      div.append(button)
-      $('#assignments').append(div);
+      var tr = $("<tr>")
+      tr.append($("<td>"+role.text + "</td>"))
+      tr.append($("<td>"+vendor.text + "</td>"))
+      var buttonTd = $("<td>");
+      tr.append(buttonTd);
+      buttonTd.append($("<input type='hidden' name='assignments["+assignmentIndex+"][vendor_id]' value='"+vendor.value+"'/>"))
+      buttonTd.append($("<input type='hidden' name='assignments["+assignmentIndex+"][role]' value='"+role.value+"'/>"))
+      buttonTd.append($("<button onclick='$(this).parent().parent().remove()' > Remove </button>"))
+      $('#assignments').append(tr);
     }
   }
   $("#addAssignment").click(addAssignment)
