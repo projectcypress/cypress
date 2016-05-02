dc0aab8dab1cffd50970559d6ca36976dac421bc
Tom Strassner <tstrassner@mitre.org>
2016-05-02 15:09:01 -0400

    Fixed a typo in production environment that was causing an error (#448)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: '',
     port: '',
