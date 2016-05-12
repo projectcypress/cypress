1e2f95890c6a2daee3e80fb723b261074dd8dda5
Tom Strassner <tstrassner@mitre.org>
2016-05-12 10:58:53 -0400

    Added debug features to the application mode settings

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -7,7 +7,7 @@ module AdminHelper
   end
 
   def application_mode_settings
-    settings_hash = { auto_approve: Settings[:auto_approve], ignore_roles: Settings[:ignore_roles] }
+    settings_hash = { auto_approve: Settings[:auto_approve], ignore_roles: Settings[:ignore_roles], debug_features: Settings[:enable_debug_features] }
     settings_hash[:default_role] = if Settings[:default_role].nil?
                                      'None'
                                    elsif Settings[:default_role] == :atl
@@ -19,14 +19,42 @@ module AdminHelper
   end
 
   def mode_internal?
-    !Settings[:auto_approve] && Settings[:ignore_roles] && Settings[:default_role].nil?
+    !Settings[:auto_approve] && Settings[:ignore_roles] && Settings[:enable_debug_features] && Settings[:default_role].nil?
   end
 
   def mode_demo?
-    !Settings[:auto_approve] && !Settings[:ignore_roles] && Settings[:default_role] == :user
+    !Settings[:auto_approve] && !Settings[:ignore_roles] && Settings[:enable_debug_features] && Settings[:default_role] == :user
   end
 
   def mode_atl?
-    Settings[:auto_approve] && !Settings[:ignore_roles] && Settings[:default_role].nil?
+    Settings[:auto_approve] && !Settings[:ignore_roles] && !Settings[:enable_debug_features] && Settings[:default_role].nil?
+  end
+
+  def mode_internal
+    Settings[:auto_approve] = false
+    Settings[:ignore_roles] = true
+    Settings[:default_role] = nil
+    Settings[:enable_debug_features] = true
+  end
+
+  def mode_demo
+    Settings[:auto_approve] = false
+    Settings[:ignore_roles] = false
+    Settings[:default_role] = :user
+    Settings[:enable_debug_features] = true
+  end
+
+  def mode_atl
+    Settings[:auto_approve] = true
+    Settings[:ignore_roles] = false
+    Settings[:default_role] = nil
+    Settings[:enable_debug_features] = false
+  end
+
+  def mode_custom(settings)
+    Settings[:auto_approve] = settings['auto_approve'] == 'enable'
+    Settings[:ignore_roles] = settings['ignore_roles'] == 'enable'
+    Settings[:default_role] = settings['default_role'] == 'None' ? nil : settings['default_role'].underscore.to_sym
+    Settings[:enable_debug_features] = settings['debug_features'] == 'enable'
   end
 end
