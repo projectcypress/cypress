afe6933169644db8896fb2796a38d62c4a5cc0cb
thecristen <cristen@outlook.com>
2016-06-15 17:23:12 -0400

    adjust panel style based on test result and add fancy summary message

--- app/helpers/test_executions_helper.rb
+++ app/helpers/test_executions_helper.rb
@@ -67,4 +67,27 @@ module TestExecutionsHelper
       'QRDA Category III XML document'
     end
   end
+
+  def execution_failure_message(execution)
+    should_display_c3 = execution.task.product_test.product.c3_test && execution.task.product_test._type == 'MeasureTest'
+    combined_errors = should_display_c3 ? execution.execution_errors + execution.sibling_execution.execution_errors : execution.execution_errors
+
+    all_warnings, all_errors = combined_errors.partition { |e| e.msg_type == :warning }
+
+    failure_message = "Failed with #{pluralize(all_errors.uniq { |e| [e.message, e.location, e.file_name] }.count, 'error')}"
+    failure_message += " and #{pluralize(all_warnings.uniq { |e| [e.message, e.location, e.file_name] }.count, 'warning')}" if all_warnings.any?
+
+    failure_message
+  end
+
+  def execution_status_class(execution)
+    case execution.status_with_sibling
+    when 'incomplete'
+      return 'info'
+    when 'passing'
+      return 'success'
+    else
+      return 'danger'
+    end
+  end
 end
