7f495cebdb9dbb05ea8e1c1f7771780ec8e47f67
MatthewPelland <mpelland@mitre.org>
2016-06-22 14:40:42 -0400

    All info on patient page (#531)
    * Adds 'not performed' indication and reason to procedures
    
    * adds all missing information to patient page

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -3,7 +3,10 @@ module RecordsHelper
                 medical_equipment medications procedures results communications
                 family_history social_history vital_signs support advance_directives
                 insurance_providers functional_statuses).freeze
-
+  FIELDS = %w(name principalDiagnosis values dose dischargeDisposition route
+              administrationTiming fulfillmentHistory reason direction ordinality
+              transferFrom laterality anatomical_location diagnosis).freeze
+  SUBFIELDS = %w(title scalar value unit units description period dispenseDate quantityDispensed time).freeze
   CV_POPULATION_KEYS = %w(IPP MSRPOPL MSRPOPLEX OBSERV).freeze
   PROPORTION_POPULATION_KEYS = %w(IPP DENOM NUMER DENEX DENEXCEP).freeze
 
@@ -31,5 +34,28 @@ module RecordsHelper
     # Returns array of records that have at least one calculation result for the given measure id
 
     records.select { |r| !r.calculation_results.where('value.measure_id' => measure.hqmf_id).where('value.sub_id' => measure.sub_id).empty? }
+  end
+
+  def display_field(field)
+    display_text = ''
+    return '' if field.nil?
+    return field if field.is_a? String
+    return display_time(field) + "\n" if field.is_a? Fixnum
+    if field.is_a? Array
+      field.each { |sub| display_text += display_field(sub) + "\n" }
+    else
+      field.each do |key, subfield|
+        display_text += display_field(subfield) + ' ' if SUBFIELDS.include? key
+      end
+      if field['codes']
+        field['codes'].each do |code_system, code|
+          display_text += "\n" + code_system + ': ' + code.join(', ')
+        end
+      end
+      if field['code_system']
+        display_text += "\n" + field['code_system'] + ': ' + field['code']
+      end
+    end
+    display_text
   end
 end
