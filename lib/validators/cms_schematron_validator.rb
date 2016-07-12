b113b94b605bed80f3824c1ccdf4e6ed7ca90bcb
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-07-12 17:48:41 -0400

    Remove banners, added options to just inject warnings into validation based on version.

--- lib/validators/cms_schematron_validator.rb
+++ lib/validators/cms_schematron_validator.rb
@@ -4,18 +4,26 @@ module Validators
     include HealthDataStandards::Validate
 
     self.validator = :cms_schematron
+    @bundle_version = APP_CONFIG.default_bundle
 
-    def initialize(schematron_file, name)
+    def initialize(schematron_file, name, bundle_version=APP_CONFIG.default_bundle)
       @validator = Schematron::Validator.new(name, schematron_file)
+      @bundle_version = bundle_version
     end
 
     def schematron_folder_for_bundle_version(bundle_version)
       ApplicationController.helpers.config_for_version(bundle_version).schematron
     end
 
+
+
     def validate(file, options = {})
       @options = options
       doc = get_document(file)
+      default_errors = ApplicationController.helpers.config_for_version(@bundle_version)["schematron_warnings"]
+      default_errors.each do |error|
+        add_warning error, :validator_type => :xml_validation, :file_name => @options[:file_name]
+      end
       errors = @validator.validate(doc, options)
       errors.each do |error|
         add_warning error.message,
@@ -24,27 +32,28 @@ module Validators
                     :validator_type => :xml_validation,
                     :file_name => @options[:file_name]
       end
+
     end
   end
 
   class CMSQRDA3SchematronValidator < CMSSchematronValidator
     def initialize(bundle_version = APP_CONFIG.default_bundle)
       super(File.join(Rails.root, 'resources', 'schematron', schematron_folder_for_bundle_version(bundle_version), 'EP_CAT_III.sch'),
-            'CMS QRDA 3 Schematron Validator')
+            'CMS QRDA 3 Schematron Validator', bundle_version)
     end
   end
 
   class CMSQRDA1HQRSchematronValidator < CMSSchematronValidator
     def initialize(bundle_version = APP_CONFIG.default_bundle)
       super(File.join(Rails.root, 'resources', 'schematron', schematron_folder_for_bundle_version(bundle_version), 'EH_CAT_I.sch'),
-            'CMS QRDA 1 HQR Schematron Validator')
+            'CMS QRDA 1 HQR Schematron Validator', bundle_version)
     end
   end
 
   class CMSQRDA1PQRSSchematronValidator < CMSSchematronValidator
     def initialize(bundle_version = APP_CONFIG.default_bundle)
       super(File.join(Rails.root, 'resources', 'schematron', schematron_folder_for_bundle_version(bundle_version), 'EP_CAT_I.sch'),
-            'CMS QRDA 1 PQRS Schematron Validator')
+            'CMS QRDA 1 PQRS Schematron Validator', bundle_version)
     end
   end
 end
