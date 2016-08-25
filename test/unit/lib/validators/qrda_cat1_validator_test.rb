3dce23cf0901bac88d2ca12d14efc9931bc9e836
Czulada, David S <dczulada@mitre.org>
2016-08-25 09:42:39 -0400

    tests needed to pass task in as an option

--- test/unit/lib/validators/qrda_cat1_validator_test.rb
+++ test/unit/lib/validators/qrda_cat1_validator_test.rb
@@ -8,20 +8,21 @@ class QrdaCat1ValidatorTest < ActiveSupport::TestCase
     @measures = Measure.in(hqmf_id: ['8A4D92B2-397A-48D2-0139-7CC6B5B8011E'])
     @validator_with_c3 = QrdaCat1Validator.new(@bundle, false, true, @measures)
     @validator_without_c3 = QrdaCat1Validator.new(@bundle, false, false, @measures)
+    @task = C1Task.new
   end
 
   def test_validate_good_file
     file = File.new(File.join(Rails.root, 'test/fixtures/product_tests/ep_qrda_test_good/0_Dental_Peds_A.xml')).read
-    @validator_with_c3.validate(file)
+    @validator_with_c3.validate(file, task: @task)
     assert_empty @validator_with_c3.errors
 
-    @validator_without_c3.validate(file)
+    @validator_without_c3.validate(file, task: @task)
     assert_empty @validator_without_c3.errors
   end
 
   def test_validate_too_much_data_error
     file = File.new(File.join(Rails.root, 'test/fixtures/product_tests/ep_qrda_test_too_much_data/0_Dental_Peds_A.xml')).read
-    @validator_with_c3.validate(file)
+    @validator_with_c3.validate(file, task: @task)
 
     errors = @validator_with_c3.errors
     assert_not_empty errors
@@ -29,7 +30,7 @@ class QrdaCat1ValidatorTest < ActiveSupport::TestCase
       assert_equal :error, e.msg_type, 'All validation messages should be errors when c3 is included'
     end
 
-    @validator_without_c3.validate(file)
+    @validator_without_c3.validate(file, task: @task)
     errors_no_c3 = @validator_without_c3.errors
     assert_not_empty errors_no_c3
     errors_no_c3.each do |e|
@@ -41,7 +42,7 @@ class QrdaCat1ValidatorTest < ActiveSupport::TestCase
 
   def test_bad_schema
     file = File.new(File.join(Rails.root, 'test/fixtures/qrda/cat_1/bad_schema.xml')).read
-    @validator_with_c3.validate(file)
+    @validator_with_c3.validate(file, task: @task)
 
     errors = @validator_with_c3.errors
     assert_not_empty errors
