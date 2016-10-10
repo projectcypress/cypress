b0a539ed1924fe9ca9dd57a848496bca19e88533
Tom Strassner <tstrassner@mitre.org>
2016-10-10 15:26:22 -0400

    Allow for progressive manual entry test (#739)

--- app/helpers/checklist_tests_helper.rb
+++ app/helpers/checklist_tests_helper.rb
@@ -1,6 +1,7 @@
 module ChecklistTestsHelper
   def disable_qrda_submission?
-    !@product_test.checked_criteria.all?(&:checklist_complete?) || @product_test.state == :errored
+    # only disable if none of the measures have all good checklist criteria
+    @product_test.checked_criteria.group_by(&:measure_id).values.all? { |cc_group| cc_group.any? { |cc| !cc.checklist_complete? } }
   end
 
   def checklist_test_criteria_attribute(measure, criteria)
