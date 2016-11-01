64077e59a68ef02bc02be548587ad1c199fde1f9
Michael O'Keefe <mokeefe@mitre.org>
2016-11-01 15:07:33 -0400

    Added an MPL download creation job, and refactored create_download_zip

---
+++ app/jobs/mpl_download_create_job.rb
@@ -0,0 +1,19 @@
+class MplDownloadCreateJob < ActiveJob::Base
+  include Job::Status
+  include CypressYaml
+  after_enqueue do |job|
+    tracker = job.tracker
+    tracker.options['original_filename'] = job.arguments[1]
+    tracker.save
+  end
+  def perform(bundle_id)
+    tracker.log('Creating Download')
+    bundle = Bundle.find(BSON::ObjectId.from_string(bundle_id))
+    path = File.join(Rails.root, 'tmp', 'cache', 'bundle_download', bundle.id.to_s)
+    zip_path = File.join(Rails.root, 'tmp', 'cache', "bundle_#{bundle.id}_mpl.zip")
+    Cypress::CreateDownloadZip.bundle_directory(bundle, path)
+    zfg = ZipFileGenerator.new(path, zip_path)
+    zfg.write
+    FileUtils.rm_rf(path)
+  end
+end
