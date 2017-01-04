5970d6dfe48bfe74140b853b2cb57d0be5e39fa5
Robert Clark <rbclark@mitre.org>
2017-01-04 18:03:06 -0500

    Fixed infinite scrolling to always register on page load

--- app/assets/javascripts/infinite-scroll.js
+++ app/assets/javascripts/infinite-scroll.js
@@ -1,44 +1,53 @@
-$(function() {
-  var approachingBottomOfPage, loadNextPageAt, nextPage, nextPageFnRunning, viewMore;
+var approachingBottomOfPage, loadNextPageAt, nextPage, nextPageFnRunning, viewMore, ready;
 
+nextPageFnRunning = false;
+loadNextPageAt = 3000;
+
+approachingBottomOfPage = function() {
+  return $(window).scrollTop() > $(document).height() - $(window).height() - loadNextPageAt;
+};
+
+nextPage = function() {
+  var loadingMore, url;
   viewMore = $('#view-more');
-  nextPageFnRunning = false;
-  loadNextPageAt = 3000;
-
-  approachingBottomOfPage = function() {
-    return $(window).scrollTop() > $(document).height() - $(window).height() - loadNextPageAt;
-  };
-
-  nextPage = function() {
-    var loadingMore, url;
-    viewMore = $('#view-more');
-    loadingMore = $('#loading-more');
-    url = viewMore.find('a').attr('href');
-    if (nextPageFnRunning || !url) {
-      return;
-    }
-    viewMore.hide();
-    loadingMore.show();
-    nextPageFnRunning = true;
-    return $.ajax({
-      url: url,
-      method: 'GET',
-      dataType: 'script'
-    }).always(function() {
-      nextPageFnRunning = false;
-      viewMore.show();
-      return loadingMore.hide();
-    });
-  };
-
-  $(window).on('load scroll', function() {
-    if (approachingBottomOfPage()) {
-      return nextPage();
-    }
+  loadingMore = $('#loading-more');
+  url = viewMore.find('a').attr('href');
+  if (nextPageFnRunning || !url) {
+    return;
+  }
+  viewMore.hide();
+  loadingMore.show();
+  nextPageFnRunning = true;
+  return $.ajax({
+    url: url,
+    method: 'GET',
+    dataType: 'script'
+  }).always(function() {
+    nextPageFnRunning = false;
+    viewMore.show();
+    return loadingMore.hide();
   });
+};
+
+// Checks to see if we are close to the bottom of the page and if we are load more measures
+checkAndLoad = function() {
+  if (approachingBottomOfPage()) {
+    return nextPage();
+  }
+};
+
+ready = function() {
+  viewMore = $('#view-more');
+
+  // Call checkAndLoad now and when the page scrolls
+  checkAndLoad();
+  $(window).on('scroll', checkAndLoad);
 
   viewMore.find('a').unbind('click').click(function(e) {
     nextPage();
     return e.preventDefault();
   });
-});
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
