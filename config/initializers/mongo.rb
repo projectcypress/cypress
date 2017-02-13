3e1e7aff8f94263505f0a6cf5b7475805c8a2b34
Robert Clark <rbclark@mitre.org>
2017-02-13 12:16:00 -0500

    Add MONGO_LOGS environment variable
    If MONGO_LOGS=true is set as an environment variable before running rails server or console then the mongo logs will be printed to the screen along with the rails logs.
    
    Sample usage: MONGO_LOGS=true bundle exec rails s

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,6 +1,8 @@
 module QME
   Mongoid.logger.level = Logger::INFO
+  Mongoid.logger = Rails.logger if ENV['MONGO_LOGS'].eql? 'true'
   Mongo::Logger.logger.level = Logger::WARN
+  Mongo::Logger.logger = Rails.logger if ENV['MONGO_LOGS'].eql? 'true'
   patient_cache = Mongoid.default_client['patient_cache']
   BSON::Config.validating_keys = false
 
