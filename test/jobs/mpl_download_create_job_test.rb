c7e90d64b722af46d7ccb3281737d4fd6376789f
Robert Clark <rbclark@mitre.org>
2017-05-04 15:16:10 -0400

    Added support for generating MPL download bundle as a background job on MPL page load
    Sometimes users manage to get to the MPL download page before the download has actually been generated. This adds automatic refreshing while the bundle is generated as well as kicking off the generation if it was not generated on initial bundle load.

---
+++ test/jobs/mpl_download_create_job_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class MplDownloadCreateJobTest < ActiveJob::TestCase
+  setup do
+    collection_fixtures('bundles', 'products')
+
+    @bundle = Bundle.find('4fdb62e01d41c820f6000001')
+    # Clean up MPL before and after running for consistency
+    FileUtils.rm_rf(@bundle.mpl_path)
+  end
+
+  teardown do
+    FileUtils.rm_rf(@bundle.mpl_path)
+  end
+
+  def test_enqueue_mpl_create_job
+    assert_enqueued_jobs 0
+
+    assert_enqueued_jobs 1 do
+      assert :building, @bundle.mpl_prepare
+    end
+  end
+
+  def test_running_mpl_job_success
+    perform_enqueued_jobs do
+      assert :building, @bundle.mpl_prepare
+      assert_performed_jobs 1
+      assert :ready, @bundle.mpl_status
+      assert File.exist?(@bundle.mpl_path)
+    end
+  end
+
+  def test_multiple_enqueue_does_not_create_multiple_jobs
+    assert_enqueued_jobs 0
+    assert :building, @bundle.mpl_prepare
+    assert_enqueued_jobs 1
+    assert :building, @bundle.mpl_prepare
+    assert_enqueued_jobs 1
+  end
+end
