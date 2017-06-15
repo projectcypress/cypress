a507d323cf86796256f15dace257b572625e1e0b
Czulada, David S <dczulada@mitre.org>
2017-06-15 13:07:24 -0400

    Update error totals based on new validation

--- test/unit/lib/validators/expected_results_validator_test.rb
+++ test/unit/lib/validators/expected_results_validator_test.rb
@@ -16,7 +16,7 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     file = File.new(File.join(Rails.root, 'test/fixtures/qrda/ep_test_qrda_cat3_good.xml')).read
 
     @validator.validate(file, 'task' => @task)
-    assert_equal 10, @validator.errors.length # 10 errors related to pop sums
+    assert_equal 18, @validator.errors.length # 10 errors related to pop sums
     assert_equal 10, @validator.errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
   end
 
@@ -24,7 +24,7 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     file = File.new(File.join(Rails.root, 'test/fixtures/qrda/ep_test_qrda_cat3_good_1_1.xml')).read
     @task.bundle.version = '2016.0.0'
     @validator.validate(file, 'task' => @task)
-    assert_equal 10, @validator.errors.length # 10 errors related to pop sums
+    assert_equal 18, @validator.errors.length # 10 errors related to pop sums
     assert_equal 10, @validator.errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
   end
 
@@ -32,8 +32,8 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     file = File.new(File.join(Rails.root, 'test/fixtures/qrda/ep_test_qrda_cat3_1_1_not_ipop.xml')).read
     @task.bundle.version = '2016.0.0'
     @validator.validate(file, 'task' => @task)
-    assert_equal 31, @validator.errors.length, 'should error on missing measure entry' # 7 errors related to pop sums
-    assert_equal 7, @validator.errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
+    assert_equal 51, @validator.errors.length, 'should error on missing measure entry' # 7 errors related to pop sums
+    assert_equal 19, @validator.errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
     assert_equal 3, @validator.errors.count { |e| e.message == 'Could not find value for Population IPOP' }
   end
 
@@ -42,7 +42,7 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     @validator.validate(file, 'task' => @task)
 
     errors = @validator.errors
-    assert_equal 11, errors.length, 'should error on missing stratifications' # 10 errors related to pop sums
+    assert_equal 19, errors.length, 'should error on missing stratifications' # 10 errors related to pop sums
     assert_equal 10, errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
     assert_equal 1, (errors.count do |e|
       !/\ACould not find value for stratification [a-zA-Z\d\-]{36}  for Population \w+\z/.match(e.message).nil?
@@ -54,7 +54,7 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     @validator.validate(file, 'task' => @task)
 
     errors = @validator.errors
-    assert_equal 16, errors.length, 'should error on missing supplemental data' # 13 errors related to pop sums
+    assert_equal 24, errors.length, 'should error on missing supplemental data' # 13 errors related to pop sums
     errors.each { |e| (assert_equal :result_validation, e.validator_type) }
     assert_equal 13, errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
     assert_equal 3, errors.count { |e| e.validator_type == :result_validation && e.message == 'supplemental data error' }
@@ -65,7 +65,7 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     @validator.validate(file, 'task' => @task)
 
     errors = @validator.errors
-    assert_equal 12, errors.length, 'should error on additional supplemental data' # 11 errors related to pop sums
+    assert_equal 20, errors.length, 'should error on additional supplemental data' # 11 errors related to pop sums
     assert_equal 11, errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
     assert_equal 1, errors.count { |e| e.message == 'supplemental data error' }
   end
