7825c744a47e5fa37981dffec29e35979717f569
Czulada, David S <dczulada@mitre.org>
2017-06-23 14:06:32 -0400

    Some negations need to use specific codes for consistent results
    First, check if a bundle has default negation codes prior

--- lib/cypress/go_import.rb
+++ lib/cypress/go_import.rb
@@ -28,10 +28,18 @@ module Cypress
                       entry.cda_identifier._id
                     end
         # If the entry is negated, there is a NA codeset and there is only one code, find a code to use for calcuation
-        if entry.negationInd && entry.codes.key?('NA_VALUESET') && entry.codes.size == 1
-          valueset = HealthDataStandards::SVS::ValueSet.where(oid: entry.codes['NA_VALUESET'].first, bundle_id: bundle.id)
-          entry.add_code(valueset.first.concepts.first['code'], valueset.first.concepts.first['code_system_name'])
-        end
+        select_negated_code(entry, bundle) if entry.negationInd && entry.codes.key?('NA_VALUESET') && entry.codes.size == 1
+      end
+    end
+
+    def self.select_negated_code(entry, bundle)
+      negated_vs = entry.codes['NA_VALUESET'].first
+      # If Cypress has a default code selected, use it.  Otherwise, use the first in the valueset.
+      if bundle.default_negation_codes && bundle.default_negation_codes[negated_vs]
+        entry.add_code(bundle.default_negation_codes[negated_vs]['code'], bundle.default_negation_codes[negated_vs]['codeSystem'])
+      else
+        valueset = HealthDataStandards::SVS::ValueSet.where(oid: entry.codes['NA_VALUESET'].first, bundle_id: bundle.id)
+        entry.add_code(valueset.first.concepts.first['code'], valueset.first.concepts.first['code_system_name'])
       end
     end
   end
