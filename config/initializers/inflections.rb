4d3ebc3ba4f8f00419f81e3142a5fea309cf7f24
Michael O'Keefe <mokeefe@mitre.org>
2017-07-24 08:38:23 -0400

    Added inflections for the remaining two irregular inflections in Record::Sections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -16,5 +16,5 @@
 # end
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.uncountable %w( medical_equipment )
+  inflect.uncountable %w( medical_equipment family_history social_history )
 end
