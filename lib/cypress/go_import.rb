b9ca7ef25449fdd0cb02cfbc27884e21cd6abdee
Czulada, David S <dczulada@mitre.org>
2017-08-02 11:32:47 -0400

    Move common go_import post processing from CVU and Cypress to go-cda-tools
    update go-cda-tools to latest, as of August 8, 2017

--- lib/cypress/go_import.rb
+++ lib/cypress/go_import.rb
@@ -1,34 +1,10 @@
 module Cypress
   module GoImport
-    def self.update_conditions(record)
-      record.conditions.each do |condition|
-        if condition.status_code['HL7 ActStatus'] && condition.status_code['HL7 ActStatus'][0] == ''
-          condition.status_code['HL7 ActStatus'][0] = nil
-        end
-        condition[:end_time] = nil if condition[:end_time].nil?
-      end
-    end
-
-    def self.resolve_references(record)
-      refs = []
+    def self.replace_negated_codes(record, bundle)
       record.entries.each do |entry|
-        entry.references.each do |ref|
-          ref.referenced_id = ref.exported_ref
-          refs << ref.exported_ref
+        if entry.negationInd && entry.codes.key?('NA_VALUESET') && entry.codes.size == 1
+          select_negated_code(entry, bundle)
         end
-      end
-      refs
-    end
-
-    def self.update_entries(record, bundle, refs)
-      record.entries.each do |entry|
-        entry._id = if refs.include?(entry.cda_identifier.extension)
-                      BSON::ObjectId.from_string(entry.cda_identifier.extension)
-                    else
-                      entry.cda_identifier._id
-                    end
-        # If the entry is negated, there is a NA codeset and there is only one code, find a code to use for calcuation
-        select_negated_code(entry, bundle) if entry.negationInd && entry.codes.key?('NA_VALUESET') && entry.codes.size == 1
       end
     end
 
