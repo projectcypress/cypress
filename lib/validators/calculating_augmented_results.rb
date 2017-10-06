c2adf3ca7eb0591a3840232723db45ba468433b7
Czulada, David S <dczulada@mitre.org>
2017-10-06 11:24:43 -0400

    Calculating Augmented Results needs to track positive and negative diffs separately

--- lib/validators/calculating_augmented_results.rb
+++ lib/validators/calculating_augmented_results.rb
@@ -5,7 +5,8 @@ module Validators
   class CalculatingAugmentedResults
     def self.augmented_sup_val_expected?(task, keys_and_ids, code, expect_report, mod_pop_labels)
       report_diff = (expect_report[:expect].nil? ? 0 : expect_report[:expect]) - (expect_report[:report].nil? ? 0 : expect_report[:report])
-      expect_max_diff = 0
+      # expect_diff [min, max]
+      expect_diff = [0, 0]
       key_to_field = { 'RACE' => :race, 'ETHNICITY' => :ethnicity, 'SEX' => :gender }
 
       task.augmented_records.each do |rec_changes|
@@ -13,16 +14,17 @@ module Validators
         next unless augmented_record_in_population?(task, rec_changes, keys_and_ids, mod_pop_labels) &&
                     augmented_field && augmented_field[0] != augmented_field[1]
         if augmented_field[0] == code
-          expect_max_diff += 1
+          expect_diff[1] += 1
         elsif augmented_field[1] == code
-          expect_max_diff -= 1
+          expect_diff[0] -= 1
         end
       end
-      reported_in_expected_range?(report_diff, expect_max_diff)
+      reported_in_expected_range?(report_diff, expect_diff)
     end
 
-    def self.reported_in_expected_range?(report_diff, expect_max_diff)
-      (report_diff <= 0 && report_diff >= expect_max_diff) || (report_diff >= 0 && report_diff <= expect_max_diff)
+    def self.reported_in_expected_range?(report_diff, expect_diff)
+      # If reported diff is negative, compare with min diff, if reported diff is positive, compare with max diff
+      (report_diff <= 0 && report_diff >= expect_diff[0]) || (report_diff >= 0 && report_diff <= expect_diff[1])
     end
 
     def self.augmented_record_in_population?(task, rec_changes, keys_and_ids, mod_pop_labels)
--- test/unit/lib/validators/expected_results_validator_test.rb
+++ test/unit/lib/validators/expected_results_validator_test.rb
@@ -10,6 +10,30 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     @validator = ExpectedResultsValidator.new(@product_test.expected_results)
     @task = C2Task.new
     @task.product_test = @product_test
+    setup_augmented_records
+  end
+
+  def setup_augmented_records
+    @augmented_record1 = { 'medical_record_number' => '098718e0-4d42-0135-8680-12999b0ed66f',
+                           'first' => %w(Ivan Ivan), 'last' => %w(Mcguire Mcguirn), 'gender' => %w(M F) }
+    @record1 = Record.new(first: 'Ivan', last: 'Mcguire', medical_record_number: '098718e0-4d42-0135-8680-12999b0ed66f')
+    pc1 = HealthDataStandards::CQM::PatientCache.new(value: { 'IPP' => 1.000000, 'patient_id' => @record1.id })
+    pc1.save!
+    @record1.save!
+
+    @augmented_record2 = { 'medical_record_number' => '198718e0-4d42-0135-8680-12999b0ed66f',
+                           'first' => %w(Jill J), 'last' => %w(Mcguire Mcguirn), 'gender' => %w(F M) }
+    @record2 = Record.new(first: 'Jill', last: 'Mcguire', medical_record_number: '198718e0-4d42-0135-8680-12999b0ed66f')
+    pc2 = HealthDataStandards::CQM::PatientCache.new(value: { 'IPP' => 1.000000, 'patient_id' => @record2.id })
+    pc2.save!
+    @record2.save!
+
+    @augmented_record3 = { 'medical_record_number' => '298718e0-4d42-0135-8680-12999b0ed66f',
+                           'first' => %w(Joe John), 'last' => %w(Mcguire Mcguirn), 'gender' => %w(M M) }
+    @record3 = Record.new(first: 'Joe', last: 'Mcguire', medical_record_number: '298718e0-4d42-0135-8680-12999b0ed66f')
+    pc3 = HealthDataStandards::CQM::PatientCache.new(value: { 'IPP' => 1.000000, 'patient_id' => @record3.id })
+    pc3.save!
+    @record3.save!
   end
 
   def test_validate_good_file
@@ -58,6 +82,75 @@ class ExpectedResultsValidatorTest < ActiveSupport::TestCase
     errors.each { |e| (assert_equal :result_validation, e.validator_type) }
     assert_equal 13, errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
     assert_equal 3, errors.count { |e| e.validator_type == :result_validation && e.message == 'supplemental data error' }
+  end
+
+  def test_validate_augmented_results_one_augmented_patient
+    file = File.new(File.join(Rails.root, 'test/fixtures/qrda/ep_test_qrda_cat3_missing_supplemental_extra_female.xml')).read
+    @task.product_test.augmented_records = [@augmented_record1]
+    @record1.test_id = @task.product_test.id
+    @record1.save!
+    @validator.validate(file, 'task' => @task)
+    errors = @validator.errors
+    assert_equal 16, errors.length, 'should error on missing supplemental data' # 13 errors related to pop sums
+    errors.each { |e| (assert_equal :result_validation, e.validator_type) }
+    assert_equal 13, errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
+    assert_equal 3, errors.count { |e| e.validator_type == :result_validation && e.message == 'supplemental data error' }
+  end
+
+  def test_validate_augmented_results_two_augmented_patients
+    file = File.new(File.join(Rails.root, 'test/fixtures/qrda/ep_test_qrda_cat3_missing_supplemental_extra_female.xml')).read
+    @task.product_test.augmented_records = [@augmented_record1, @augmented_record3]
+    @record1.test_id = @task.product_test.id
+    @record1.save!
+    @record3.test_id = @task.product_test.id
+    @record3.save!
+    @validator.validate(file, 'task' => @task)
+    errors = @validator.errors
+    assert_equal 16, errors.length, 'should error on missing supplemental data' # 13 errors related to pop sums
+    errors.each { |e| (assert_equal :result_validation, e.validator_type) }
+    assert_equal 13, errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
+    assert_equal 3, errors.count { |e| e.validator_type == :result_validation && e.message == 'supplemental data error' }
+  end
+
+  def test_validate_augmented_results_three_augmented_patients_with_opposing_values
+    file = File.new(File.join(Rails.root, 'test/fixtures/qrda/ep_test_qrda_cat3_missing_supplemental_extra_male.xml')).read
+    @task.product_test.augmented_records = [@augmented_record1, @augmented_record2, @augmented_record3]
+    @record1.test_id = @task.product_test.id
+    @record1.save!
+    @record2.test_id = @task.product_test.id
+    @record2.save!
+    @record3.test_id = @task.product_test.id
+    @record3.save!
+    @validator.validate(file, 'task' => @task)
+    errors = @validator.errors
+    assert_equal 16, errors.length, 'should error on missing supplemental data' # 13 errors related to pop sums
+    errors.each { |e| (assert_equal :result_validation, e.validator_type) }
+    assert_equal 13, errors.count { |e| !pop_sum_err_regex.match(e.message).nil? }
+    assert_equal 3, errors.count { |e| e.validator_type == :result_validation && e.message == 'supplemental data error' }
+  end
+
+  def test_validate_augmented_results_three_augmented_patients_reporting_extra_male
+    file = File.new(File.join(Rails.root, 'test/fixtures/qrda/ep_test_qrda_cat3_missing_supplemental_two_extra_male.xml')).read
+    @task.product_test.augmented_records = [@augmented_record1, @augmented_record2, @augmented_record3]
+    @record1.test_id = @task.product_test.id
+    @record1.save!
+    @record2.test_id = @task.product_test.id
+    @record2.save!
+    @record3.test_id = @task.product_test.id
+    @record3.save!
+    @validator.validate(file, 'task' => @task)
+    errors = @validator.errors
+
+    error_details = { type: 'supplemental_data',
+                      population_key: 'IPP',
+                      population_id: 'E83A6DA6-D34E-4107-8431-2FB2C86738C7',
+                      data_type: 'SEX',
+                      code: 'F',
+                      expected_value: 3,
+                      reported_value: 1 }
+
+    # The should be error messages when reported value is outside of the expected range.  In this example, the range is 2-3.
+    assert_equal 3, errors.count { |e| e.validator_type == :result_validation && e.error_details == error_details }
   end
 
   def test_validate_extra_data
