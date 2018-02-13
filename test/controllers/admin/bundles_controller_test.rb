78feeef6442017f22c5ba25f5bae43f5f82fb60b
Robert Clark <rbclark@mitre.org>
2018-02-13 17:47:20 -0500

    Revert back to rails 4 way for test routes and set rubocop to know that we are using rails 4 instead of 5

--- test/controllers/admin/bundles_controller_test.rb
+++ test/controllers/admin/bundles_controller_test.rb
@@ -35,7 +35,7 @@ module Admin
 
         upload = Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'bundles', 'minimal_bundle.zip'), 'application/zip')
         perform_enqueued_jobs do
-          post :create, params: { file: upload }
+          post :create, file: upload
           assert_performed_jobs 2
           assert_equal orig_bundle_count + 1, Bundle.count, 'Should have added 1 new Bundle'
           assert orig_measure_count < Measure.count, 'Should have added new measures in the bundle'
@@ -47,7 +47,7 @@ module Admin
     test 'should deny access to import for non admin users ' do
       for_each_logged_in_user([USER, OWNER, VENDOR]) do
         upload = Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'bundles', 'minimal_bundle.zip'), 'application/zip')
-        post :create, params: { file: upload }
+        post :create, file: upload
         assert_response 401
       end
     end
@@ -59,7 +59,7 @@ module Admin
         upload = Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'artifacts', 'qrda.zip'), 'application/zip')
         assert_equal 0, BundleUploadJob.trackers.count, 'should be 0 trackers in db'
         perform_enqueued_jobs do
-          post :create, params: { file: upload }
+          post :create, file: upload
           assert_performed_jobs 1
           assert_equal 1, BundleUploadJob.trackers.count, 'should be 1 tracker in db'
           assert_equal :failed, BundleUploadJob.trackers.first.status, 'Status of tracker should be failed '
@@ -75,7 +75,7 @@ module Admin
         active_bundle = Bundle.default
         inactive_bundle = Bundle.where('$or' => [{ 'active' => false }, { :active.exists => false }]).sample
 
-        post :set_default, params: { id: inactive_bundle._id }
+        post :set_default, id: inactive_bundle._id
 
         active_bundle.reload
         inactive_bundle.reload
@@ -90,7 +90,7 @@ module Admin
         flunk 'Should have at least 2 test bundles' if Bundle.count < 2
         inactive_bundle = Bundle.where('$or' => [{ 'active' => false }, { :active.exists => false }]).sample
 
-        post :set_default, params: { id: inactive_bundle._id }
+        post :set_default, id: inactive_bundle._id
         assert_response 401
       end
     end
@@ -102,7 +102,7 @@ module Admin
         orig_record_count = Record.count
         id = '4fdb62e01d41c820f6000001'
 
-        delete :destroy, params: { id: id }
+        delete :destroy, id: id
 
         assert_equal 0, Bundle.where(_id: id).count, 'Should have deleted bundle'
         assert_equal orig_bundle_count - 1, Bundle.count, 'Should have deleted Bundle'
@@ -114,7 +114,7 @@ module Admin
     test 'should not allow non admins to remove bundle' do
       for_each_logged_in_user([USER, OWNER, VENDOR]) do
         id = Bundle.default._id
-        delete :destroy, params: { id: id }
+        delete :destroy, id: id
         assert_response 401
       end
     end
@@ -126,7 +126,7 @@ module Admin
         orig_record_count = Record.count
         id = '4fdb62e01d41c820f6000001'
 
-        post :deprecate, params: { id: id }
+        post :deprecate, id: id
 
         assert_equal 0, Bundle.available.where(_id: id).count, 'Should have deprecated bundle'
         assert_equal orig_bundle_count - 1, Bundle.available.count, 'Should have deprecated Bundle'
@@ -138,7 +138,7 @@ module Admin
     test 'should not allow non admins to deprecate bundle' do
       for_each_logged_in_user([USER, OWNER, VENDOR]) do
         id = Bundle.default._id
-        delete :deprecate, params: { id: id }
+        delete :deprecate, id: id
         assert_response 401
       end
     end
--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -24,7 +24,7 @@ module Admin
 
     test 'Admin can view edit screen ' do
       for_each_logged_in_user([ADMIN]) do
-        get :edit, params: { id: @user.id }
+        get :edit, id: @user.id
         assert_response :success
         assert assigns(:user)
       end
@@ -32,7 +32,7 @@ module Admin
 
     test 'Non Admin cannot view edit ' do
       for_each_logged_in_user([OWNER, ATL, VENDOR]) do
-        get :edit, params: { id: @user.id }
+        get :edit, id: @user.id
         assert_response 401
       end
     end
@@ -45,7 +45,7 @@ module Admin
       for_each_logged_in_user([ADMIN]) do
         assert !u.user_role?(:user)
         assert !u.user_role?(:owner, v)
-        patch :update, params: { :id => u.id, :role => :user, :assignments => { '0' => { :role => :owner, :vendor_id => v.id } } }
+        patch :update, :id => u.id, :role => :user, :assignments => { '0' => { :role => :owner, :vendor_id => v.id } }
         assert_response 302
         u.reload
         assert u.user_role?(:user)
@@ -55,7 +55,7 @@ module Admin
 
     test 'Non Admin cannot update user ' do
       for_each_logged_in_user([OWNER, ATL, VENDOR]) do
-        post :update, :params => { :id => @user.id }
+        post :update, :id => @user.id
         assert_response 401
       end
     end
@@ -63,7 +63,7 @@ module Admin
     test 'Admin can delete user' do
       u = User.create(:email => 'admin_test@test.com', :password => 'TestTest!', :password_confirmation => 'TestTest!', :terms_and_conditions => '1')
       for_each_logged_in_user([ADMIN]) do
-        delete :destroy, :params => { :id => u.id }
+        delete :destroy, :id => u.id
         assert_response 302
         assert_equal 0, User.where('_id' => u.id).count
       end
@@ -71,21 +71,21 @@ module Admin
 
     test 'Non Admin cannot delete user ' do
       for_each_logged_in_user([OWNER, ATL, VENDOR]) do
-        delete :destroy, :params => { :id => @user.id }
+        delete :destroy, :id => @user.id
         assert_response 401
       end
     end
 
     test 'Admin can send invitation ' do
       for_each_logged_in_user([ADMIN]) do
-        get :send_invitation, :params => { :id => @user.id }
+        get :send_invitation, :id => @user.id
         assert_response 302
       end
     end
 
     test 'Non Admin cannot  send invitation ' do
       for_each_logged_in_user([OWNER, ATL, VENDOR]) do
-        get :send_invitation, :params => { :id => @user.id }
+        get :send_invitation, :id => @user.id
         assert_response 401
       end
     end
@@ -94,10 +94,10 @@ module Admin
       u = User.create(:email => 'admin_test@test.com', :password => 'TestTest!', :password_confirmation => 'TestTest!', :terms_and_conditions => '1')
       approved = u.approved
       for_each_logged_in_user([ADMIN]) do
-        get :toggle_approved, :params => { :id => u.id }
+        get :toggle_approved, :id => u.id
         assert_response 302
         assert_equal !approved, User.find(u.id).approved
-        get :toggle_approved, :params => { :id => u.id }
+        get :toggle_approved, :id => u.id
         assert_response 302
         assert_equal approved, User.find(u.id).approved
       end
@@ -105,7 +105,7 @@ module Admin
 
     test 'Non Admin cannot toggle approved status ' do
       for_each_logged_in_user([OWNER, ATL, VENDOR]) do
-        get :toggle_approved, :params => { :id => @user.id }
+        get :toggle_approved,  :id => @user.id
         assert_response 401
       end
     end
@@ -114,7 +114,7 @@ module Admin
       u = User.create(:email => 'admin_test@test.com', :password => 'TestTest!', :password_confirmation => 'TestTest!', :terms_and_conditions => '1', :locked_at => Time.now.in_time_zone)
       for_each_logged_in_user([ADMIN]) do
         assert !u.locked_at.nil?
-        get :unlock, :params => { :id => u.id }
+        get :unlock, :id => u.id
         assert_response 302
         assert User.find(u.id).locked_at.nil?
       end
@@ -122,7 +122,7 @@ module Admin
 
     test 'Non Admin cannot unlock a locked account' do
       for_each_logged_in_user([OWNER, ATL, VENDOR]) do
-        get :unlock, :params => { :id => @user.id }
+        get :unlock, :id => @user.id
         assert_response 401
       end
     end
