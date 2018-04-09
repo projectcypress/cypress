c0a863459e6a81c052aae4141adbabfb7d542d32
Czulada, David S <dczulada@mitre.org>
2018-04-09 10:49:06 -0400

    Increase test coverage

--- test/helpers/checklist_tests_helper_test.rb
+++ test/helpers/checklist_tests_helper_test.rb
@@ -23,4 +23,23 @@ class ChecklistTestsHelperTest < ActiveSupport::TestCase
     field_value_mixed['low'] = field_value_low['low']
     assert_equal '90 d < stay &#8804; 120 d', length_of_stay_string(field_value_mixed)
   end
+
+  def test_checklist_test_criteria_attribute
+    measure = FactoryGirl.create(:static_measure)
+    c1 = {}
+    c2 = {}
+    c3 = {}
+
+    c1[:negation] = true
+    assert_equal 'Negation Code', checklist_test_criteria_attribute(measure, c1)
+
+    c2[:value] = { type: 'IVL_PQ',
+                   high: { type: 'PQ',
+                           unit: 'mg/dL',
+                           value: '100' } }
+    assert_equal 'Result', checklist_test_criteria_attribute(measure, c2)
+
+    c3[:value] = { type: 'CD', system: 'Administrative Sex', code: 'F' }
+    assert_equal '', checklist_test_criteria_attribute(measure, c3)
+  end
 end
