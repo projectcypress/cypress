1c61662b846d332c496668738694a07ad7c7a66d
Czulada, David S <dczulada@mitre.org>
2018-05-03 09:16:20 -0400

    Expected Results calculator updated to store data for measures with sub measures

--- app/jobs/measure_evaluation_job.rb
+++ app/jobs/measure_evaluation_job.rb
@@ -18,28 +18,14 @@ class MeasureEvaluationJob < ApplicationJob
 
   def perform_for_product_test(product_test, options)
     results = eval_measures(product_test.measures, product_test, options)
-
     product_test.expected_results = results
     product_test.save
   end
 
   def eval_measures(measures, product_test, options, &_block)
-    results = {}
-    erc = Cypress::ExpectedResultsCalculator.new(product_test.patients)
-    measures.each_with_index do |measure|
-      individual_results = IndividualResult.where('measure' => measure.id, 'extended_data.correlation_id' => product_test.id.to_s)
-      results[measure.key] = erc.aggregate_results(individual_results, measure.population_ids)
-      results[measure.key]['measure_id'] = measure.hqmf_id
-      create_query_cache_object(results[measure.key], product_test, measure)
-    end
+    erc = Cypress::ExpectedResultsCalculator.new(product_test)
+    results = erc.aggregate_results_for_measures(measures)
     results
-  end
-
-  def create_query_cache_object(result, product_test, measure)
-    result['test_id'] = product_test.id
-    result['effective_date'] = product_test.effective_date
-    result['sub_id'] = measure.sub_id if measure.sub_id
-    Mongoid.default_client["query_cache"].insert_one(result)
   end
 
 end
--- lib/cypress/expected_results_calculator.rb
+++ lib/cypress/expected_results_calculator.rb
@@ -1,10 +1,11 @@
 module Cypress
   class ExpectedResultsCalculator
 
-    def initialize(patients)
+    def initialize(product_test)
+      @product_test = product_test
       @patient_sup_map = {}
       @measure_result_hash = {}
-      patients.each do |patient|
+      @product_test.patients.each do |patient|
         @patient_sup_map[patient.id] = {}
         @patient_sup_map[patient.id]['SEX'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.55')[0].dataElementCodes[0].code
         @patient_sup_map[patient.id]['RACE'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.59')[0].dataElementCodes[0].code
@@ -13,39 +14,58 @@ module Cypress
       end
     end
 
-    def aggregate_results(individual_results, population_ids)
-      @measure_result_hash['population_ids'] = population_ids
-      measure_populations = %w[DENOM NUMER DENEX DENEXCEP IPP MSRPOPL MSRPOPLEX]
-      @measure_result_hash['supplemental_data'] = {}
-      measure_populations.each do |pop|
-        @measure_result_hash[pop] = 0
-      end
-      individual_results.each do |ir|
-        measure_populations.each do |pop|
-          next if ir[pop].nil? || ir[pop] == 0
-          @measure_result_hash[pop] += ir[pop]
-          increment_sup_info(@patient_sup_map[ir.patient], pop)
-        end
+
+    def aggregate_results_for_measures(measures)
+      measures.each do |measure|
+        @measure_result_hash[measure.key] = {}
+        aggregate_results_for_measure(measure)
       end
       @measure_result_hash
     end
 
-    def increment_sup_info(patient_sup, pop)
-      if !@measure_result_hash['supplemental_data'][pop]
-        @measure_result_hash['supplemental_data'][pop] = { 'RACE' => {}, 'ETHNICITY' => {}, 'SEX' => {}, 'PAYER' => {} }
+    def aggregate_results_for_measure(measure)
+      individual_results = QDM::IndividualResult.where('measure_id' => measure._id, 'extendedData.correlation_id' => @product_test.id.to_s)
+      measure_populations = %w[DENOM NUMER DENEX DENEXCEP IPP MSRPOPL MSRPOPLEX]
+      @measure_result_hash[measure.key]['supplemental_data'] = {}
+      measure_populations.each do |pop|
+        @measure_result_hash[measure.key][pop] = 0
+      end
+      individual_results.each do |ir|
+        measure_populations.each do |pop|
+          next if ir[pop].nil? || ir[pop] == 0
+          @measure_result_hash[measure.key][pop] += ir[pop]
+          increment_sup_info(@patient_sup_map[ir.patient_id], pop, @measure_result_hash[measure.key])
+        end
+      end
+      @measure_result_hash[measure.key]['measure_id'] = measure.hqmf_id
+      @measure_result_hash[measure.key]['population_ids'] = measure.population_ids
+      create_query_cache_object(@measure_result_hash[measure.key], measure)
+    end
+
+    def increment_sup_info(patient_sup, pop, single_measure_result_hash)
+      if !single_measure_result_hash['supplemental_data'][pop]
+        single_measure_result_hash['supplemental_data'][pop] = { 'RACE' => {}, 'ETHNICITY' => {}, 'SEX' => {}, 'PAYER' => {} }
       end
       patient_sup.keys.each do |sup_type|
-        add_or_increment_code(pop, sup_type, patient_sup[sup_type])
+        add_or_increment_code(pop, sup_type, patient_sup[sup_type], single_measure_result_hash)
       end
     end
 
-    def add_or_increment_code(pop, sup_type, code)
-      if @measure_result_hash['supplemental_data'][pop][sup_type][code]
-        @measure_result_hash['supplemental_data'][pop][sup_type][code] += 1
+    def add_or_increment_code(pop, sup_type, code, single_measure_result_hash)
+      if single_measure_result_hash['supplemental_data'][pop][sup_type][code]
+        single_measure_result_hash['supplemental_data'][pop][sup_type][code] += 1
       else
-        @measure_result_hash['supplemental_data'][pop][sup_type][code] = 1
+        single_measure_result_hash['supplemental_data'][pop][sup_type][code] = 1
       end
     end
+
+    def create_query_cache_object(result, measure)
+      qco = result
+      qco['test_id'] = @product_test.id
+      qco['effective_date'] = @product_test.effective_date
+      qco['sub_id'] = measure.sub_id if measure.sub_id
+      Mongoid.default_client["query_cache"].insert_one(qco)
+    end
+
   end
  end
-
