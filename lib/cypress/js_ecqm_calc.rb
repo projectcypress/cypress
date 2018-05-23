9bc515b4a82668a5c538edb8cc619ae673f6b8d8
Michael O'Keefe <mokeefe@mitre.org>
2018-05-23 12:05:05 -0400

    Refactored JsEcqmCalc to supply patient_ids and measure_ids in the job calls, rather than the initializer. Also fixed a number of Rubocop issues

--- lib/cypress/js_ecqm_calc.rb
+++ lib/cypress/js_ecqm_calc.rb
@@ -1,80 +1,79 @@
 require 'bunny'
 require 'json'
+require 'securerandom'
 
 module Cypress
-    class JsEcqmCalc
-        
-        attr_accessor :patient_ids, :measure_ids, :options, :lock, :response,
-        :connection, :channel, :queue, :reply_queue, :exchange, :call_id, :condition
-        
-        def initialize (patient_ids, measure_ids, options)
-            # @hds_record_converter = CQM::Converter::HDSRecord.new
-            @connection = Bunny.new(automatically_recover: false)
-            @connection.start
-            @channel = connection.create_channel
-            @queue=channel.queue('calculation_queue', durable: true)
-            
-            @patient_ids = patient_ids
-            @measure_ids = measure_ids
-            @options = options
-        end
-        
-        def sync_job
-            setup_reply_queue
-            @exchange = @channel.default_exchange
+  class JsEcqmCalc
+    attr_accessor :patient_ids, :measure_ids, :options, :lock, :response,
+                  :connection, :channel, :queue, :reply_queue, :exchange, :call_id, :condition
 
-            @call_id = SecureRandom.uuid
-            
-            message = JSON.dump({ patient_ids: @patient_ids,
-                                  measure_ids: @measure_ids,
-                                  options: @options,
-                                  type: 'sync',
-                                  reply_to: @reply_queue.name })
-            
-            @exchange.publish(message,
-                routing_key: 'calculation_queue',
-                correlation_id: @call_id,
-                reply_to: @reply_queue.name)
-                
-                @lock.synchronize { @condition.wait(@lock) }
-                
-                response
-            end 
-            
-            def async_job
-                message = JSON.dump({ patient_ids: @patient_ids,
-                                      measure_ids: @measure_ids,
-                                      options: @options,
-                                      type: 'async' })
-                @queue.publish(message, persistent: true)
-                
-                puts " [x] Sent #{message}"
-            end 
-            
-            def stop
-                @channel.close
-                @connection.close
-            end
-            
-            private
-            
-            def setup_reply_queue
-                @lock = Mutex.new
-                @condition = ConditionVariable.new
-                that = self
-                @reply_queue = channel.queue('', exclusive: true)
-                
-                @reply_queue.subscribe do |_delivery_info, properties, payload|
-                    if properties[:correlation_id] == that.call_id
-                        that.response = payload.to_s
-                        
-                        # sends the signal to continue the execution of #call
-                        that.lock.synchronize { that.condition.signal }
-                    end
-                end
-            end
-            
-            
-        end
+    def initialize(options)
+      # @hds_record_converter = CQM::Converter::HDSRecord.new
+      @connection = Bunny.new(automatically_recover: false)
+      @connection.start
+      @channel = connection.create_channel
+      @queue = channel.queue('calculation_queue', durable: true)
+
+      @options = options
     end
-        
+
+    def sync_job(patient_ids, measure_ids)
+      @patient_ids = patient_ids
+      @measure_ids = measure_ids
+      setup_reply_queue
+      @exchange = @channel.default_exchange
+
+      @call_id = SecureRandom.uuid
+
+      message = JSON.dump(patient_ids: @patient_ids,
+                          measure_ids: @measure_ids,
+                          options: @options,
+                          type: 'sync',
+                          reply_to: @reply_queue.name)
+
+      @exchange.publish(message,
+                        routing_key: 'calculation_queue',
+                        correlation_id: @call_id,
+                        reply_to: @reply_queue.name)
+
+      @lock.synchronize { @condition.wait(@lock) }
+
+      response
+    end
+
+    def async_job(patient_ids, measure_ids)
+      @patient_ids = patient_ids
+      @measure_ids = measure_ids
+      message = JSON.dump(patient_ids: @patient_ids,
+                          measure_ids: @measure_ids,
+                          options: @options,
+                          type: 'async')
+      @queue.publish(message, persistent: true)
+
+      puts " [x] Sent #{message}"
+    end
+
+    def stop
+      @channel.close
+      @connection.close
+    end
+
+    private
+
+    def setup_reply_queue
+      @lock = Mutex.new
+      @condition = ConditionVariable.new
+      that = self
+      @reply_queue = channel.queue('', exclusive: true)
+
+      @reply_queue.subscribe do |_delivery_info, properties, payload|
+        if properties[:correlation_id] == that.call_id
+          that.response = payload.to_s
+
+          # sends the signal to continue the execution of #call
+          that.lock.synchronize { that.condition.signal }
+        end
+      end
+    end
+  end
+end
