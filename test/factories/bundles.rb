68fcd92edbbc953622bd8f05aa971fc9a8660740
Laura <laclark@mitre.org>
2018-05-24 15:51:08 -0400

    reformated measure, made oids correlate to valuesets, added new id names to bundles.rb and added a version to valuesets

--- test/factories/bundles.rb
+++ test/factories/bundles.rb
@@ -5,6 +5,8 @@ FactoryBot.define do
     sequence(:title) { |i| "Bundle Name #{i}" }
 
     factory :static_bundle do
+      entry = Rails.root.join('test', 'fixtures', 'artifacts', 'cms127v7.json')
+      source_measure = JSON.parse(File.read(entry),:max_nesting => 100)
       active true
       done_importing true
       name 'Static Bundle'
@@ -24,9 +26,29 @@ FactoryBot.define do
                                                             'value' => BSON::Code.new(fn) }, upsert: true)
         end
 
+        # Include 40 valuesets
+        40.times do |count|
+          create(:value_set, seq_id: count, bundle: bundle)
+        end
+        create(:value_set_payer, bundle: bundle)
+
         # Always include a complete measure (BE65090C-EB1F-11E7-8C3F-9A214CF093AE)
         measure = create(:static_measure, bundle_id: bundle._id)
+        measure['value_set_oid_version_objects'] = source_measure['value_set_oid_version_objects']
+        measure['elm_annotations'] = source_measure['elm_annotations']
+        measure['observations'] = source_measure['observations']
+        measure['elm'] = source_measure['elm']
+        measure['main_cql_library'] = source_measure['main_cql_library']
+        measure['cql_statement_dependencies'] = source_measure['cql_statement_dependencies']
+        measure['populations_cql_map'] = source_measure['populations_cql_map']
         measure['id'] = measure.hqmf_id
+        
+        # TODO find object ids for all of the oids in the measure
+        valueset_id_list = []
+        measure['value_set_oid_version_objects'].each do |vsv|
+          #valueset_id_list << HealthDataStandards::SVS::ValueSet.where(:oid => vsv.oid, :version => vsv.version).first.id
+        end
+        measure['value_sets'] = valueset_id_list
         measure.save
 
         # Always include a random measure with a diagnosis
@@ -40,12 +62,6 @@ FactoryBot.define do
           random_measure['id'] = random_measure.hqmf_id
           random_measure.save
         end
-
-        # Include 40 valuesets
-        40.times do |count|
-          create(:value_set, seq_id: count, bundle: bundle)
-        end
-        create(:value_set_payer, bundle: bundle)
 
         # Include a record that will evaluate against the static measure
         9.times do
