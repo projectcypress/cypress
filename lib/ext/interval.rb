75b9c348b5b0f4f5642dc18070454f16520ef489
laurend <laurend@mitre.org>
2018-05-29 08:44:07 -0400

    pass effective date to engine, enable date shift, and use cqm-models date shift
    # Conflicts:
    #	app/jobs/product_test_setup_job.rb
    #	app/views/products/_product_form.html.erb
    #	lib/cypress/population_clone_job.rb
    #	lib/ext/interval.rb
    #	lib/ext/patient.rb

--- lib/ext/interval.rb
+++ lib/ext/interval.rb
@@ -1,12 +1,5 @@
 # The Interval model is an extension of app/models/qdm/basetypes/interval.rb as defined by CQM-Models.
 module QDM
   class Interval
-    def shift_dates(date_diff)
-      formatstr = '%FT%T%:z'
-      low_date = DateTime.parse(low).utc
-      high_date = DateTime.parse(high).utc
-      low = low.nil? ? nil : (low_date + date_diff).strftime(formatstr)
-      high = high.nil? ? nil : (high_date + date_diff).strftime(formatstr)
-    end
   end
 end
