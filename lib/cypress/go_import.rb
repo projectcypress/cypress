d93283f9e7b6d7bbba2faeed61fb7430ec49c834
Czulada, David S <dczulada@mitre.org>
2018-06-25 15:22:39 -0400

    Use cqm-parsers qrda import.  import/export of references

--- lib/cypress/go_import.rb
+++ lib/cypress/go_import.rb
@@ -1,20 +1,22 @@
 module Cypress
   module GoImport
-    def self.replace_negated_codes(record, bundle)
-      record.entries.each do |entry|
-        select_negated_code(entry, bundle) if entry.negationInd && entry.codes.key?('NA_VALUESET') && entry.codes.size == 1
+    def self.replace_negated_codes(patient, bundle)
+      patient.dataElements.each do |de|
+        select_negated_code(de, bundle) if de['negationRationale'] && de.codes.find { |c| c.codeSystem == 'NA_VALUESET' }
       end
     end
 
-    def self.select_negated_code(entry, bundle)
-      negated_vs = entry.codes['NA_VALUESET'].first
+    def self.select_negated_code(data_element, bundle)
+      negated_element = data_element.dataElementCodes.map { |dec| dec if dec.codeSystem == 'NA_VALUESET' }.first
+      negated_vs = negated_element.code
       # If Cypress has a default code selected, use it.  Otherwise, use the first in the valueset.
-      if bundle.default_negation_codes && bundle.default_negation_codes[negated_vs]
-        entry.add_code(bundle.default_negation_codes[negated_vs]['code'], bundle.default_negation_codes[negated_vs]['codeSystem'])
-      else
-        valueset = HealthDataStandards::SVS::ValueSet.where(oid: entry.codes['NA_VALUESET'].first, bundle_id: bundle.id)
-        entry.add_code(valueset.first.concepts.first['code'], valueset.first.concepts.first['code_system_name'])
-      end
+      negated_code = if bundle.default_negation_codes && bundle.default_negation_codes[negated_vs]
+                       QDM::Code.new(bundle.default_negation_codes[negated_vs]['code'], bundle.default_negation_codes[negated_vs]['codeSystem'])
+                     else
+                       valueset = HealthDataStandards::SVS::ValueSet.where(oid: negated_vs, bundle_id: bundle.id)
+                       QDM::Code.new(valueset.first.concepts.first['code'], valueset.first.concepts.first['code_system_name'])
+                     end
+      data_element.dataElementCodes << negated_code
     end
   end
 end
