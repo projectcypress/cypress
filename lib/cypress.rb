4bd6d2ea4efe117d25d3a2287708316ed2174858
Czulada, David S <dczulada@mitre.org>
2018-06-26 14:11:39 -0400

    Render Data Criteria using mustache

--- lib/cypress.rb
+++ lib/cypress.rb
@@ -1,4 +1,5 @@
 Dir[File.dirname(__FILE__) + '/cypress/*.rb'].each { |file| require file }.each { |file| require file }
+Dir[File.dirname(__FILE__) + '/cypress/patient_export/*.rb'].each { |file| require file }.each { |file| require file }
 Dir[File.dirname(__FILE__) + '/ext/*.rb'].each { |file| require file }.each { |file| require file }
 Dir[File.dirname(__FILE__) + '/validators/*.rb'].each { |file| require file }.each { |file| require file }
 require_relative 'job_status.rb'
