757847b6f0b6f462e767dfdf49e318b01e4564af
David Czulada <dczulada@mitre.org>
2018-10-05 15:54:57 -0400

    measure_evaluation_job should use the results returned from sync_job instead of searching the db

--- lib/cypress/expected_results_calculator.rb
+++ lib/cypress/expected_results_calculator.rb
@@ -1,34 +1,47 @@
 module Cypress
   class ExpectedResultsCalculator
+
+    # The ExpectedResultsCalculator aggregates Individual Results to calculated the expected results for a
+    # Measure Test or Task 
+    #
+    # @param [Array] patients the list of patients that are included in the aggregate results
+    # @param [String] correlation_id the id used to associate a group of patients
+    # @param [String] effective_date used when generating the query_cache_object for HDS QRDA Cat III export
+    # @param [Hash] options :individual_results are the raw results from JsEcqmCalc
     def initialize(patients, correlation_id, effective_date)
       @correlation_id = correlation_id
+      # Hash of patient_id and their supplemental information
       @patient_sup_map = {}
       @measure_result_hash = {}
       @effective_date = effective_date
       patients.each do |patient|
+        # iterate through each patient and store their supplemental information
         add_patient_to_sup_map(@patient_sup_map, patient)
       end
     end
 
     def add_patient_to_sup_map(ps_map, patient)
-      ps_map[patient.id] = {}
-      ps_map[patient.id]['SEX'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.55')[0].dataElementCodes[0].code
-      ps_map[patient.id]['RACE'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.59')[0].dataElementCodes[0].code
-      ps_map[patient.id]['ETHNICITY'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.56')[0].dataElementCodes[0].code
-      ps_map[patient.id]['PAYER'] = JSON.parse(patient.extendedData.insurance_providers).first['codes']['SOP'].first
+      patient_id = patient.id.to_s
+      ps_map[patient_id] = {}
+      ps_map[patient_id]['SEX'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.55')[0].dataElementCodes[0].code
+      ps_map[patient_id]['RACE'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.59')[0].dataElementCodes[0].code
+      ps_map[patient_id]['ETHNICITY'] = patient.get_by_hqmf_oid('2.16.840.1.113883.10.20.28.3.56')[0].dataElementCodes[0].code
+      ps_map[patient_id]['PAYER'] = JSON.parse(patient.extendedData.insurance_providers).first['codes']['SOP'].first
     end
 
-    def aggregate_results_for_measures(measures)
+    def aggregate_results_for_measures(measures, individual_results = nil)
       measures.each do |measure|
         @measure_result_hash[measure.key] = {}
-        aggregate_results_for_measure(measure)
+        measure_individual_results = nil
+        measure_individual_results = individual_results[measure.id.to_s].values if individual_results
+        aggregate_results_for_measure(measure, measure_individual_results)
       end
       @measure_result_hash
     end
 
     # rubocop:disable Metrics/AbcSize
-    def aggregate_results_for_measure(measure)
-      individual_results = QDM::IndividualResult.where('measure_id' => measure._id, 'extendedData.correlation_id' => @correlation_id)
+    def aggregate_results_for_measure(measure, individual_results = nil)
+      individual_results ||= QDM::IndividualResult.where('measure_id' => measure._id, 'extendedData.correlation_id' => @correlation_id)
       measure_populations = %w[DENOM NUMER DENEX DENEXCEP IPP MSRPOPL MSRPOPLEX]
       @measure_result_hash[measure.key]['supplemental_data'] = {}
       measure_populations.each do |pop|
@@ -39,10 +52,9 @@ module Cypress
         measure_populations.each do |pop|
           next if ir[pop].nil? || ir[pop].zero?
           @measure_result_hash[measure.key][pop] += ir[pop]
-          increment_sup_info(@patient_sup_map[ir.patient_id], pop, @measure_result_hash[measure.key])
+          increment_sup_info(@patient_sup_map[ir.patient_id.to_s], pop, @measure_result_hash[measure.key])
         end
-
-        observ_values.concat get_observ_values(ir.episode_results) if ir.episode_results
+        observ_values.concat get_observ_values(ir['episode_results']) if ir['episode_results']
       end
       @measure_result_hash[measure.key]['OBSERV'] = median(observ_values.reject(&:nil?))
       @measure_result_hash[measure.key]['measure_id'] = measure.hqmf_id
