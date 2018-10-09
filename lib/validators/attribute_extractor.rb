27b989a1574add972d420a38a82f59bd499ec64e
David Czulada <dczulada@mitre.org>
2018-10-09 16:38:37 -0400

    Relative paths for c1 record sample tests are problematic when finding times https://oncprojectracking.healthit.gov/support/browse/CYPRESS-1492

--- lib/validators/attribute_extractor.rb
+++ lib/validators/attribute_extractor.rb
@@ -1,11 +1,11 @@
 module Validators
   module AttributeExtractor
     XPATH_CONSTS = {
-      'relevantPeriod' => './cda:effectiveTime/cda:low',
-      'prevalencePeriod' => './cda:effectiveTime/cda:low',
-      'authorDatetime' => "../../..//cda:author[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.155']]/cda:time",
-      'relatedTo' => "./sdtc:inFulfillmentOf1/sdtc:templateId[@root='2.16.840.1.113883.10.20.24.3.126']",
-      'resultDatetime' => "./cda:observation[./cda:templateId[@root='2.16.840.1.113883.10.20.22.4.2']]/cda:effectiveTime"
+      'relevantPeriod' => 'cda:effectiveTime/cda:low',
+      'prevalencePeriod' => 'cda:effectiveTime/cda:low',
+      'authorDatetime' => "/cda:author[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.155']]/cda:time",
+      'relatedTo' => "sdtc:inFulfillmentOf1/sdtc:templateId[@root='2.16.840.1.113883.10.20.24.3.126']",
+      'resultDatetime' => "cda:entryRelationship/cda:observation[./cda:templateId[@root='2.16.840.1.113883.10.20.22.4.2']]/cda:effectiveTime"
     }.freeze
 
     # does a node need to be checked for an atttibute value
@@ -26,26 +26,46 @@ module Validators
     def find_attribute_values(node, code, source_criteria, index)
       # xpath expressions with codes
       xpath_map = {
-        'components' => "./cda:entryRelationship/cda:observation[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.149]/cda:code[@code='#{code}']",
-        'diagnoses' => "./cda:entryRelationship/cda:act[./cda:code[@code='29308-4']]/cda:entryRelationship
+        'components' => "cda:entryRelationship/cda:observation[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.149]/cda:code[@code='#{code}']",
+        'diagnoses' => "cda:entryRelationship/cda:act[./cda:code[@code='29308-4']]/cda:entryRelationship
                        /cda:observation/cda:value[@code='#{code}']",
-        'dischargeDisposition' => "./sdtc:dischargeDispositionCode[@code='#{code}']", # CMS31v5
-        'facilityLocations' => "./cda:participant[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.100']]
+        'dischargeDisposition' => "sdtc:dischargeDispositionCode[@code='#{code}']", # CMS31v5
+        'facilityLocations' => "cda:participant[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.100']]
                                /cda:participantRole/cda:code[@code='#{code}']",
-        'admissionSource' => "./cda:participant/cda:participantRole[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.151']]
-                             /cda:code[@code='#{code}']",
-        'route' => "../../../cda:routeCode[@code='#{code}']",
-        'ordinality' => "./cda:entryRelationship/cda:observation[./cda:code[@code='260870009']]/cda:value[@code='#{code}']
-                     or ./cda:priorityCode[@code='#{code}']", 'anatomicalLocationSite' => "./cda:targetSiteCode[@code='#{code}']",
-        'principalDiagnosis' => "./cda:entryRelationship/cda:observation[./cda:code[@code='8319008']]/cda:value[@code='#{code}']", # CMS188v6
-        'severity' => "./cda:entryRelationship/cda:observation[./cda:templateId[@root='2.16.840.1.113883.10.20.22.4.8']]/cda:value[@code='#{code}']"
+        'admissionSource' => "cda:participant/cda:participantRole[./cda:templateId[@root='2.16.840.1.113883.10.20.24.3.151']]
+                             /cda:code[@code='#{code}']", 'route' => "cda:routeCode[@code='#{code}']",
+        'ordinality' => "cda:entryRelationship/cda:observation[./cda:code[@code='260870009']]/cda:value[@code='#{code}']
+                     or cda:priorityCode[@code='#{code}']", 'anatomicalLocationSite' => "cda:targetSiteCode[@code='#{code}']",
+        'principalDiagnosis' => "cda:entryRelationship/cda:observation[./cda:code[@code='8319008']]/cda:value[@code='#{code}']", # CMS188v6
+        'severity' => "cda:entryRelationship/cda:observation[./cda:templateId[@root='2.16.840.1.113883.10.20.22.4.8']]/cda:value[@code='#{code}']"
       }
       # XPATH_CONSTS are the expressions without codes
       xpath_map.merge!(XPATH_CONSTS)
       if source_criteria['attributes']
-        return node.xpath(xpath_map[source_criteria.attributes[index].attribute_name]).blank? ? false : true
+        relative_path = relative_path_to_template_root(source_criteria['definition'])
+        return node.xpath(relative_path + xpath_map[source_criteria.attributes[index].attribute_name]).blank? ? false : true
       end
       false
+    end
+
+    def relative_path_to_template_root(definition)
+      relative_path_map = { 'adverse_event' => '../../',
+                            'allergy_intolerance' => '../../../',
+                            'assessment' => './',
+                            'communication_from_patient_to_provider' => './',
+                            'communication_from_provider_to_patient' => './',
+                            'communication_from_provider_to_provider' => './',
+                            'device' => '../../../',
+                            'diagnosis' => './',
+                            'diagnostic_study' => './',
+                            'encounter' => './',
+                            'immunization' => '../../../',
+                            'intervention' => './',
+                            'laboratory_test' => './',
+                            'medication' => '../../../',
+                            'physical_exam' => './',
+                            'procedure' => './' }
+      relative_path_map[definition]
     end
   end
 end
