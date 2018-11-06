02508509069d7b9bd52089dedcc0f7f54fef733c
Matt Mayer <mmayer@mitre.org>
2018-11-06 13:38:00 -0500

    Add user to test execution in cat1_filter_task_test

--- test/models/cat1_filter_task_test.rb
+++ test/models/cat1_filter_task_test.rb
@@ -13,11 +13,12 @@ class Cat1FilterTaskTest < ActiveSupport::TestCase
   end
 
   def test_task_good_results_should_pass
+    user = User.create(email: 'testvendor@test.com', password: 'TestTest!', password_confirmation: 'TestTest!', terms_and_conditions: '1')
     task = @product_test.tasks.create({}, Cat1FilterTask)
     testfile = Tempfile.new(['good_results_debug_file', '.zip'])
     testfile.write task.good_results
     perform_enqueued_jobs do
-      te = task.execute(testfile, User.first)
+      te = task.execute(testfile, user)
       te.reload
       assert_empty te.execution_errors, 'test execution with known good results should have no errors'
     end
