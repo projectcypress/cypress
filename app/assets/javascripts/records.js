b1c68146314e3e288c0b2b87630710e66fe9746c
Robert Clark <rbclark@mitre.org>
2018-12-21 13:56:04 -0500

    The form element generated by rails which we use to switch the active bundle has changed. Updating element accordingly.

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -5,7 +5,7 @@ ready = function() {
   // when the user selects a different bundle
   // just take them to the new page
   // use Turbolinks so it doesn't full refresh
-  $('.btn-checkbox input[name="[bundle_id]"]').on('change', function() {
+  $('label.btn-checkbox input[name="bundle_id"]').on('change', function() {
     var bundle_id = $(this).val();
     Turbolinks.visit("/bundles/"+bundle_id+"/records");
   });
