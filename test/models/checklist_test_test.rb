14f0c229a5225b00edaa018f97a51911363f7c9d
David Czulada <dczulada@mitre.org>
2018-12-24 14:25:49 -0500

    Cypress_394 - Record Sample tests included attibutes that could not be validated.

--- test/models/checklist_test_test.rb
+++ test/models/checklist_test_test.rb
@@ -13,6 +13,24 @@ class ChecklistTestTest < ActiveJob::TestCase
     assert @test.checked_criteria?
   end
 
+  def test_attribute_index
+    attributes1 = [{ 'attribute_name' => 'relevantPeriod', 'attribute_valueset' => nil },
+                   { 'attribute_name' => 'dischargeDisposition', 'attribute_valueset' => '1.1.2.3' }]
+    attributes2 = [{ 'attribute_name' => 'relevantPeriod', 'attribute_valueset' => nil },
+                   { 'attribute_name' => 'dischargeDisposition', 'attribute_valueset' => nil }]
+    attributes3 = [{ 'attribute_name' => 'relevantPeriod', 'attribute_valueset' => nil },
+                   { 'attribute_name' => 'dischargeDisposition', 'attribute_valueset' => nil },
+                   { 'attribute_name' => 'dischargeDisposition', 'attribute_valueset' => '1.1.2.3' }]
+    checklist_measure = @test.measures.first
+    checklist_measure.source_data_criteria['fake_dc_1'] = { 'attributes' => attributes1 }
+    checklist_measure.source_data_criteria['fake_dc_2'] = { 'attributes' => attributes2 }
+    checklist_measure.source_data_criteria['fake_dc_3'] = { 'attributes' => attributes3 }
+    checklist_measure.save
+    assert_equal 1, @test.attribute_index?(checklist_measure, 'fake_dc_1'), 'should return index for dischargeDisposition with a valueset oid'
+    assert_equal 0, @test.attribute_index?(checklist_measure, 'fake_dc_2'), 'should return index for relevantPeriod'
+    assert_equal 2, @test.attribute_index?(checklist_measure, 'fake_dc_3'), 'should return index for dischargeDisposition with a valueset oid'
+  end
+
   def test_create_checked_criteria
     @test.create_checked_criteria
     assert @test.checked_criteria.count.positive?, 'should create checked criteria for one measure'
