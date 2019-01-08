e790b29f40862dd65dfe7f42c0e6844b6236e05b
Michael O'Keefe <mokeefe@mitre.org>
2019-01-08 16:45:30 -0500

    Apparently we weren't require-ing this one file

--- lib/cypress.rb
+++ lib/cypress.rb
@@ -2,4 +2,5 @@ Dir[File.dirname(__FILE__) + '/cypress/*.rb'].each { |file| require file }.each 
 Dir[File.dirname(__FILE__) + '/cypress/patient_export/*.rb'].each { |file| require file }.each { |file| require file }
 Dir[File.dirname(__FILE__) + '/ext/*.rb'].each { |file| require file }.each { |file| require file }
 Dir[File.dirname(__FILE__) + '/validators/*.rb'].each { |file| require file }.each { |file| require file }
+require_relative 'bootstrap_breadcrumbs_builder.rb'
 require_relative 'job_status.rb'
