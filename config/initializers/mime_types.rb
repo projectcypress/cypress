5f41a6070279d4ea9aae88ca9df2c1b45dc27f26
Robert Clark <rbclark@mitre.org>
2019-01-08 14:28:50 -0500

    Upgrade to Ruby 2.6 and Rails 5.2

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,9 +2,3 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-
-# Mime::Type.register 'application/vnd.api+json', :json_api
-# ActionController::Renderers.add :json_api do |obj, options|
-#   self.content_type ||= Mime[:json_api]
-#   obj
-# end
