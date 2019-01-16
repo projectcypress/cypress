c67d5cf4a02671e11c0796bb00813e31dc95af08
Robert Clark <rbclark@mitre.org>
2019-01-16 15:34:09 -0500

    Fix eager loading in production

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Cypress
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += Dir["#{config.root}/lib/"]
+    config.eager_load_paths << Rails.root.join('lib')
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # prevent rails from wrapping inputs with errors in a div of class "field_with_errors"
