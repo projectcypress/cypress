d524bc892ffc42833835ae50541d68591a444b2c
Robert Clark <rbclark@mitre.org>
2019-01-29 17:12:15 -0500

    Add tests to verify that lookup_codevalues is working correctly

--- test/helpers/checklist_tests_helper_test.rb
+++ test/helpers/checklist_tests_helper_test.rb
@@ -3,6 +3,15 @@ require 'test_helper'
 class ChecklistTestsHelperTest < ActiveSupport::TestCase
   include ChecklistTestsHelper
 
+  # This ensures that the filtering on lookup_codevalues properly limits results
+  # to a specific bundle when specified
+  def test_lookup_codevalues_multiple_bundles
+    @bundle1 = FactoryBot.create(:static_bundle)
+    @bundle2 = FactoryBot.create(:bundle)
+
+    assert_empty lookup_codevalues(@bundle1.value_sets.first.oid, @bundle2)
+  end
+
   def test_checklist_test_criteria_attribute
     c1 = {}
     c2 = {}
