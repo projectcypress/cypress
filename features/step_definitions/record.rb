11a5541830abe34dc628eefcb792e7161e469a5e
Dave Czulada <dczulada@mitre.org>
2019-03-26 08:45:23 -0400

    update cucumber test to look for the new display name

--- features/step_definitions/record.rb
+++ features/step_definitions/record.rb
@@ -68,7 +68,7 @@ And(/^the user selects a measure from the dropdown$/) do
 end
 
 Then(/^the user should see results for that measure$/) do
-  page.assert_text @measure.description + ' Patients'
+  page.assert_text measure_display_name(@measure, @measure.population_sets_and_stratifications_for_measure.first) + ' Patients'
 
   records = records_by_measure(@bundle.patients, @measure)
 
