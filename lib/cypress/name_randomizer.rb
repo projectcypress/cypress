b233203bf09b20de5916cecb3dcce4d796023a35
Dave Czulada <dczulada@mitre.org>
2019-04-29 09:44:41 -0400

    Verify that a new name does not conflict with an Augmented Record

--- lib/cypress/name_randomizer.rb
+++ lib/cypress/name_randomizer.rb
@@ -1,41 +1,54 @@
 module Cypress
   class NameRandomizer
     def self.randomize_patient_name_first(patient, augmented_patients, random: Random.new)
-      original_given_name = patient.givenNames
-      new_given_name = original_given_name.clone
+      original_gn = patient.givenNames
+      new_gn = original_gn.clone
       loop do
-        case random.rand(3) # random chooses how to modify the field
-        when 0 then new_given_name = [original_given_name[0][0]] # replace with only first initial
-        when 1
-          rand_idx = random.rand(original_given_name.count)
-          new_given_name[rand_idx] = replace_random_char(original_given_name[rand_idx].clone, random: random) # insert incorrect letter
-        when 2 # nickname
-          original_given_name.each_index do |idx|
-            nicknames = NAMES_RANDOM['nicknames'][patient.gender][original_given_name[idx]]
-            patients = patient.product_test ? patient.product_test.patients : []
-            new_given_name[idx] = safe_nickname(nicknames,
-                                                original_given_name[idx],
-                                                patient.familyName,
-                                                patients,
-                                                random: random)
-          end
-        end
-        patient.givenNames = new_given_name unless augmented_patients.any? { |ap| ap[:first][1] == new_given_name && ap[:last][0] == patient.familyName }
-        break if patient.givenNames != original_given_name
+        # Try to create a new random name
+        new_gn = random_given_name(patient, original_gn, random: random)
+        # Verify that the new random name, does not conflict with an existing Augmented Patient
+        patient.givenNames = new_gn unless augmented_patients.any? { |ap| ap[:first][1] == new_gn[0] && ap[:last][0] == patient.familyName }
+        # Break when a new unique name is found
+        break if patient.givenNames != original_gn
       end
       patient
     end
 
+    def self.random_given_name(patient, original_given_name, random: Random.new)
+      updated_name = original_given_name.clone
+      case random.rand(3) # random chooses how to modify the field
+      when 0 then [updated_name[0][0]] # replace with only first initial
+      when 1
+        rand_idx = random.rand(updated_name.count)
+        updated_name[rand_idx] = replace_random_char(updated_name[rand_idx].clone, random: random) # insert incorrect letter
+        updated_name
+      when 2 # nickname
+        updated_name.each_index do |idx|
+          nicknames = NAMES_RANDOM['nicknames'][patient.gender][updated_name[idx]]
+          patients = patient.product_test ? patient.product_test.patients : []
+          updated_name[idx] = safe_nickname(nicknames,
+                                            updated_name[idx],
+                                            patient.familyName,
+                                            patients,
+                                            random: random)
+        end
+        updated_name
+      end
+    end
+
     def self.randomize_patient_name_last(patient, augmented_patients, random: Random.new)
-      original_family_name = patient.familyName
-      new_family_name = original_family_name.clone
+      original_fn = patient.familyName
+      new_fn = original_fn.clone
       loop do
+        # Try to create a new random name
         case random.rand(2)
         when 0 then patient.familyName = patient.familyName[0] # replace with initial
         when 1 then patient.familyName = replace_random_char(patient.familyName.clone, random: random) # insert incorrect letter
         end
-        patient.familyName = new_family_name unless augmented_patients.any? { |ap| ap[:last][1] == new_family_name && ap[:first][0] == patient.givenNames }
-        break if patient.familyName != original_family_name
+        # Verify that the new random name, does not conflict with an existing Augmented Patient
+        patient.familyName = new_fn unless augmented_patients.any? { |ap| ap[:last][1] == new_fn && ap[:first][0] == patient.givenNames }
+        # Break when a new unique name is found
+        break if patient.familyName != original_fn
       end
       patient
     end
