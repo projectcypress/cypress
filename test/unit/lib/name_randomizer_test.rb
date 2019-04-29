440d48901f033424ad29be755c51bf8face70f6e
Dave Czulada <dczulada@mitre.org>
2019-04-29 09:44:41 -0400

    Verify that a new name does not conflict with an Augmented Record

--- test/unit/lib/name_randomizer_test.rb
+++ test/unit/lib/name_randomizer_test.rb
@@ -4,9 +4,18 @@ require 'fileutils'
 class NameRandomizerTest < ActiveSupport::TestCase
   setup do
     @pt = FactoryBot.create(:product_test_static_result)
-    record1 = Patient.new(givenNames: ['AA'], familyName: 'BB', extendedData: { 'correlation_id' => @pt.id })
-    record1.save
+    record1 = @pt.patients.first.clone
+    record1.update(givenNames: ['AA'], familyName: 'BB', extendedData: { 'correlation_id' => @pt.id })
     @prng = Random.new(Random.new_seed)
+  end
+
+  def test_randomize_augmented_record
+    augmented_patients = [{ first: %w[AA A], last: %w[BB BB] }]
+    patient_to_clone = @pt.patients.where(familyName: 'BB').first.clone
+    20.times do
+      Cypress::NameRandomizer.randomize_patient_name_first(patient_to_clone, augmented_patients, random: @prng)
+      assert_not_equal ['A'], patient_to_clone.givenNames
+    end
   end
 
   def test_randomize_no_nickname
