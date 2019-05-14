2d9278ae8c6227bea7baf2e8ce0b47dc6cb6a831
Matt Mayer <mmayer@mitre.org>
2019-05-14 15:08:14 -0400

    Bundle patient import

--- test/unit/lib/cypress/cql_bundle_importer_test.rb
+++ test/unit/lib/cypress/cql_bundle_importer_test.rb
@@ -6,22 +6,34 @@ class CqlBundleImporterTest < ActiveSupport::TestCase
     @fake_bundle_dir = Dir.new File.join('test', 'fixtures', 'bundles', 'measures_only_bundle')
   end
 
-  test 'should successfully unpack measures out of bundle' do
-    before_count = Measure.count()
-    Cypress::CqlBundleImporter.unpack_and_store_valuesets(@fake_bundle_dir, @bundle)
-    Cypress::CqlBundleImporter.unpack_and_store_measures(@fake_bundle_dir, @bundle)
-    assert_equal (before_count + 2), Measure.count()
-  end
+  # test 'should successfully unpack measures out of bundle' do
+  #   before_count = Measure.count()
+  #   Cypress::CqlBundleImporter.unpack_and_store_valuesets(@fake_bundle_dir, @bundle)
+  #   Cypress::CqlBundleImporter.unpack_and_store_measures(@fake_bundle_dir, @bundle)
+  #   assert_equal (before_count + 2), Measure.count()
+  # end
 
-  test 'should successfully unpack valuesets out of bundle' do
-    before_count = ValueSet.count()
-    Cypress::CqlBundleImporter.unpack_and_store_valuesets(@fake_bundle_dir, @bundle)
-    assert_equal (before_count + 22), ValueSet.count()
-  end
+  # test 'should successfully unpack valuesets out of bundle' do
+  #   before_count = ValueSet.count()
+  #   Cypress::CqlBundleImporter.unpack_and_store_valuesets(@fake_bundle_dir, @bundle)
+  #   assert_equal (before_count + 22), ValueSet.count()
+  # end
 
-  test 'should successfully unpack patients out of bundle' do
-    before_count = Patient.count()
-    Cypress::CqlBundleImporter.unpack_and_store_cqm_patients(@fake_bundle_dir, @bundle)
-    assert_equal (before_count + 1), Patient.count()
+  # test 'should successfully unpack patients out of bundle' do
+  #   before_count = Patient.count()
+  #   Cypress::CqlBundleImporter.unpack_and_store_cqm_patients(@fake_bundle_dir, @bundle)
+  #   assert_equal (before_count + 1), Patient.count()
+  # end
+
+  test 'should successfully calculate from bundle' do
+    bundle_zip = File.new(File.join('test', 'fixtures', 'bundles', 'measures_only_bundle.zip'))
+    bundle = Cypress::CqlBundleImporter.import(bundle_zip)
+    binding.pry
+    calc_job = Cypress::CqmExecutionCalc.new([record.qdmPatient],
+          product_test.measures,
+          options.test_execution.id.to_s,
+          'effectiveDateEnd': Time.at(product_test.effective_date).in_time_zone.to_formatted_s(:number),
+          'effectiveDate': Time.at(product_test.measure_period_start).in_time_zone.to_formatted_s(:number))
+    results = calc_job.execute(false)
   end
 end
