6abb91bf2000dbee0c43a754754e0ef680d875c6
Matt Mayer <mmayer@mitre.org>
2019-05-15 13:19:47 -0400

    Add result checking into bundle import calcs

--- test/unit/lib/cypress/cql_bundle_importer_test.rb
+++ test/unit/lib/cypress/cql_bundle_importer_test.rb
@@ -5,22 +5,24 @@ class CqlBundleImporterTest < ActiveSupport::TestCase
     @fake_bundle_dir = Dir.new File.join('test', 'fixtures', 'bundles', 'measures_only_bundle')
   end
 
-  test 'should successfully calculate from bundle' do
+  test 'should successfully import bundle and perform calculations' do
     before_measure_count = Measure.count()
     before_value_set_count = ValueSet.count()
     before_patient_count = Patient.count()
+    before_results_count = IndividualResult.count()
     bundle_zip = File.new(File.join('test', 'fixtures', 'bundles', 'measures_only_bundle.zip'))
     bundle = Cypress::CqlBundleImporter.import(bundle_zip)
     measure = Measure.where({bundle_id: bundle.id}).last()
     patient = Patient.where({bundleId: bundle.id}).last()
-    calc_job = Cypress::CqmExecutionCalc.new([patient.qdmPatient],
-          [measure],
-          bundle.id.to_s,
-          'effectiveDateEnd': Time.at(bundle.effective_date).in_time_zone.to_formatted_s(:number),
-          'effectiveDate': Time.at(bundle.measure_period_start).in_time_zone.to_formatted_s(:number))
-    results = calc_job.execute(false)
     assert_equal (before_measure_count + 2), Measure.count()
     assert_equal (before_value_set_count + 21), ValueSet.count()
     assert_equal (before_patient_count + 1), Patient.count()
+    assert_equal (before_results_count + 9), IndividualResult.count()
+    # Assert calculation is correct for a given patient
+    measure_id = Measure.where(cms_id: "CMS111v8").first().id
+    result = IndividualResult.where(population_set_key: "PopulationSet_1", measure_id: measure_id).first
+    assert_equal 1, result.IPP
+    assert_equal 1, result.MSRPOPL
+    assert_equal [5], result.episode_results[result.episode_results.keys[0]]['values']
   end
 end
