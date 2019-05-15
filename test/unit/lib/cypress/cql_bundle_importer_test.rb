2bb8a09870049c145f049a80900ae464e09d1042
Matt Mayer <mmayer@mitre.org>
2019-05-15 09:20:13 -0400

    Consolidate bundle upload assertions to one integration test

--- test/unit/lib/cypress/cql_bundle_importer_test.rb
+++ test/unit/lib/cypress/cql_bundle_importer_test.rb
@@ -2,30 +2,13 @@ require 'test_helper'
 
 class CqlBundleImporterTest < ActiveSupport::TestCase
   setup do
-    @bundle = FactoryBot.create(:static_bundle)
     @fake_bundle_dir = Dir.new File.join('test', 'fixtures', 'bundles', 'measures_only_bundle')
   end
 
-  # test 'should successfully unpack measures out of bundle' do
-  #   before_count = Measure.count()
-  #   Cypress::CqlBundleImporter.unpack_and_store_valuesets(@fake_bundle_dir, @bundle)
-  #   Cypress::CqlBundleImporter.unpack_and_store_measures(@fake_bundle_dir, @bundle)
-  #   assert_equal (before_count + 2), Measure.count()
-  # end
-
-  # test 'should successfully unpack valuesets out of bundle' do
-  #   before_count = ValueSet.count()
-  #   Cypress::CqlBundleImporter.unpack_and_store_valuesets(@fake_bundle_dir, @bundle)
-  #   assert_equal (before_count + 22), ValueSet.count()
-  # end
-
-  # test 'should successfully unpack patients out of bundle' do
-  #   before_count = Patient.count()
-  #   Cypress::CqlBundleImporter.unpack_and_store_cqm_patients(@fake_bundle_dir, @bundle)
-  #   assert_equal (before_count + 1), Patient.count()
-  # end
-
   test 'should successfully calculate from bundle' do
+    before_measure_count = Measure.count()
+    before_value_set_count = ValueSet.count()
+    before_patient_count = Patient.count()
     bundle_zip = File.new(File.join('test', 'fixtures', 'bundles', 'measures_only_bundle.zip'))
     bundle = Cypress::CqlBundleImporter.import(bundle_zip)
     measure = Measure.where({bundle_id: bundle.id}).last()
@@ -36,5 +19,8 @@ class CqlBundleImporterTest < ActiveSupport::TestCase
           'effectiveDateEnd': Time.at(bundle.effective_date).in_time_zone.to_formatted_s(:number),
           'effectiveDate': Time.at(bundle.measure_period_start).in_time_zone.to_formatted_s(:number))
     results = calc_job.execute(false)
+    assert_equal (before_measure_count + 2), Measure.count()
+    assert_equal (before_value_set_count + 21), ValueSet.count()
+    assert_equal (before_patient_count + 1), Patient.count()
   end
 end
