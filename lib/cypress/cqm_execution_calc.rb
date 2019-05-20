eafd50df6f45f90b8fa0d2b6f6692b64fb33cb54
Dave Czulada <dczulada@mitre.org>
2019-05-20 13:24:23 -0400

    revert saving for IPP only for tests.  another PR does this change anyways

--- lib/cypress/cqm_execution_calc.rb
+++ lib/cypress/cqm_execution_calc.rb
@@ -60,7 +60,7 @@ module Cypress
         individual_result['patient_id'] = patient.id.to_s
         individual_result['cqm_patient'] = patient
         # save to database
-        save_individual_result(individual_result) if save && individual_result.IPP != 0
+        save_individual_result(individual_result) if save
         # update the patients, measure_relevance_hash
         patient.update_measure_relevance_hash(individual_result) if individual_result.IPP != 0
       end
