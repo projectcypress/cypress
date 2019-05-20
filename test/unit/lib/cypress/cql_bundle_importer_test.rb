8fb873c07e952c4f0114a8d08b7cc070c765d104
dczulada <dczulada@users.noreply.github.com>
2019-05-20 19:03:31 -0400

    Update cql_bundle_importer_test.rb

--- test/unit/lib/cypress/cql_bundle_importer_test.rb
+++ test/unit/lib/cypress/cql_bundle_importer_test.rb
@@ -12,7 +12,8 @@ class CqlBundleImporterTest < ActiveSupport::TestCase
     # 21 valuesets from csv file, 3 direct reference codes
     assert_equal (before_value_set_count + 24), ValueSet.count
     assert_equal (before_patient_count + 1), Patient.count
-    assert_equal (before_results_count + 9), IndividualResult.count
+    # only 2 individual results are saved
+    assert_equal (before_results_count + 2), IndividualResult.count
     # Assert calculation is correct for a given patient
     measure_id = Measure.where(cms_id: 'CMS111v8').first.id
     result = IndividualResult.where(population_set_key: 'PopulationSet_1', measure_id: measure_id).first
