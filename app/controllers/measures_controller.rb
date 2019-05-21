1ff8425ec5915baff5eb7cc72a56b87e32492ea2
Dave Czulada <dczulada@mitre.org>
2019-05-21 15:40:28 -0400

    measure title in controller

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -8,7 +8,7 @@ class MeasuresController < ApplicationController
 
   def grouped
     @bundle = Bundle.find(params[:bundle_id])
-    @measures = @bundle.measures.only(:cms_id, :description, :category, :hqmf_id, :reporting_program_type)
+    @measures = @bundle.measures.only(:cms_id, :description, :title, :category, :hqmf_id, :reporting_program_type)
     @measures_categories = @measures.group_by(&:category)
     render partial: 'products/measure_selection', locals: { measures_categories: @measures_categories }
   end
@@ -18,7 +18,7 @@ class MeasuresController < ApplicationController
     @filter = Regexp.escape(params[:filter]) if params[:filter]
     @bundle = Bundle.find(params[:bundle_id])
     @measures = @bundle.measures.and(
-      Measure.any_of({ description: /#{@filter}/i }, cms_id: /#{@filter}/i).selector
+      Measure.any_of({ title: /#{@filter}/i }, cms_id: /#{@filter}/i).selector
     ).only(:hqmf_id, :category, :reporting_program_type)
     @measures_categories = @measures.group_by(&:category)
     render partial: 'products/measure_selection.json'
