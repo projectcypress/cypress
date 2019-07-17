ae702d3a4334862889623c8ff0e2d56e6ccd36a3
Padro Rodriguez <luispr@mm197454-pc.mitre.org>
2019-07-17 14:22:39 -0400

    changes in build script to fix the network interface issue

--- contrib/scripts/cleanup.sh
+++ contrib/scripts/cleanup.sh
@@ -3,10 +3,5 @@
 # Ubuntu 16.04 introduces a new feature that causes ethernet device names to
 # no longer be consistent, which causes problems with exported vms.
 # This works around the issue by turning the feature off.
-echo 'GRUB_CMDLINE_LINUX="biosdevname=0 net.ifnames=0"' >> /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+echo -e 'network:\n  version: 2\n  renderer: networkd\n  ethernets:\n    all:\n      match:\n        name: e*\n      dhcp4: yes\n'  > /etc/netplan/01-netcfg.yaml
 
-echo 'auto eth0' >> /etc/network/interfaces
-echo 'iface eth0 inet dhcp' >> /etc/network/interfaces
-echo 'auto eth1' >> /etc/network/interfaces
-echo 'iface eth1 inet manual' >> /etc/network/interfaces
