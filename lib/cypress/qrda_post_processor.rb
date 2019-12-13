b4c274b0ae7536ad465b7a420da9a930228fd6f5
lmd59 <laurend@mitre.org>
2019-12-13 08:49:40 -0500

    basic single code negation error/warning (#1272)
    * basic single code negation error/warning
    
    * Fixes and message changes
    
    * fixes
    
    * Add testing for single code negation
    
    * fix code strong to code object comparison for drc
    
    * Export an entry for each valueset a negated code is part of (#1270)
    
    * Export an entry for each valueset a negated code is part of
    
    * Add test to confirm export of 2 negated valuesets
    
    * update error message

--- lib/cypress/qrda_post_processor.rb
+++ lib/cypress/qrda_post_processor.rb
@@ -1,11 +1,13 @@
 module Cypress
   module QRDAPostProcessor
+    # checks for placeholder negation code_system
     def self.replace_negated_codes(patient, bundle)
       patient.qdmPatient.dataElements.each do |de|
         select_negated_code(de, bundle) if de['negationRationale'] && de.codes.find { |c| c.codeSystemOid == '1.2.3.4.5.6.7.8.9.10' }
       end
     end
 
+    # use "code" (which is used to store the valuset) to find an appropriate actual code to use for calculation
     def self.select_negated_code(data_element, bundle)
       negated_element = data_element.dataElementCodes.map { |dec| dec if dec.codeSystemOid == '1.2.3.4.5.6.7.8.9.10' }.first
       negated_vs = negated_element.code
@@ -18,6 +20,30 @@ module Cypress
                { code: valueset.first.concepts.first['code'], codeSystemOid: valueset.first.concepts.first['code_system_oid'] }
              end
       data_element.dataElementCodes << code
+    end
+
+    # create an issue message for any negations that are done with a single code rather than vs
+    def self.issues_for_negated_single_codes(patient, bundle, measures)
+      drc_codes = ValueSet.where(oid: /drc/i).map { |vs| vs.concepts.first.code }
+      error_list = []
+      patient.qdmPatient.dataElements.each do |de|
+        next unless de['negationRationale']
+
+        de.codes.each do |c|
+          next unless c.codeSystemOid != '1.2.3.4.5.6.7.8.9.10' && !drc_codes.include?(c.code)
+
+          # pull relevant measures from patient if possible
+          vs_ids = measures.map(&:value_set_ids).flatten.uniq
+          potential_vs = ValueSet.where(:id.in => vs_ids, 'concepts.code' => c.code, bundle_id: bundle._id).map(&:oid)
+          vs_list = potential_vs.empty? ? 'None' : potential_vs.join(', ')
+          msg = 'CMS QRDA Implementation Guide, Section 5.2.3.1, “Not Done” with a Reason: ' \
+                'Must provide the value set OID instead of a specific code from the value set. ' \
+                'Set the code attribute code/sdtc:valueset="[VSAC value set OID]" ' \
+                "Valuesets for code #{c.code} that may be relevant for this test include: #{vs_list}"
+          error_list << msg
+        end
+      end
+      error_list
     end
   end
 end
--- lib/cypress/scoop_and_filter.rb
+++ lib/cypress/scoop_and_filter.rb
@@ -20,23 +20,35 @@ module Cypress
 
     def scoop_and_filter(patient)
       demographic_criteria = patient.qdmPatient.dataElements.collect { |de| de if de.qdmCategory == 'patient_characteristic' }.compact
+      # If a negated code belongs to multiple valuesets, we need to add a cloned entry for each valueset.
+      # This array stores the cloned entries to be added
+      multi_vs_negation_elements = []
       patient.qdmPatient.dataElements.keep_if { |de| data_element_used_by_measure(de) }
       patient.qdmPatient.dataElements.each do |data_element|
-        # keep if data_element code and codesystem is in one of the relevant_codes
-        data_element.dataElementCodes.keep_if { |de_code| @relevant_codes.include?(code: de_code.code, codeSystemOid: de_code.codeSystemOid) }
-        # Do not try to replace with negated valueset if all codes are removed
-        next if data_element.dataElementCodes.blank?
-
-        add_description_to_data_element(data_element)
-        replace_negated_code_with_valueset(data_element) if data_element.respond_to?('negationRationale') && data_element.negationRationale
+        scoop_and_filter_data_element_codes(data_element, multi_vs_negation_elements)
       end
       # keep data element if codes is not empty
       patient.qdmPatient.dataElements.keep_if { |data_element| data_element.dataElementCodes.present? }
       patient.qdmPatient.dataElements.concat(demographic_criteria)
+      patient.qdmPatient.dataElements.concat(multi_vs_negation_elements)
       patient
     end
 
     private
+
+    # Method to remove codes from a data element that are not relevant to measure.
+    # Multi_vs_negation_elements is an array of cloned elements to add to patient record to capture all of the negated valuesets
+    def scoop_and_filter_data_element_codes(data_element, multi_vs_negation_elements)
+      # keep if data_element code and codesystem is in one of the relevant_codes
+      data_element.dataElementCodes.keep_if { |de_code| @relevant_codes.include?(code: de_code.code, codeSystemOid: de_code.codeSystemOid) }
+      # Do not try to replace with negated valueset if all codes are removed
+      return if data_element.dataElementCodes.blank?
+
+      add_description_to_data_element(data_element)
+      return unless data_element.respond_to?('negationRationale') && data_element.negationRationale
+
+      replace_negated_code_with_valueset(data_element, multi_vs_negation_elements)
+    end
 
     def data_element_category_and_status(data_element)
       { category: data_element.qdmCategory, status: data_element['qdmStatus'] }
@@ -55,13 +67,22 @@ module Cypress
       de.description = vs.display_name
     end
 
-    def replace_negated_code_with_valueset(data_element)
+    # For negated elements, replace codes (that aren't direct reference codes) with valuesets.
+    # If a code is in multiple valuesets, create new entries to be added to record
+    def replace_negated_code_with_valueset(data_element, multi_vs_negation_elements)
       de = data_element
       neg_vs = @valuesets.select { |vs| vs.concepts.any? { |c| c.code == de.codes.first.code && c.code_system_oid == de.codes.first.codeSystemOid } }
-      # If more than one valueset (in the measures uses the code, it is not possible for scoop and filter to know which valueset to negate)
-      return if neg_vs.size > 1
 
       negated_valueset = neg_vs.first
+      neg_vs.drop(1).each do |additional_vs|
+        next if additional_vs.oid[0, 3] == 'drc'
+
+        de_for_additional_vs = data_element.clone
+        de_for_additional_vs.id = QDM::Id.new(value: BSON::ObjectId.new.to_s)
+        de_for_additional_vs.dataElementCodes = [{ code: additional_vs.oid, codeSystemOid: '1.2.3.4.5.6.7.8.9.10' }]
+        multi_vs_negation_elements << de_for_additional_vs
+      end
+
       # If the first three characters of the valueset oid is drc, this is a direct reference code, not a valueset.  Do not negate a valueset here.
       return if negated_valueset.oid[0, 3] == 'drc'
 
