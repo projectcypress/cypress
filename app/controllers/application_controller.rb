1c940e52648f4f2d318f76bf1eff62b9fbb49bea
Padro Rodriguez <luispr@mitre.org>
2020-03-09 09:04:30 -0400

    changes to enable product report on debug mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_admin_atl
-    raise CanCan::AccessDenied.new, 'Forbidden' unless current_user.user_role?(:admin) || current_user.user_role?(:atl)
+    raise CanCan::AccessDenied.new, 'Forbidden' unless current_user.user_role?(:admin) || current_user.user_role?(:atl) || Settings.current.enable_debug_features
   end
 
   private
