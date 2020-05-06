e4d725f3ea29d1c2fdb9b3042b27e163a088ca9f
Dave Czulada <dczulada@mitre.org>
2020-05-06 13:54:33 -0400

    Update Record Sample to allow for string attribute values

--- lib/validators/checklist_criteria_validator.rb
+++ lib/validators/checklist_criteria_validator.rb
@@ -61,6 +61,7 @@ module Validators
       return true if attribute.is_a? DateTime
       return true if attribute.is_a? Integer
       return true if attribute.is_a? Float
+      return true if attribute.is_a? String
       # If the attribute is an Array, loop through the array
       return attribute.any? { |at| attribute_has_data(at, checked_criteria) } if attribute.is_a? Array
 
