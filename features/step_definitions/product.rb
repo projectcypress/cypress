a0dc1a6d31700adfafdc3b8876496f61a057733f
David S Czulada <dczulada@mitre.org>
2020-08-25 13:44:32 -0400

    without sleeps

--- features/step_definitions/product.rb
+++ features/step_definitions/product.rb
@@ -213,7 +213,6 @@ end
 
 And(/^the user types "([^"]*)" into the measure filter box$/) do |filter_text|
   page.find('#product_search_measures').native.send_keys(filter_text)
-  sleep(0.5)
 end
 
 # ^ ^ ^ Measure Selection ^ ^ ^
@@ -490,7 +489,6 @@ Then(/^all measures should still be selected$/) do
 end
 
 Then(/^"([^"]*)" is active on the screen$/) do |measure|
-  sleep(3)
   page.find('#measure_tabs').assert_text measure
 end
 
