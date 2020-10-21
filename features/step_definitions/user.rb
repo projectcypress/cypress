76d2a5d0409203a36291120bc3517dac7258f1a0
David S Czulada <dczulada@mitre.org>
2020-10-21 10:33:51 -0400

    I swapped the incorrectPasswords out of the wrong test

--- features/step_definitions/user.rb
+++ features/step_definitions/user.rb
@@ -64,8 +64,8 @@ end
 
 When(/^the user tries to log in with invalid information$/) do
   visit '/users/sign_in'
-  page.fill_in 'Email', with: @user.email
-  page.fill_in 'Password', with: @user.password
+  page.fill_in 'Email', with: 'unauth@mitre.org'
+  page.fill_in 'Password', with: 'incorrectPassword'
   page.click_button 'Sign in'
 end
 
@@ -73,8 +73,8 @@ When(/^the user tries to log in with invalid umls information$/) do
   Settings.current.update(umls: true)
   Settings.current.update(http_proxy: '')
   visit '/users/sign_in'
-  page.fill_in 'Email', with: 'unauth@mitre.org'
-  page.fill_in 'Password', with: 'incorrectPassword'
+  page.fill_in 'Email', with: @user.email
+  page.fill_in 'Password', with: @user.password
   page.click_button 'Sign in'
 end
 
