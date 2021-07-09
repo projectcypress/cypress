9bdf02f3474174cf1bc270a64792fbc4bb2d015d
David Czulada <dczulada@mitre.org>
2021-07-09 09:45:14 -0400

    pass in flag for 2021 vs 2022 CMS Cat III IG

--- app/models/multi_measure_cat3_task.rb
+++ app/models/multi_measure_cat3_task.rb
@@ -20,7 +20,7 @@ class MultiMeasureCat3Task < Task
   def good_results
     # Set the Submission Program to MIPS_INDIV
     options = { provider: product_test.patients.first.providers.first, submission_program: 'MIPS_INDIV',
-                start_time: start_date, end_time: end_date }
+                start_time: start_date, end_time: end_date, ry2022_submission: product_test.bundle.major_version == '2021' }
     Qrda3R21.new(product_test.expected_results, product_test.measures, options).render
   end
 end
