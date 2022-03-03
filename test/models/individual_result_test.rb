27a478bdeee3a83b0563c1922868b66a46a89bd7
David Czulada <dczulada@mitre.org>
2022-03-03 15:41:45 -0500

    Add tests for multi ccde validation

--- test/models/individual_result_test.rb
+++ test/models/individual_result_test.rb
@@ -78,4 +78,83 @@ class IndividualResultTest < ActiveSupport::TestCase
     _passed, issues = individual_result.compare_results(calculated, options, false)
     assert issues.include? 'firstHR of [50 /min] is missing'
   end
+
+  def test_issue_for_good_multiple_results
+    @measure.hqmf_id = '2C928082-74C2-3313-0174-E01E3F200882'
+    @measure.save
+    statement_results = [{ 'raw' =>
+                           { 'firstHR' =>
+                             [{ 'FirstResult' => { 'value' => 50, 'unit' => '/min' } },
+                              { 'FirstResult' => { 'value' => 40, 'unit' => '/min' } }] },
+                           'statement_name' => 'Results' }]
+    bad_statement_results = [{ 'raw' =>
+                               { 'firstHR' =>
+                                 [{ 'FirstResult' => { 'value' => 50, 'unit' => '/min' } },
+                                  { 'FirstResult' => { 'value' => 40, 'unit' => '/min' } }] },
+                               'statement_name' => 'Results' }]
+    options = { population_set: { populations: { 'IPP' => { hqmf_id: 'HHH' } } } }
+    calculated = { 'IPP' => 1, 'statement_results' => bad_statement_results }
+    individual_result = CQM::IndividualResult.new(IPP: 1, measure: @measure, patient: @patient, statement_results: statement_results)
+    _passed, issues = individual_result.compare_results(calculated, options, false)
+    assert issues.empty?
+  end
+
+  def test_issue_for_swapped_multiple_results
+    @measure.hqmf_id = '2C928082-74C2-3313-0174-E01E3F200882'
+    @measure.save
+    statement_results = [{ 'raw' =>
+                           { 'firstHR' =>
+                             [{ 'FirstResult' => { 'value' => 50, 'unit' => '/min' } },
+                              { 'FirstResult' => { 'value' => 40, 'unit' => '/min' } }] },
+                           'statement_name' => 'Results' }]
+    bad_statement_results = [{ 'raw' =>
+                               { 'firstHR' =>
+                                 [{ 'FirstResult' => { 'value' => 40, 'unit' => '/min' } },
+                                  { 'FirstResult' => { 'value' => 50, 'unit' => '/min' } }] },
+                               'statement_name' => 'Results' }]
+    options = { population_set: { populations: { 'IPP' => { hqmf_id: 'HHH' } } } }
+    calculated = { 'IPP' => 1, 'statement_results' => bad_statement_results }
+    individual_result = CQM::IndividualResult.new(IPP: 1, measure: @measure, patient: @patient, statement_results: statement_results)
+    _passed, issues = individual_result.compare_results(calculated, options, false)
+    assert issues.empty?
+  end
+
+  def test_issue_for_bad_multiple_results
+    @measure.hqmf_id = '2C928082-74C2-3313-0174-E01E3F200882'
+    @measure.save
+    statement_results = [{ 'raw' =>
+                           { 'firstHR' =>
+                             [{ 'FirstResult' => { 'value' => 50, 'unit' => '/min' } },
+                              { 'FirstResult' => { 'value' => 50, 'unit' => '/min' } }] },
+                           'statement_name' => 'Results' }]
+    bad_statement_results = [{ 'raw' =>
+                               { 'firstHR' =>
+                                 [{ 'FirstResult' => { 'value' => 50, 'unit' => '/min' } },
+                                  { 'FirstResult' => { 'value' => 40, 'unit' => '/min' } }] },
+                               'statement_name' => 'Results' }]
+    options = { population_set: { populations: { 'IPP' => { hqmf_id: 'HHH' } } } }
+    calculated = { 'IPP' => 1, 'statement_results' => bad_statement_results }
+    individual_result = CQM::IndividualResult.new(IPP: 1, measure: @measure, patient: @patient, statement_results: statement_results)
+    _passed, issues = individual_result.compare_results(calculated, options, false)
+    assert issues.include? 'firstHR of [50 /min, 50 /min] does not match [40 /min, 50 /min]'
+  end
+
+  def test_issue_for_missing_multiple_results
+    @measure.hqmf_id = '2C928082-74C2-3313-0174-E01E3F200882'
+    @measure.save
+    statement_results = [{ 'raw' =>
+                           { 'firstHR' =>
+                             [{ 'FirstResult' => { 'value' => 50, 'unit' => '/min' } },
+                              { 'FirstResult' => { 'value' => 50, 'unit' => '/min' } }] },
+                           'statement_name' => 'Results' }]
+    bad_statement_results = [{ 'raw' =>
+                               { 'firstHR' =>
+                                 [{ 'FirstResult' => { 'value' => 50, 'unit' => '/min' } }] },
+                               'statement_name' => 'Results' }]
+    options = { population_set: { populations: { 'IPP' => { hqmf_id: 'HHH' } } } }
+    calculated = { 'IPP' => 1, 'statement_results' => bad_statement_results }
+    individual_result = CQM::IndividualResult.new(IPP: 1, measure: @measure, patient: @patient, statement_results: statement_results)
+    _passed, issues = individual_result.compare_results(calculated, options, false)
+    assert issues.include? 'firstHR of [50 /min, 50 /min] does not match [50 /min]'
+  end
 end
