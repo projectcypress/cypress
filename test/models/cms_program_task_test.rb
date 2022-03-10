f8a7daa3cc0dafe52b1833b6c221890f1564bfa3
David Czulada <dczulada@mitre.org>
2022-03-10 12:49:55 -0500

    Add program specific constraints to expected measure check

--- test/models/cms_program_task_test.rb
+++ test/models/cms_program_task_test.rb
@@ -97,10 +97,27 @@ class CMSProgramTaskTest < ActiveSupport::TestCase
     perform_enqueued_jobs do
       te = task.execute(file, @user)
       te.reload
-      assert_equal 18, te.execution_errors.size
+      assert_equal 17, te.execution_errors.size
       assert_equal 1, te.execution_errors.where(validator: 'Validators::ProgramValidator').size
-      assert_equal 13, te.execution_errors.where(validator: 'Validators::Cat3PopulationValidator').size
+      assert_equal 12, te.execution_errors.where(validator: 'Validators::Cat3PopulationValidator').size
       assert_equal 4, te.execution_errors.where(validator: 'Validators::ProgramCriteriaValidator').size
+    end
+  end
+
+  def test_should_not_warn_for_missing_non_pcf_measure
+    setup_ep
+    task = @product.product_tests.cms_program_tests.where(cms_program: 'MIPS_INDIV').first.tasks.first
+    file = File.new(Rails.root.join('test', 'fixtures', 'qrda', 'cat_III', 'cms_test_qrda_cat3_cv.xml'))
+    perform_enqueued_jobs do
+      te = task.execute(file, @user)
+      te.reload
+      assert_equal 1, te.execution_errors.where(message: 'Document does not state it is reporting measure CMS134v6').size
+    end
+    task = @product.product_tests.cms_program_tests.where(cms_program: 'PCF').first.tasks.first
+    perform_enqueued_jobs do
+      te = task.execute(file, @user)
+      te.reload
+      assert_equal 0, te.execution_errors.where(message: 'Document does not state it is reporting measure CMS134v6').size
     end
   end
 
