796681762f2b307624591f54430573acc4963e35
David Czulada <dczulada@mitre.org>
2023-03-16 10:54:23 -0400

    update rack version
    update rails too
    
    add codecov action
    
    back to ubuntu 18 til 22 incorporates mongodb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,27 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+SimpleCov.start 'rails'
+
+module SimpleCov
+  module Formatter
+    class QualityFormatter
+      def format(result)
+        SimpleCov::Formatter::HTMLFormatter.new.format(result)
+        File.open('coverage/covered_percent', 'w') do |f|
+          f.puts result.source_files.covered_percent.to_f
+        end
+      end
+    end
+  end
+end
+
+if ENV['CI'] == 'true'
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+else
+  SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
+end
 
 ENV['IGNORE_ROLES'] ||= 'false'
 
