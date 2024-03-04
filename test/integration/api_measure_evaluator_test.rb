f81b33b5a3fcd73514934fb1d3cbbe747c1c0bbf
David Czulada <dczulada@mitre.org>
2024-03-04 12:23:57 -0500

    update vcrs

--- test/integration/api_measure_evaluator_test.rb
+++ test/integration/api_measure_evaluator_test.rb
@@ -24,9 +24,9 @@ class ApiMeasureEvaluatorTest < ActionController::TestCase
 
   # Get bundle from the demo server.  Use VCR if available
   def retrieve_bundle
-    VCR.use_cassette('bundle_download') do
+    VCR.use_cassette('bundle_download_2023') do
       bundle_resource = RestClient::Request.execute(method: :get,
-                                                    url: 'https://cypress.healthit.gov/measure_bundles/fixture-bundle-2020.zip',
+                                                    url: 'https://cypress.healthit.gov/measure_bundles/bundle-2023.zip',
                                                     user: ENV.fetch('VSAC_USERNAME', nil),
                                                     password: ENV.fetch('VSAC_PASSWORD', nil),
                                                     raw_response: true,
