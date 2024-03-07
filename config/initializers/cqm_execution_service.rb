7f8fc18cdaef9f20141ef1b6922c40305734439f
David Czulada <dczulada@mitre.org>
2024-03-07 12:45:48 -0500

    remove calls to the cqm-execution-service-55

--- config/initializers/cqm_execution_service.rb
+++ config/initializers/cqm_execution_service.rb
@@ -1,4 +1,2 @@
-Rails.application.config.ces_55_host = ENV['CQM_EXECUTION55_SERVICE_HOST'] || 'localhost'
-Rails.application.config.ces_55_port = ENV['CQM_EXECUTION55_SERVICE_PORT'] || '8081'
 Rails.application.config.ces_56_host = ENV['CQM_EXECUTION_SERVICE_HOST'] || 'localhost'
 Rails.application.config.ces_56_port = ENV['CQM_EXECUTION_SERVICE_PORT'] || '8082'
