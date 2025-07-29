c3c4729e035e435013d74ab209158f6ecfdea415
David Czulada <dczulada@mitre.org>
2025-07-29 15:58:45 -0400

    more updates

--- app/assets/javascripts/vendors.js
+++ app/assets/javascripts/vendors.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const commentsContainer = document.getElementById("pocs");
   const addCommentButton = document.getElementById("add-poc");
 
+  let uniqueIndex = new Date().getTime();
+
   addCommentButton.addEventListener("click", () => {
     // Get the template for a new comment
     const newCommentTemplate = document.querySelector("#new-poc-template").innerHTML;
 
+    const newFieldHtml = newCommentTemplate.replace(/new_record/g, uniqueIndex);
+
     // Insert the new comment fields into the container
-    commentsContainer.insertAdjacentHTML("beforeend", newCommentTemplate);
+    commentsContainer.insertAdjacentHTML("beforeend", newFieldHtml);
+
+    uniqueIndex++;
   });
 
   commentsContainer.addEventListener("click", (event) => {
