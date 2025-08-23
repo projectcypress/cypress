5f94009b9f5571eb181abeb5dd23d2466c4118c7
Patrick O'Leary <poleary@mitre.org>
2025-08-23 10:21:57 -0400

    try test fix

--- test/controllers/test_executions_controller_test.rb
+++ test/controllers/test_executions_controller_test.rb
@@ -442,7 +442,11 @@ class TestExecutionsControllerTest < ActionController::TestCase
     @first_c2_task.save!
     for_each_logged_in_user([ADMIN, ATL, OWNER, VENDOR]) do
       perform_enqueued_jobs do
-        post :create, params: { format: :json, task_id: @first_c2_task.id, results: zip_upload }
+        post :create,
+             params: { task_id: @first_c2_task.id,
+                       results: zip_upload },
+             as: :multipart,
+             format: :json
         # post :create, params: { format: :json, task_id: @first_c2_task.id, results: zip_upload }, as: :multipart
         get :show, params: { format: :json, task_id: @first_c2_task.id, id: @first_c2_task.most_recent_execution.id }
         assert_response 200, 'response should be OK on test_execution show'
