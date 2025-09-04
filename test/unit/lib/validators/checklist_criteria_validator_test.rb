37c439e5cf57291a6345abb692b4322ade72e7ad
David Czulada <dczulada@mitre.org>
2025-09-04 09:52:17 -0400

    skip these long tests too

--- test/unit/lib/validators/checklist_criteria_validator_test.rb
+++ test/unit/lib/validators/checklist_criteria_validator_test.rb
@@ -63,53 +63,53 @@ class ChecklistCriteriaValidator < ActiveSupport::TestCase
     @checklist_test.save
   end
 
-  def test_validate_good_files
-    validator = CqmValidators::Cat1R53.instance
-    cda_validator = CqmValidators::CDA.instance
+  # def test_validate_good_files
+  #   validator = CqmValidators::Cat1R53.instance
+  #   cda_validator = CqmValidators::CDA.instance
 
-    TEST_ATTRIBUTES.each do |ta|
-      dt = QDM::PatientGeneration.generate_loaded_datatype(ta[6], ta[7])
-      negated_valueset_or_drc = nil
-      if ta[3]
-        # For negations, randomly assign a valueset or direct reference code
-        if [true, false].sample
-          negated_valueset_or_drc = 'drc-8ea552d96b89cc373a6adc60b8c6d8afcbde72628d5ff6b519a3232fc211b2ee'
-          dt.dataElementCodes = [{ code: '1001', system: '2.16.840.1.113883.6.96' }]
-        else
-          negated_valueset_or_drc = '1.3.4.5'
-          dt.dataElementCodes = [{ code: '1.3.4.5', system: '1.2.3.4.5.6.7.8.9.10' }]
-        end
-      end
+  #   TEST_ATTRIBUTES.each do |ta|
+  #     dt = QDM::PatientGeneration.generate_loaded_datatype(ta[6], ta[7])
+  #     negated_valueset_or_drc = nil
+  #     if ta[3]
+  #       # For negations, randomly assign a valueset or direct reference code
+  #       if [true, false].sample
+  #         negated_valueset_or_drc = 'drc-8ea552d96b89cc373a6adc60b8c6d8afcbde72628d5ff6b519a3232fc211b2ee'
+  #         dt.dataElementCodes = [{ code: '1001', system: '2.16.840.1.113883.6.96' }]
+  #       else
+  #         negated_valueset_or_drc = '1.3.4.5'
+  #         dt.dataElementCodes = [{ code: '1.3.4.5', system: '1.2.3.4.5.6.7.8.9.10' }]
+  #       end
+  #     end
 
-      setup_sdc(dt.clone, ta[2], negated_valueset_or_drc)
-      test_specific_qdm_patient = qdm_patient_for_attribute(dt, ta, @qdm_patient)
+  #     setup_sdc(dt.clone, ta[2], negated_valueset_or_drc)
+  #     test_specific_qdm_patient = qdm_patient_for_attribute(dt, ta, @qdm_patient)
 
-      @cqm_patient.qdmPatient = test_specific_qdm_patient
+  #     @cqm_patient.qdmPatient = test_specific_qdm_patient
 
-      patient_xml = Qrda1R5.new(@cqm_patient, Measure.where(hqmf_id: 'BE65090C-EB1F-11E7-8C3F-9A214CF093AE'), @options).render
+  #     patient_xml = Qrda1R5.new(@cqm_patient, Measure.where(hqmf_id: 'BE65090C-EB1F-11E7-8C3F-9A214CF093AE'), @options).render
 
-      doc = Nokogiri::XML::Document.parse(patient_xml)
-      doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-      doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
-      exported_qrda = doc
-      # File.write("script/checklist_errors/#{ta[6].gsub(/:/, '')}_#{ta[2]}.xml", exported_qrda.to_xml)
-      @validator.validate(doc)
+  #     doc = Nokogiri::XML::Document.parse(patient_xml)
+  #     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+  #     doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
+  #     exported_qrda = doc
+  #     # File.write("script/checklist_errors/#{ta[6].gsub(/:/, '')}_#{ta[2]}.xml", exported_qrda.to_xml)
+  #     @validator.validate(doc)
 
-      begin
-        errors = validator.validate(exported_qrda)
-        cda_errors = cda_validator.validate(exported_qrda)
-        errors.each do |error|
-          puts "\e[31mSchematron Error In #{ta[0]}_#{ta[1]}: #{error.message}\e[0m"
-        end
-        cda_errors.each do |error|
-          puts "\e[31mSchema Error In #{ta[0]}_#{ta[1]}: #{error.message}\e[0m"
-        end
-      rescue StandardError => e
-        puts "\e[31mException validating #{ta[0]}_#{ta[1]}: #{e.message}\e[0m"
-      end
-      assert @checklist_test.checked_criteria.first[:passed_qrda], "should pass with a good file for #{ta[0]} #{ta[1]} with a #{ta[2]}"
-    end
-  end
+  #     begin
+  #       errors = validator.validate(exported_qrda)
+  #       cda_errors = cda_validator.validate(exported_qrda)
+  #       errors.each do |error|
+  #         puts "\e[31mSchematron Error In #{ta[0]}_#{ta[1]}: #{error.message}\e[0m"
+  #       end
+  #       cda_errors.each do |error|
+  #         puts "\e[31mSchema Error In #{ta[0]}_#{ta[1]}: #{error.message}\e[0m"
+  #       end
+  #     rescue StandardError => e
+  #       puts "\e[31mException validating #{ta[0]}_#{ta[1]}: #{e.message}\e[0m"
+  #     end
+  #     assert @checklist_test.checked_criteria.first[:passed_qrda], "should pass with a good file for #{ta[0]} #{ta[1]} with a #{ta[2]}"
+  #   end
+  # end
 
   def test_validate_result_with_string
     validator = CqmValidators::Cat1R53.instance
@@ -146,41 +146,41 @@ class ChecklistCriteriaValidator < ActiveSupport::TestCase
     assert @checklist_test.checked_criteria.first[:passed_qrda]
   end
 
-  def test_validate_bad_files
-    # bad deletes attribute, then confirms that we don't import it
-    TEST_ATTRIBUTES.each do |ta|
-      restricted_dt = QDM::PatientGeneration.generate_loaded_datatype(ta[6])
-      setup_sdc(restricted_dt, ta[2], ta[3])
+  # def test_validate_bad_files
+  #   # bad deletes attribute, then confirms that we don't import it
+  #   TEST_ATTRIBUTES.each do |ta|
+  #     restricted_dt = QDM::PatientGeneration.generate_loaded_datatype(ta[6])
+  #     setup_sdc(restricted_dt, ta[2], ta[3])
 
-      restricted_dt.prescriberId = QDM::Identifier.new(namingSystem: '1.2.3.4', value: '1234') if restricted_dt.respond_to?(:prescriberId)
-      restricted_dt.dispenserId = QDM::Identifier.new(namingSystem: '1.2.3.4', value: '1234') if restricted_dt.respond_to?(:dispenserId)
+  #     restricted_dt.prescriberId = QDM::Identifier.new(namingSystem: '1.2.3.4', value: '1234') if restricted_dt.respond_to?(:prescriberId)
+  #     restricted_dt.dispenserId = QDM::Identifier.new(namingSystem: '1.2.3.4', value: '1234') if restricted_dt.respond_to?(:dispenserId)
 
-      if restricted_dt.respond_to?(:relevantDatetime) && restricted_dt.respond_to?(:relevantPeriod) && ta[2] == 'relevantPeriod'
-        restricted_dt.relevantDatetime = nil
-      end
-      if restricted_dt.respond_to?(:relevantDatetime) && restricted_dt.respond_to?(:relevantPeriod) && ta[2] == 'relevantDatetime'
-        restricted_dt.relevantPeriod = nil
-      end
+  #     if restricted_dt.respond_to?(:relevantDatetime) && restricted_dt.respond_to?(:relevantPeriod) && ta[2] == 'relevantPeriod'
+  #       restricted_dt.relevantDatetime = nil
+  #     end
+  #     if restricted_dt.respond_to?(:relevantDatetime) && restricted_dt.respond_to?(:relevantPeriod) && ta[2] == 'relevantDatetime'
+  #       restricted_dt.relevantPeriod = nil
+  #     end
 
-      restricted_dt[ta[2]] = nil
-      remove_embeded_objects(restricted_dt, ta[2])
+  #     restricted_dt[ta[2]] = nil
+  #     remove_embeded_objects(restricted_dt, ta[2])
 
-      test_specific_qdm_patient = @qdm_patient.clone
-      test_specific_qdm_patient.dataElements << restricted_dt
-      @cqm_patient.qdmPatient = test_specific_qdm_patient
+  #     test_specific_qdm_patient = @qdm_patient.clone
+  #     test_specific_qdm_patient.dataElements << restricted_dt
+  #     @cqm_patient.qdmPatient = test_specific_qdm_patient
 
-      patient_xml = Qrda1R5.new(@cqm_patient, Measure.where(hqmf_id: 'BE65090C-EB1F-11E7-8C3F-9A214CF093AE'), @options).render
+  #     patient_xml = Qrda1R5.new(@cqm_patient, Measure.where(hqmf_id: 'BE65090C-EB1F-11E7-8C3F-9A214CF093AE'), @options).render
 
-      doc = Nokogiri::XML::Document.parse(patient_xml)
-      doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-      doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
+  #     doc = Nokogiri::XML::Document.parse(patient_xml)
+  #     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+  #     doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
 
-      @validator.validate(doc)
+  #     @validator.validate(doc)
 
-      # File.write("script/checklist_without/#{ta[6].gsub(/:/, '')}_#{ta[2]}.xml", doc.to_xml)
-      assert_nil @checklist_test.checked_criteria.first[:passed_qrda], "should fail with a bad file for #{ta[0]} #{ta[1]} without a #{ta[2]}"
-    end
-  end
+  #     # File.write("script/checklist_without/#{ta[6].gsub(/:/, '')}_#{ta[2]}.xml", doc.to_xml)
+  #     assert_nil @checklist_test.checked_criteria.first[:passed_qrda], "should fail with a bad file for #{ta[0]} #{ta[1]} without a #{ta[2]}"
+  #   end
+  # end
 
   def remove_embeded_objects(restricted_dt, attribute_name)
     restricted_dt.performer.destroy if attribute_name == 'performer'
