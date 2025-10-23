43cb0956413b83d6647ab956e3bea843a0267042
David Czulada <dczulada@mitre.org>
2025-10-23 10:18:41 -0400

    not min

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,5 @@ pin "application", preload: true
 pin "jquery2" # @2.0.3
 pin "turbolinks" # @5.2.0
 pin "@popperjs/core", to: "extensions/@popperjs--core.js", preload: true # @2.11.8
-pin "bootstrap", to: 'extensions/bootstrap.bundle.min.js', preload: true # @5.3.8
+pin "bootstrap", to: 'extensions/bootstrap.bundle.js', preload: true # @5.3.8
 pin "cypress", to: "extensions/cypress.js"
