e6341703eed5c91fb57307f1492e1f4847c438c7
Elsa <eperelli@mitre.org>
2026-03-12 10:34:02 -0400

    Remove public/assets test execution files, add folder to .gitignore

--- public/assets/extensions/jquery2_global-40693a64.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery2-core";
-
-// Make the imported instance the global one used by plugins
-window.$ = $;
-window.jQuery = $;
-
-export default $;
--- public/assets/manifest-f9c8da9eac0f5f8d20bcc68620d6565eb584432d5b6ab9d198815d78427c5452.js
+++
@@ -1,5 +0,0 @@
-// manifest.js necessary for newer version of dartsass-sprockets
-
-
-// compile all SCSS through application.scss, expose that one CSS bundle
-;
