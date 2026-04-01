3605d4f091fc1cfd1adc3b598009d120fe66249a
David Czulada <dczulada@mitre.org>
2026-04-01 11:22:31 -0400

    cleanup importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,24 +5,13 @@
 pin 'application', preload: true
 pin 'jquery2-core', to: 'jquery2.min.js' # or whatever file jquery2 currently resolves to in your setup
 pin 'jquery2', to: 'jquery2_global.js'
-pin 'turbolinks' # @5.2.0
 pin '@popperjs/core', to: 'popper.min.js', preload: true # @2.11.8
 pin 'bootstrap', to: 'bootstrap.bundle.min.js', preload: true # @5.3.8
 pin 'cypress', to: 'extensions/cypress.js'
 pin 'datatables', to: 'datatables.min.js'
 pin 'assets.core', to: 'assets_framework/assets.core.js'
-# pin "jquery-ui/widgets/autocomplete", to: "extensions/jquery-ui.js" # @1.14.1
 pin 'jquery-ui', to: 'jquery-ui.min.js' # @1.14.1
 pin 'parsleyjs', to: 'parsley/parsley.min.js'
-# pin "jquery-ui/widgets/accordion", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/button", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/dialog", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/menu", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/progressbar", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/slider", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/spinner", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/tooltip", to: "extensions/jquery-ui.js" # @1.14.1
-# pin "jquery-ui/widgets/datepicker", to: "extensions/jquery-ui.js" # @1.14.1
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
